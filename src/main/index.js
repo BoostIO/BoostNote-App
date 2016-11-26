3dcb7b1b643abd4a9846aa477836b68278e9e256
Sarah Seo <sarah.seo.0311@gmail.com>
2016-11-26 17:25:42 +0900

    Add router logger for developing

--- src/main/index.js
+++ src/main/index.js
@@ -7,7 +7,9 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer'
 import App from './App'
 
-if (process.env.NODE_ENV !== 'production') {
+const isDev = process.env.NODE_ENV !== 'production'
+
+if (isDev) {
   installExtension(REACT_DEVELOPER_TOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
     .catch((err) => console.log('An error occurred: ', err))
@@ -23,6 +25,14 @@ document.addEventListener('dragover', function (e) {
 })
 
 const history = syncHistoryWithStore(hashHistory, store)
+
+if (isDev) {
+  history.listen((location) => {
+    if (location.action === 'PUSH') {
+      console.info('PUSHING...', location.pathname + location.search)
+    }
+  })
+}
 
 const render = () => {
   ReactDOM.render(
