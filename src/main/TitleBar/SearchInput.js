587b937a597a99565e49e6d3f224bd097cabaea7
Sarah Seo <sarah.seo.0311@gmail.com>
2016-12-25 17:56:47 +0900

    :art: Improve behavior of SearchInput Add Reset Button Add Search Icon

--- src/main/TitleBar/SearchInput.js
+++ src/main/TitleBar/SearchInput.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import styled from 'styled-components'
+import Octicon from 'components/Octicon'
 
 const Root = styled.div`
   height: 26px;
@@ -7,14 +8,49 @@ const Root = styled.div`
   display: inline-block;
   margin: 0 2.5px;
   outline: none;
-  .search {
-    ${p => p.theme.input}
+  .Octicon {
+    position: absolute;
+    left: 2px;
+    top: 7px;
+    height: 12px;
+    width: 24px;
+    text-align: center;
+    fill: ${p => p.theme.inactiveColor};
+    pointer-events: none;
+  }
+  .input {
+    ${p => p.theme.input};
+    background-color: transparent;
+    color: ${p => p.theme.color};
+    outline: none;
     -webkit-app-region: no-drag;
     -webkit-user-select: none;
     height: 26px;
-    padding: 0 10px;
+    width: 150px;
+    padding: 0 28px 0 24px;
     box-sizing: border-box;
     margin-left: auto;
+  }
+  .reset {
+    position: absolute;
+    right: 0;
+    font-size: 12px;
+    width: 28px;
+    height: 28px;
+    background: transparent;
+    border: none;
+    padding: 0;
+    outline: none;
+    color: ${p => p.theme.inactiveColor};
+    .Octicon {
+      fill: currentColor;
+    }
+    &:hover {
+      color: ${p => p.theme.color};
+    }
+    &:active {
+      color: ${p => p.theme.activeColor};
+    }
   }
 `
 
@@ -30,8 +66,11 @@ class SearchInput extends React.Component {
   handleKeyDown = e => {
     switch (e.keyCode) {
       case 27:
-        this.props.onChange('')
-        window.dispatchEvent(new window.CustomEvent('list:focus'))
+        if (this.props.value.length > 0) {
+          this.reset()
+        } else {
+          window.dispatchEvent(new window.CustomEvent('list:focus'))
+        }
         break
       case 13:
         window.dispatchEvent(new window.CustomEvent('list:focus'))
@@ -39,9 +78,17 @@ class SearchInput extends React.Component {
     }
   }
 
+  handleResetButtonClick = e => {
+    this.reset()
+  }
+
   focus () {
     this.refs.input.select()
     this.refs.input.focus()
+  }
+
+  reset () {
+    this.props.onChange('')
   }
 
   render () {
@@ -50,7 +97,8 @@ class SearchInput extends React.Component {
       tabIndex='-1'
       onMouseDown={this.handleMouseDown}
     >
-      <input className='search'
+      <Octicon icon='search' />
+      <input className='input'
         ref='input'
         title='Quickly find notes'
         placeholder='Search...'
@@ -59,6 +107,13 @@ class SearchInput extends React.Component {
         onMouseDown={this.handleMouseDown}
         onKeyDown={this.handleKeyDown}
       />
+      {this.props.value.length > 0 &&
+        <button className='reset'
+          onClick={this.handleResetButtonClick}
+        >
+          <Octicon icon='x' />
+        </button>
+      }
     </Root>
   }
 }
