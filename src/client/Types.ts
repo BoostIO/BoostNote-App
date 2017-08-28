fa85c055db82217df1e981dcbc5232ea245ba648
Junyoung Choi <fluke8259@gmail.com>
2017-08-29 00:27:49 +0900

    Add common types for Note, Folder and Repository

--- src/client/Types.ts
+++ src/client/Types.ts
@@ -4,6 +4,19 @@ namespace Types {
     search: string
     hash: string
   }
+
+  export interface Note {
+    content: string
+    folder: string
+    createdAt: Date
+    updatedAt: Date
+  }
+
+  export interface Folder {
+  }
+
+  export interface Repository {
+  }
 }
 
 export default Types
