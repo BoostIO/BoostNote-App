8c5e388dd90a18f21665a70e7c1bd476b2a831fe
Junyoung Choi <fluke8259@gmail.com>
2018-07-14 02:01:46 +0900

    Discard legacy code

--- src/main/lib/dataAPI/consts.js
+++ src/main/lib/dataAPI/consts.js
@@ -4,15 +4,3 @@ export const TAG_ID_PREFIX = 'tag:'
 export const isNoteId = new RegExp(`^${NOTE_ID_PREFIX}.+`)
 export const isFolderId = new RegExp(`^${FOLDER_ID_PREFIX}.+`)
 export const isTagId = new RegExp(`^${TAG_ID_PREFIX}.+`)
-
-// Views for reducin' & mappin'
-export const noteView = {
-  _id: '_design/notes',
-  views: {
-    by_folder: {
-      map: `function mapFun(doc) {
-        emit(doc.folder);
-      }`
-    }
-  }
-}
