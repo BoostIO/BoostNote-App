9d29529346935cb55614aa4c60998348ff6be574
Junyoung Choi <fluke8259@gmail.com>
2018-07-18 23:13:40 +0900

    Implement synced router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'mobx-react'
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
+import { Router } from 'react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+const routerStore = new RouterStore()
+
+const stores = {
+  router: routerStore
+}
+
+const history = syncHistoryWithStore(createBrowserHistory(), routerStore)
+
+function render (AppComponent: typeof React.Component) {
+  ReactDOM.render(
+    <Provider {...stores}>
+      <Router history={history}>
+        <AppComponent />
+      </Router>
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App.tsx', () => {
+    render(App)
+  })
+}
