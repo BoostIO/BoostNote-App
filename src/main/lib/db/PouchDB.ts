0a83c4744dded58f3a3f0587b4d6278511611337
Junyoung Choi <fluke8259@gmail.com>
2018-10-31 00:04:13 +0900

    Enable pouchdb-mapreduce

--- src/main/lib/db/PouchDB.ts
+++ src/main/lib/db/PouchDB.ts
@@ -1,6 +1,7 @@
 import PouchDBCore from 'pouchdb-core'
 import PouchDBIDBAdapter from 'pouchdb-adapter-idb'
 import PouchDBMemoryAdapter from 'pouchdb-adapter-memory'
+import PouchDBMapReduce from 'pouchdb-mapreduce'
 import PouchDBHttpAdapter from 'pouchdb-adapter-http'
 import PouchDBReplication from 'pouchdb-replication'
 
@@ -8,6 +9,7 @@ const PouchDB = PouchDBCore
   .plugin(PouchDBIDBAdapter)
   .plugin(PouchDBMemoryAdapter)
   .plugin(PouchDBHttpAdapter)
+  .plugin(PouchDBMapReduce)
   .plugin(PouchDBReplication)
 
 export default PouchDB
