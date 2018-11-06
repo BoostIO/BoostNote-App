61e362ec436fbac58873bf90712d7d767ad6ea36
Junyoung Choi <fluke8259@gmail.com>
2018-11-06 16:27:04 +0900

    Replace parcel with webpack

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'mobx-react'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import DataStore from './stores/DataStore'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+const data = new DataStore()
+
+function render (Component: typeof App) {
+  ReactDOM.render(
+      <Provider data={data}>
+        <BrowserRouter>
+          <Component />
+        </BrowserRouter>
+      </Provider>,
+    document.getElementById('root')
+  )
+}
+
+if (module.hot != null) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
+render(App)
---
+++ webpack.config.ts
@@ -0,0 +1,77 @@
+const path = require('path')
+const webpack = require('webpack')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:3000',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './src/main/index.tsx'
+    // the entry point of our app
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: path.resolve(__dirname, 'dist'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          { loader: 'ts-loader', options: { happyPackMode: true } }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+
+    new ForkTsCheckerWebpackPlugin({
+      tslint: true,
+      checkSyntacticErrors: true,
+      watch: ['./src'] // optional but improves performance (fewer stat calls)
+    })
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+
+    historyApiFallback: true,
+    // respond to 404s with index.html
+
+    hot: true
+    // enable HMR on the server
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+}
