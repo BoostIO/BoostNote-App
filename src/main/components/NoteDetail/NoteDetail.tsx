98878e6f154933c406246c8d70f9e380338d489f
Junyoung Choi <fluke8259@gmail.com>
2018-11-22 18:05:01 +0900

    Implement debounce saving

--- src/main/components/NoteDetail/NoteDetail.tsx
+++ src/main/components/NoteDetail/NoteDetail.tsx
@@ -9,7 +9,10 @@ type NoteDetailProps = {
   route?: RouteStore
 }
 
-type NoteDetailState = {}
+type NoteDetailState = {
+  prevNoteId: string
+  content: string
+}
 
 const storageRegexp = pathToRegexp('/storages/:storageName/:rest*', undefined, {
   sensitive: true
@@ -21,6 +24,12 @@ export default class NoteDetail extends React.Component<
   NoteDetailProps,
   NoteDetailState
 > {
+  state = {
+    prevNoteId: '',
+    content: ''
+  }
+  contentTextareaRef = React.createRef<HTMLTextAreaElement>()
+
   // TODO: Duplicated
   getCurrentStorageName() {
     const { route } = this.props
@@ -47,6 +56,83 @@ export default class NoteDetail extends React.Component<
     return storage.noteMap.get(id)
   }
 
+  static getDerivedStateFromProps(
+    props: NoteDetailProps,
+    state: NoteDetailState
+  ): NoteDetailState {
+    const { route, data } = props
+    const { hash } = route!
+    const noteId = hash.slice(1)
+    if (noteId !== state.prevNoteId) {
+      const { pathname } = route!
+      const result = storageRegexp.exec(pathname)
+      if (result == null)
+        return {
+          prevNoteId: noteId,
+          content: ''
+        }
+      const [, storageName] = result
+      const storage = data!.storageMap.get(storageName)
+      if (storage == null)
+        return {
+          prevNoteId: noteId,
+          content: ''
+        }
+      const note = storage.noteMap.get(noteId)
+      if (note == null)
+        return {
+          prevNoteId: noteId,
+          content: ''
+        }
+
+      return {
+        prevNoteId: noteId,
+        content: note.content
+      }
+    }
+    return state
+  }
+
+  updateContent = () => {
+    this.setState(
+      {
+        content: this.contentTextareaRef.current!.value
+      },
+      () => {
+        this.queueToSave()
+      }
+    )
+  }
+
+  queued = false
+  timer: NodeJS.Timeout
+
+  queueToSave = () => {
+    this.queued = true
+    if (this.timer != null) {
+      clearTimeout(this.timer)
+    }
+    this.timer = setTimeout(() => {
+      this.saveNote()
+    }, 3000)
+    console.log('queued')
+  }
+
+  async saveNote() {
+    clearTimeout(this.timer)
+    this.queued = false
+
+    const { data, route } = this.props
+    const { content } = this.state
+    const { hash } = route!
+    const noteId = hash.slice(1)
+    const storageName = this.getCurrentStorageName()
+    await data!.updateNote(storageName, noteId, {
+      content
+    })
+    console.log('saved')
+  }
+
   render() {
     const note = this.getNote()
     return (
@@ -58,7 +144,11 @@ export default class NoteDetail extends React.Component<
           <>
             <div>{note._id}</div>
             <div>
-              <textarea value={note.content} readOnly={true} />
+              <textarea
+                ref={this.contentTextareaRef}
+                value={this.state.content}
+                onChange={this.updateContent}
+              />
             </div>
           </>
         )}
