14fd67f422a2bb3b2067a465b4ce7a00ff614ca6
Junyoung Choi <fluke8259@gmail.com>
2018-12-19 12:57:38 +0900

    Apply global background color and text color

--- src/main/components/GlobalStyle.tsx
+++ src/main/components/GlobalStyle.tsx
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  body {
+    color: #DDD;
+    background-color: #252525;
+  }
   a {
     color: inherit;
   }
