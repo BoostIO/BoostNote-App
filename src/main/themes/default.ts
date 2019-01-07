81f6494bc36072320063674fbbba465171806655
Junyoung Choi <fluke8259@gmail.com>
2019-01-07 13:26:04 +0900

    Implement default theme

---
+++ src/main/themes/default.ts
@@ -0,0 +1,16 @@
+import colors from 'open-color'
+import { Theme } from './interface'
+
+const textColor = colors.gray[2]
+const backgroundColor = colors.gray[9]
+const activeColor = colors.blue[5]
+
+export const defaultTheme: Theme = {
+  global: {
+    textColor,
+    backgroundColor
+  },
+  sideNav: {
+    linkActiveColor: activeColor
+  }
+}
---
+++ src/main/themes/interface.ts
@@ -0,0 +1,9 @@
+export interface Theme {
+  global: {
+    textColor: string
+    backgroundColor: string
+  }
+  sideNav: {
+    linkActiveColor: string
+  }
+}
