a6a0c630ca1979d5c8c58b25b348b513c4be5c27
Junyoung Choi <fluke8259@gmail.com>
2019-01-07 13:38:52 +0900

    Use open-color and replace styled-components with emotion

--- src/main/components/App.tsx
+++ src/main/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-import styled from 'styled-components'
+import styled from './styled'
 import SideNavigator from './SideNavigator'
 import NotePage from './NotePage'
 import AppStore from '../stores/AppStore'
 import GlobalStyle from './GlobalStyle'
+import { ThemeProvider } from 'emotion-theming'
+import { defaultTheme } from '../themes/default'
 
 const Root = styled.div`
   display: flex;
@@ -27,17 +29,19 @@ class App extends React.Component<AppProps> {
   render() {
     const app = this.props.app!
     return (
-      <Root>
-        {app.dataIsInitialized ? (
-          <>
-            <SideNavigator />
-            <NotePage />
-          </>
-        ) : (
-          <div>Loading data</div>
-        )}
-        <GlobalStyle />
-      </Root>
+      <ThemeProvider theme={defaultTheme}>
+        <Root>
+          {app.dataIsInitialized ? (
+            <>
+              <SideNavigator />
+              <NotePage />
+            </>
+          ) : (
+            <div>Loading data</div>
+          )}
+          <GlobalStyle />
+        </Root>
+      </ThemeProvider>
     )
   }
 }
