b897c6e41b63b58830616f4e28bca026a174f115
Junyoung Choi <fluke8259@gmail.com>
2019-01-24 11:33:05 +0900

    Implement temporary ContextMenuStore

---
+++ src/main/stores/ContextMenuStore.ts
@@ -0,0 +1,21 @@
+import { observable, action } from 'mobx'
+import { MenuItem } from '../components/ContextMenu/interfaces'
+
+export default class ContextMenuStore {
+  @observable isOpen: boolean = false
+  @observable xPosition: number = 0
+  @observable yPosition: number = 0
+  @observable menuItems: MenuItem[] = []
+
+  @action
+  open(event: React.MouseEvent<any>, menuItems: MenuItem[]) {
+    this.isOpen = true
+    console.log(event, menuItems)
+    // TODO: Adjust x and y position based on event and height of menu
+    // 1. Calculate menu height
+    // 2. Get x and y position of mouse cursor
+    // 3. Determine where to put menu
+    //   - If there is enough space, show menu at the cursor
+    //   - If there is not enough space, shrink menu and make it scrollable.
+  }
+}
