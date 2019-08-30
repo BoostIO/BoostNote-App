0ef8a0316ba7283528d870a42fb341b773b06d09
Junyoung Choi <rokt33r.choi@gmail.com>
2019-08-30 17:33:12 +0900

    Add more utils for db

---
+++ src/lib/db/utils.spec.ts
@@ -0,0 +1,113 @@
+import {
+  isFolderNameValid,
+  isFolderPathnameValid,
+  getFolderId,
+  getFolderPathname,
+  getTagId,
+  getTagName,
+  isTagNameValid
+} from './utils'
+
+describe('getFolderId', () => {
+  it('returns folder id from pathname', () => {
+    const result = getFolderId('/test')
+
+    expect(result).toBe('folder:/test')
+  })
+})
+
+describe('getFolderPathname', () => {
+  it('returns folder pathname from id', () => {
+    const result = getFolderPathname('folder:/test')
+
+    expect(result).toBe('/test')
+  })
+})
+
+describe('getTagId', () => {
+  it('returns tag id from name', () => {
+    const result = getTagId('test')
+
+    expect(result).toBe('tag:test')
+  })
+})
+
+describe('getTagname', () => {
+  it('returns tag name from id', () => {
+    const result = getTagName('tag:test')
+
+    expect(result).toBe('test')
+  })
+})
+
+describe('isFolderNameValid', () => {
+  it('returns false when invalid(including invalid characters)', () => {
+    const result = isFolderNameValid('/not/valid')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when invalid(empty)', () => {
+    const result = isFolderNameValid('')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns true when valid', () => {
+    const result = isFolderNameValid('valid')
+
+    expect(result).toBe(true)
+  })
+})
+
+describe('isFolderPathnameValid', () => {
+  it('returns false when invalid(empty)', () => {
+    const result = isFolderPathnameValid('')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when invalid(double slash)', () => {
+    const result = isFolderPathnameValid('/not//valid')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when invalid(invalid folder name)', () => {
+    const result = isFolderPathnameValid('/not?valid')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when invalid(relative pathname)', () => {
+    const result = isFolderPathnameValid('not/valid')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns true when valid', () => {
+    const result = isFolderPathnameValid('/valid')
+
+    expect(result).toBe(true)
+  })
+})
+
+describe('isTagNameValid', () => {
+  it('returns false when invalid(including invalid characters)', () => {
+    const result = isTagNameValid('not valid')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when invalid(empty)', () => {
+    const result = isTagNameValid('')
+
+    expect(result).toBe(false)
+  })
+
+  it('returns true when valid', () => {
+    const result = isTagNameValid('valid')
+
+    expect(result).toBe(true)
+  })
+})
--- src/lib/db/utils.ts
+++ src/lib/db/utils.ts
@@ -13,6 +13,30 @@ export function getFolderId(pathname: string): string {
   return `${FOLDER_ID_PREFIX}${pathname}`
 }
 
+export function getFolderPathname(id: string): string {
+  return id.substring(FOLDER_ID_PREFIX.length)
+}
+
 export function getTagId(name: string): string {
   return `${TAG_ID_PREFIX}${name}`
+}
+
+export function getTagName(id: string): string {
+  return id.substring(TAG_ID_PREFIX.length)
+}
+
+export function isFolderNameValid(name: string): boolean {
+  if (name.length === 0) return false
+  return !/[<>:"\/\\|?*\x00-\x1F]/g.test(name)
+}
+
+export function isFolderPathnameValid(pathname: string): boolean {
+  if (!pathname.startsWith('/')) return false
+  const [, ...folderNames] = pathname.split('/')
+  return folderNames.every(isFolderNameValid)
+}
+
+export function isTagNameValid(name: string): boolean {
+  if (name.length === 0) return false
+  return !/[\s#<>:"\/\\|?*\x00-\x1F]/g.test(name)
 }
