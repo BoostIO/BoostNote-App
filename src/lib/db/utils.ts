44402969aeb4041bc397cded0b171ca1fa9bbc93
Junyoung Choi <rokt33r.choi@gmail.com>
2019-08-30 18:19:13 +0900

    Move error apis  for db to utils

--- src/lib/db/utils.ts
+++ src/lib/db/utils.ts
@@ -40,3 +40,45 @@ export function isTagNameValid(name: string): boolean {
   if (name.length === 0) return false
   return !/[\s#<>:"\/\\|?*\x00-\x1F]/g.test(name)
 }
+
+enum DbClientErrorCode {
+  UnprocessableEntity = 'UnprocessableEntity',
+  Conflict = 'Conflict',
+  NotFound = 'NotFound'
+}
+
+export class DbClientError extends Error {
+  readonly name: string = 'DbClientError'
+  code: DbClientErrorCode
+  statusCode: number
+  constructor(message: string, code: DbClientErrorCode) {
+    super(message)
+    this.code = code
+    this.statusCode = getStatusCodeFromCode(code)
+  }
+}
+
+export function getStatusCodeFromCode(code: string) {
+  switch (code) {
+    case DbClientErrorCode.NotFound:
+      return 404
+    case DbClientErrorCode.Conflict:
+      return 409
+    case DbClientErrorCode.UnprocessableEntity:
+      return 422
+  }
+
+  return 500
+}
+
+export function createUnprocessableEntityError(message: string) {
+  return new DbClientError(message, DbClientErrorCode.UnprocessableEntity)
+}
+
+export function createNotFoundError(message: string) {
+  return new DbClientError(message, DbClientErrorCode.NotFound)
+}
+
+export function createConflictError(message: string) {
+  return new DbClientError(message, DbClientErrorCode.Conflict)
+}
