5e2aa82f99e51ec0011d6e4f5b27f9c5a434994b
Junyoung Choi <rokt33r.choi@gmail.com>
2019-09-24 10:25:53 +0900

    Move CodeMirror type definitions

--- src/lib/CodeMirror.ts
+++ src/lib/CodeMirror.ts
@@ -4,36 +4,6 @@ import 'codemirror/addon/mode/overlay'
 import 'codemirror/mode/markdown/markdown'
 import debounce from 'lodash/debounce'
 
-window.CodeMirror = CodeMirror
-
-declare module 'codemirror' {
-  function autoLoadMode(instance: CodeMirror.Editor, mode: string): void
-
-  interface ModeInfo {
-    name: string
-    mime?: string
-    mimes?: string[]
-    mode: string
-    ext: string[]
-    alias?: string[]
-  }
-  const modeInfo: ModeInfo[]
-
-  interface Editor {
-    options: CodeMirror.EditorConfiguration
-  }
-
-  function findModeByMIME(mime: string): ModeInfo | undefined
-  function findModeByName(name: string): ModeInfo | undefined
-
-  function runMode(
-    text: string,
-    modespec: any,
-    callback: HTMLElement | ((text: string, style: string | null) => void),
-    options?: { tabSize?: number; state?: any }
-  ): void
-}
-
 const dispatchModeLoad = debounce(() => {
   window.dispatchEvent(new CustomEvent('codemirror-mode-load'))
 }, 300)
--- typings/codemirror.d.ts
+++ typings/codemirror.d.ts
@@ -1,3 +1,31 @@
-declare module 'codemirror/mode/*' {
+import CodeMirror from 'codemirror'
 
+declare module 'codemirror/mode/*'
+
+declare module 'codemirror' {
+  function autoLoadMode(instance: CodeMirror.Editor, mode: string): void
+
+  interface ModeInfo {
+    name: string
+    mime?: string
+    mimes?: string[]
+    mode: string
+    ext: string[]
+    alias?: string[]
+  }
+  const modeInfo: ModeInfo[]
+
+  interface Editor {
+    options: CodeMirror.EditorConfiguration
+  }
+
+  function findModeByMIME(mime: string): ModeInfo | undefined
+  function findModeByName(name: string): ModeInfo | undefined
+
+  function runMode(
+    text: string,
+    modespec: any,
+    callback: HTMLElement | ((text: string, style: string | null) => void),
+    options?: { tabSize?: number; state?: any }
+  ): void
 }
