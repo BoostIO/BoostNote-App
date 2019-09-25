c8d1543de803d152b77e9f14d26b184c333bc867
Junyoung Choi <rokt33r.choi@gmail.com>
2019-09-25 17:52:03 +0900

    Fix SideNavigation's overflow style

--- src/components/SideNavigator/SideNavigator.tsx
+++ src/components/SideNavigator/SideNavigator.tsx
@@ -2,9 +2,27 @@ import React, { useMemo } from 'react'
 import StorageItem from './StorageItem/StorageItem'
 import { useRouter } from '../../lib/router'
 import SotrageCreateForm from './StorageCreateForm'
-import { StyledSideNavContainer, StyledStorageList } from './styled'
 import { useDb } from '../../lib/db'
 import { entries } from '../../lib/db/utils'
+import styled from '../../lib/styled'
+import Toolbar from '../atoms/Toolbar'
+import ToolbarSeparator from '../atoms/ToolbarSeparator'
+import ToolbarIconButton from '../atoms/ToolbarIconButton'
+import { mdiSettings } from '@mdi/js'
+
+const StyledStorageList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  flex: 1;
+  overflow: auto;
+`
+
+const StyledSideNavContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  border-right: solid 1px ${({ theme }) => theme.colors.border};
+`
 
 export default () => {
   const {
@@ -23,6 +41,10 @@ export default () => {
 
   return (
     <StyledSideNavContainer style={{ width: 160 }}>
+      <Toolbar>
+        <ToolbarSeparator />
+        <ToolbarIconButton path={mdiSettings} onClick={() => {}} />
+      </Toolbar>
       <StyledStorageList>
         {storageEntries.map(([id, storage]) => {
           const pathname = router.pathname
