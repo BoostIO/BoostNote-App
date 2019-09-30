d4e42156f1ddc2d75347765f99868b922093e009
Junyoung Choi <rokt33r.choi@gmail.com>
2019-09-30 13:09:52 +0900

    Add ToolbarIconInput for search input

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -3,10 +3,11 @@ import MdiIcon from '@mdi/react'
 
 interface IconProps {
   path: string
+  className?: string
 }
 
-const Icon = ({ path }: IconProps) => (
-  <MdiIcon path={path} size='1em' color='currentColor' />
+const Icon = ({ path, className }: IconProps) => (
+  <MdiIcon path={path} className={className} size='1em' color='currentColor' />
 )
 
 export default Icon
