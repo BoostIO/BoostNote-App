218b84c9859faee6015f705c73a18a3a15aedc83
Junyoung Choi <fluke8259@gmail.com>
2019-11-01 13:30:58 +0900

    Use preferences store in GeneralTab

--- src/components/PreferencesModal/GeneralTab.tsx
+++ src/components/PreferencesModal/GeneralTab.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { ChangeEventHandler } from 'react'
 import Icon from '../atoms/Icon'
 import { mdiPlus } from '@mdi/js'
 import { Section, SectionHeader, SectionControl } from './styled'
+import {
+  usePreferences,
+  GeneralThemeOptions,
+  GeneralLanguageOptions,
+  GeneralNoteSortingOptions
+} from '../../lib/preferences'
+
+type SelectChangeEventHandler = ChangeEventHandler<HTMLSelectElement>
 
 const GeneralTab = () => {
+  const { preferences, setPreferences } = usePreferences()
+
+  const selectTheme: SelectChangeEventHandler = event => {
+    setPreferences({
+      'general.theme': event.target.value as GeneralThemeOptions
+    })
+  }
+
+  const selectLanguage: SelectChangeEventHandler = event => {
+    setPreferences({
+      'general.language': event.target.value as GeneralLanguageOptions
+    })
+  }
+
+  const selectNoteSorting: SelectChangeEventHandler = event => {
+    setPreferences({
+      'general.noteSorting': event.target.value as GeneralNoteSortingOptions
+    })
+  }
+
+  const setEnableAnalytics: ChangeEventHandler<HTMLInputElement> = event => {
+    setPreferences({
+      'general.enableAnalytics': event.target.checked
+    })
+  }
+
   return (
     <div>
       <Section>
-        <SectionHeader>Accounts</SectionHeader>
+        <SectionHeader>Account</SectionHeader>
         <div>
           <ul>
             <li>Account list</li>
@@ -21,30 +55,57 @@ const GeneralTab = () => {
       <Section>
         <SectionHeader>Interface Language</SectionHeader>
         <SectionControl>
-          <select>
-            <option>English</option>
-            <option>Japanese</option>
+          <select
+            value={preferences['general.language']}
+            onChange={selectLanguage}
+          >
+            <option value='en-US'>English (US)</option>
+            <option value='ja'>Japanese</option>
           </select>
         </SectionControl>
       </Section>
       <Section>
         <SectionHeader>Application Theme</SectionHeader>
         <SectionControl>
-          <select>
-            <option>Light</option>
-            <option>Dark</option>
-            <option>Solarized Dark</option>
+          <select value={preferences['general.theme']} onChange={selectTheme}>
+            <option value='auto'>Auto</option>
+            <option value='light'>Light</option>
+            <option value='dark'>Dark</option>
+            <option value='solarized-dark'>Solarized Dark</option>
           </select>
         </SectionControl>
       </Section>
       <Section>
         <SectionHeader>Note Sorting</SectionHeader>
         <SectionControl>
-          <select>
-            <option>Date Updated</option>
-            <option>Date Created</option>
-            <option>Title</option>
+          <select
+            value={preferences['general.noteSorting']}
+            onChange={selectNoteSorting}
+          >
+            <option value='date-updated'>Date Updated</option>
+            <option value='date-created'>Date Created</option>
+            <option value='title'>Title</option>
           </select>
+        </SectionControl>
+      </Section>
+      <Section>
+        <SectionHeader>Analytics</SectionHeader>
+        <SectionControl>
+          <p>
+            Boost Note collects anonymous data for the sole purpose of improving
+            the application, and strictly does not collect any personal
+            information such the contents of your notes. You can see how it
+            works on GitHub.
+          </p>
+          <p>You can choose to enable or disable this option.</p>
+          <label>
+            <input
+              type='checkbox'
+              checked={preferences['general.enableAnalytics']}
+              onChange={setEnableAnalytics}
+            />
+            Enable analytics to help improve Boostnote
+          </label>
         </SectionControl>
       </Section>
     </div>
