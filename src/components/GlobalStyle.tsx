1a76426f5d7c67b2f7c2f368e8939d3ec7a0ceec
Junyoung Choi <rokt33r.choi@gmail.com>
2019-11-18 00:16:19 +0900

    Style scrollbar

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -3,8 +3,30 @@ import { createGlobalStyle } from 'styled-components'
 export default createGlobalStyle`
   body {
     margin: 0;
+    
   }
   a {
     color: inherit;
+  }
+  
+  /* total width */
+  ::-webkit-scrollbar {
+    background-color: transparent;
+    width:12px;
+  }
+
+  /* background of the scrollbar except button or resizer */
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  /* scrollbar itself */
+  ::-webkit-scrollbar-thumb {
+    background-color: #babac0;
+  }
+
+  /* set button(top and bottom of the scrollbar) */
+  ::-webkit-scrollbar-button {
+    display: none
   }
 `
