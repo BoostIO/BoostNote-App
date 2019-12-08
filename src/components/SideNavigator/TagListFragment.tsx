debd4189aecedde5d67fc3b7257c0ddf5c1d7fb3
Junyoung Choi <rokt33r.choi@gmail.com>
2019-12-09 01:55:19 +0900

    Make tag list fragments foldable

--- src/components/SideNavigator/TagListFragment.tsx
+++ src/components/SideNavigator/TagListFragment.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
   mdiTagMultiple,
   // mdiTag,
@@ -6,26 +6,45 @@ import {
 } from '@mdi/js'
 import SideNavigatorItem from './SideNavigatorItem'
 import { NoteStorage } from '../../lib/db/types'
+import { useGeneralStatus } from '../../lib/generalStatus'
+import { getTagListItemId } from '../../lib/nav'
 
 interface TagListFragmentProps {
   storage: NoteStorage
 }
 
 const TagListFragment = ({ storage }: TagListFragmentProps) => {
-  const tagList = Object.keys(storage.tagMap).map(tagName => {
-    return (
-      <SideNavigatorItem
-        key={`storage:${storage.id}/tags:${tagName}`}
-        depth={2}
-        iconPath={mdiTagOutline}
-        label={tagName}
-      />
-    )
-  })
+  const { toggleSideNavOpenedItem, sideNavOpenedItemSet } = useGeneralStatus()
+  const { id: storageId, tagMap } = storage
+
+  const tagListNavItemId = getTagListItemId(storage.id)
+  const tagListIsFolded = !sideNavOpenedItemSet.has(tagListNavItemId)
+
+  const tagList = useMemo(() => {
+    return Object.keys(tagMap).map(tagName => {
+      return (
+        <SideNavigatorItem
+          key={`storage:${storageId}/tags:${tagName}`}
+          depth={2}
+          iconPath={mdiTagOutline}
+          label={tagName}
+        />
+      )
+    })
+  }, [storageId, tagMap])
+
   return (
     <>
-      <SideNavigatorItem depth={1} iconPath={mdiTagMultiple} label='Tags' />
-      {tagList}
+      <SideNavigatorItem
+        depth={1}
+        iconPath={mdiTagMultiple}
+        label='Tags'
+        folded={tagListIsFolded}
+        onFoldButtonClick={() => {
+          toggleSideNavOpenedItem(tagListNavItemId)
+        }}
+      />
+      {!tagListIsFolded && tagList}
     </>
   )
 }
