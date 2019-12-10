4dca85269f6cbb0c36acc8507fd48293a1a423df
davy-c <d.castel33@gmail.com>
2019-12-10 13:10:57 +0900

    redirect to create storage page

--- src/lib/router/redirect.ts
+++ src/lib/router/redirect.ts
@@ -1,24 +1,18 @@
 import { useDb } from '../db'
 import { useRouter } from './store'
 import { useEffect } from 'react'
-import { usePreferences } from '../preferences'
 
 export default function useRedirectHandler() {
   const db = useDb()
-  const { preferences } = usePreferences()
   const { push, pathname } = useRouter()
 
   useEffect(() => {
     if (!db.initialized || db.storageMap == null) {
       return
     }
-    if (
-      pathname === '/app' &&
-      preferences['general.tutorials'] === 'display' &&
-      Object.keys(db.storageMap).length === 0
-    ) {
-      push('/app/tutorials/welcome-pack/guides/notes/note:storage-guide')
+    if (pathname === '/app' && Object.keys(db.storageMap).length === 0) {
+      push('/app/storages')
     }
     return
-  }, [pathname, db.initialized, preferences['general.displayTutorials']])
+  }, [pathname, db.initialized])
 }
