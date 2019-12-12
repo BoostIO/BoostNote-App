1568b487c172dce08b591bae8a135bd18952ac32
davy-c <d.castel33@gmail.com>
2019-12-12 15:48:17 +0900

    redirect to first storage on /app

--- src/lib/router/redirect.ts
+++ src/lib/router/redirect.ts
@@ -1,14 +1,22 @@
 import { useDb } from '../db'
 import { useRouter } from './store'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
+import { entries } from '../db/utils'
 
 export default function useRedirectHandler() {
   const db = useDb()
   const { push, pathname } = useRouter()
 
+  const storageEntries = useMemo(() => {
+    return entries(db.storageMap)
+  }, [db.storageMap])
+
   useEffect(() => {
-    if (pathname === '/app' && Object.keys(db.storageMap).length === 0) {
-      push('/app/storages')
-    }
+    if (pathname === '/app')
+      if (storageEntries.length === 0) {
+        push('/app/storages')
+      } else {
+        push(`/app/storages/${storageEntries[0][1].id}`)
+      }
   }, [pathname, db.storageMap, push])
 }
