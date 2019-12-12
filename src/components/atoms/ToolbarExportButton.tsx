e8b424a65aa9a71389fa2bb9bc64c64f49817eaf
davy-c <d.castel33@gmail.com>
2019-12-12 17:34:08 +0900

    export button fix

--- src/components/atoms/ToolbarExportButton.tsx
+++ src/components/atoms/ToolbarExportButton.tsx
@@ -3,24 +3,11 @@ import styled from '../../lib/styled'
 import { iconColor } from '../../lib/styled/styleFunctions'
 import { useContextMenu, MenuTypes } from '../../lib/contextMenu'
 import { NoteDoc } from '../../lib/db/types'
-import unified from 'unified'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import remarkStringify from 'remark-stringify'
-import rehypeDocument from 'rehype-document'
-import rehypeRaw from 'rehype-raw'
-import rehypeSanitize from 'rehype-sanitize'
-import rehypeStringify from 'rehype-stringify'
-import { mergeDeepRight } from 'ramda'
-import gh from 'hast-util-sanitize/lib/github.json'
+import { mdiFileExport } from '@mdi/js'
+import Icon from './Icon'
+import { exportToHtml, exportToMarkdown } from '../../lib/exports'
 import { usePreferences } from '../../lib/preferences'
-import { rehypeCodeMirror } from './MarkdownPreviewer'
 import { usePreviewStyle } from '../../lib/preview'
-import { downloadString } from '../../lib/download'
-
-const sanitizeSchema = mergeDeepRight(gh, {
-  attributes: { '*': ['className'] }
-})
 
 const StyledButton = styled.button<{ active: boolean }>`
   background: transparent;
@@ -55,7 +42,8 @@ const ToolbarExportButton = ({ className, note }: ToolbarExportButtonProps) => {
         {
           type: MenuTypes.Normal,
           label: 'HTML export',
-          onClick: async () => await exportToHtml(note)
+          onClick: async () =>
+            await exportToHtml(note, preferences, previewStyle)
         },
         {
           type: MenuTypes.Normal,
@@ -64,67 +52,8 @@ const ToolbarExportButton = ({ className, note }: ToolbarExportButtonProps) => {
         }
       ])
     },
-    [popup, note]
+    [popup, note, preferences, previewStyle]
   )
-
-  const exportToHtml = async (note: NoteDoc) => {
-    await unified()
-      .use(remarkParse)
-      .use(remarkRehype, { allowDangerousHTML: false })
-      .use(rehypeCodeMirror, {
-        ignoreMissing: true,
-        theme: preferences['markdown.codeBlockTheme']
-      })
-      .use(rehypeRaw)
-      .use(rehypeSanitize, sanitizeSchema)
-      .use(rehypeDocument, {
-        title: note.title,
-        style: previewStyle,
-        meta: { keywords: note.tags.join() }
-      })
-      .use(rehypeStringify)
-      .process(note.content, (err, file) => {
-        if (err != null) {
-          /* TODO: Toast error */
-          console.error(err)
-          return
-        }
-
-        downloadString(
-          file.toString(),
-          `${note.title.toLowerCase().replace(/\s+/g, '-')}.html`,
-          'text/html'
-        )
-        return
-      })
-  }
-
-  const exportToMarkdown = async (note: NoteDoc) => {
-    console.log('export markdown')
-    await unified()
-      .use(remarkParse)
-      .use(remarkStringify)
-      .process(note.content, (err, file) => {
-        if (err != null) {
-          /* TODO: Toast error */
-          console.error(err)
-          return
-        }
-        downloadString(
-          [
-            '---',
-            `title: "${note.title}"`,
-            `tags: "${note.tags.join()}"`,
-            '---',
-            file.toString()
-          ].join('\n'),
-          `${note.title.toLowerCase().replace(/\s+/g, '-')}.md`,
-          'text/markdown'
-        )
-        return
-      })
-    return
-  }
 
   return (
     <StyledButton
@@ -132,7 +61,7 @@ const ToolbarExportButton = ({ className, note }: ToolbarExportButtonProps) => {
       onClick={openExportButtonContextMenu}
       className={className}
     >
-      <span>Export</span>
+      <Icon path={mdiFileExport} />
     </StyledButton>
   )
 }
