91e8fb87d274daf9a93366033b35bf9b362869fb
davy-c <d.castel33@gmail.com>
2019-12-12 14:25:32 +0900

    search notes with highlight

--- src/components/NotePage/NoteList/NoteItem.tsx
+++ src/components/NotePage/NoteList/NoteItem.tsx
@@ -70,27 +70,74 @@ export const StyledNoteListItem = styled.div`
     border-radius: 13px;
     display: inline-block;
   }
+
+  .highlighted {
+    background: rgba(217, 211, 46, 0.6);
+  }
 `
 
 type NoteItemProps = {
   note: NoteDoc
   active: boolean
   storageId: string
+  search: string
   basePathname: string
   focusList: () => void
 }
 
-export default ({ storageId, note, active, basePathname }: NoteItemProps) => {
+export default ({
+  storageId,
+  note,
+  active,
+  basePathname,
+  search
+}: NoteItemProps) => {
   const href = `${basePathname}/${note._id}`
 
+  const getHighlightedText = useCallback(
+    (text: string) => {
+      if (search === '') return text
+      const searchRegex = new RegExp(`(${search})`, 'gi')
+      const parts = text.split(searchRegex)
+      return (
+        <span>
+          {parts.map((part, i) =>
+            part.toLowerCase() === search.toLowerCase() ? (
+              <span key={i} className='highlighted'>
+                {part}
+              </span>
+            ) : (
+              <React.Fragment key={i}>{part}</React.Fragment>
+            )
+          )}
+        </span>
+      )
+    },
+    [search]
+  )
+
   const contentPreview = useMemo(() => {
+    const searchFirstIndex = note.content
+      .trim()
+      .toLowerCase()
+      .indexOf(search.toLowerCase())
+    if (search !== '' && searchFirstIndex !== -1) {
+      return getHighlightedText(
+        note.content
+          .trim()
+          .substring(searchFirstIndex)
+          .split('\n')
+          .shift() || 'Empty note'
+      )
+    }
+
     return (
       note.content
         .trim()
         .split('\n')
         .shift() || 'Empty note'
     )
-  }, [note.content])
+  }, [note.content, search])
 
   const handleDragStart = useCallback(
     (event: React.DragEvent) => {
@@ -107,16 +154,14 @@ export default ({ storageId, note, active, basePathname }: NoteItemProps) => {
     >
       <Link href={href}>
         <div className='container'>
-          <div className='title'>{note.title}</div>
+          <div className='title'>{getHighlightedText(note.title)}</div>
           {note.title.length === 0 && <div className='title'>No title</div>}
           <div className='date'>DD days ago</div>
           <div className='preview'>{contentPreview}</div>
           {note.tags.length > 0 && (
             <div className='tag-area'>
               {note.tags.map(tag => (
-                <span className='tag' key={tag}>
-                  {tag}
-                </span>
+                <span key={tag}>{getHighlightedText(tag)}</span>
               ))}
             </div>
           )}
