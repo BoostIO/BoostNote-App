0cb195457fdc9d04154761a9747925dd6695a56e
Junyoung Choi <fluke8259@gmail.com>
2019-12-12 09:12:52 +0900

    Improve updater behavior
    - Update can be postponed
    - Background update check
      - 10 secs after start up
      - Everyday

--- app/updater.js
+++ app/updater.js
@@ -1,20 +1,15 @@
-/**
- * updater.js
- *
- * Please use manual update only when it is really required, otherwise please use recommended non-intrusive auto update.
- *
- * Import steps:
- * 1. create `updater.js` for the code snippet
- * 2. require `updater.js` for menu implementation, and set `checkForUpdates` callback from `updater` for the click property of `Check Updates...` MenuItem.
- */
-const { dialog } = require('electron')
+const { dialog, Notification } = require('electron')
 const { autoUpdater } = require('electron-updater')
 const logger = require('electron-log')
+
 autoUpdater.logger = logger
 autoUpdater.logger.transports.file.level = 'info'
 
 let updater
 autoUpdater.autoDownload = false
+
+let updating = false
+let foundUpdates = false
 
 autoUpdater.on('error', error => {
   dialog.showErrorBox(
@@ -24,47 +19,92 @@ autoUpdater.on('error', error => {
 })
 
 autoUpdater.on('update-available', () => {
-  dialog
-    .showMessageBox({
-      type: 'info',
-      title: 'Found Updates',
-      message: 'Found updates, do you want update now?',
-      buttons: ['Sure', 'No']
+  foundUpdates = true
+  if (updater == null) {
+    const notification = new Notification({
+      title: 'Found Updates!',
+      body: 'Click here to update'
     })
-    .then(({ response }) => {
-      if (response === 0) {
-        autoUpdater.downloadUpdate()
-      } else {
-        updater.enabled = true
-        updater = null
-      }
+    notification.addListener('click', () => {
+      dialog
+        .showMessageBox({
+          type: 'info',
+          title: 'Found Updates',
+          message: 'Found updates, do you want update now?',
+          buttons: ['Sure', 'No']
+        })
+        .then(({ response }) => {
+          if (response === 0) {
+            autoUpdater.downloadUpdate()
+          }
+        })
     })
+    notification.show()
+  } else {
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Found Updates',
+        message: 'Found updates, do you want update now?',
+        buttons: ['Sure', 'No']
+      })
+      .then(({ response }) => {
+        if (response === 0) {
+          autoUpdater.downloadUpdate()
+        } else {
+          updater.enabled = true
+          updater = null
+        }
+      })
+  }
 })
 
 autoUpdater.on('update-not-available', () => {
-  dialog.showMessageBox({
-    title: 'No Updates',
-    message: 'Current version is up-to-date.'
-  })
-  updater.enabled = true
-  updater = null
+  if (updater != null) {
+    dialog.showMessageBox({
+      title: 'No Updates',
+      message: 'Current version is up-to-date.'
+    })
+    updater.enabled = true
+    updater = null
+  }
 })
 
 autoUpdater.on('update-downloaded', () => {
   dialog
     .showMessageBox({
-      title: 'Install Updates',
-      message: 'Updates downloaded, application will be quit for update...'
+      title: 'Updates downloaded',
+      message: 'To install the update, the app must be restarted.',
+      buttons: ['Restart and Install', 'Not Yet']
     })
-    .then(() => {
-      setImmediate(() => autoUpdater.quitAndInstall())
+    .then(({ response }) => {
+      if (response === 0) {
+        setImmediate(() => autoUpdater.quitAndInstall())
+      } else {
+        if (updater != null) {
+          updater.enabled = true
+          updater = null
+        }
+      }
     })
 })
 
-// export this to MenuItem click callback
 function checkForUpdates(menuItem, focusedWindow, event) {
   updater = menuItem
   updater.enabled = false
   autoUpdater.checkForUpdates()
 }
+
+setTimeout(() => {
+  if (updater == null) {
+    autoUpdater.checkForUpdates()
+  }
+}, 10 * 1000) // After 10 secs
+
+setInterval(() => {
+  if (!foundUpdates && updater == null) {
+    autoUpdater.checkForUpdates()
+  }
+}, 24 * 3600 * 1000) // Everyday
+
 module.exports.checkForUpdates = checkForUpdates
