fa37dd737a00e07aeb73c0205c59c1ab22202a70
Elle Kasai <elle.kasai@gmail.com>
2019-12-12 20:15:47 +0900

    Adjusted Width of the Input & Select

--- src/components/PreferencesModal/styled.tsx
+++ src/components/PreferencesModal/styled.tsx
@@ -36,6 +36,7 @@ export const SectionControl = styled.div`
 export const SectionSelect = styled.select`
   ${selectStyle}
   padding: 0 16px;
+  width: 200px;
   height: 40px;
   border-radius: 2px;
 
@@ -66,6 +67,7 @@ export const SectionSecondaryButton = styled.button`
 export const SectionInput = styled.input`
   ${inputStyle}
   padding: 0 16px;
+  width: 200px;
   height: 40px;
   border-radius: 2px;
 `
