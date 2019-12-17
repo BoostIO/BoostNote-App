2829ad453790a2c1e87dbee42a28cc647358ff55
Simon Leigh <sleigh.dev@gmail.com>
2019-12-17 11:55:14 +0900

    added new api endpoints

--- src/lib/accounts/api/storage.ts
+++ src/lib/accounts/api/storage.ts
@@ -1,4 +1,4 @@
-import { apiPost, apiGet } from './utils'
+import { apiPost, apiGet, apiDelete } from './utils'
 
 export interface CloudStorage {
   id: number
@@ -35,6 +35,50 @@ export const getStorages = async ({ token }: User): Promise<GetResponse> => {
   switch (response.status) {
     case 200:
       return response.json()
+    case 401:
+      throw new Error('InvalidUser')
+    default:
+      throw new Error('NetworkError')
+  }
+}
+
+export const renameStorage = async (
+  { token }: User,
+  id: number,
+  name: string
+): Promise<'Ok'> => {
+  const response = await apiPost(`/api/storages/${id}`, {
+    token,
+    body: { name }
+  })
+
+  switch (response.status) {
+    case 200:
+      return 'Ok'
+    case 401:
+      throw new Error('InvalidUser')
+    case 404:
+      throw new Error('NotFound')
+    default:
+      throw new Error('NetworkError')
+  }
+}
+
+export const deleteStorage = async (
+  { token }: User,
+  id: number
+): Promise<'Ok'> => {
+  const response = await apiDelete(`/api/storages/${id}`, {
+    token
+  })
+
+  switch (response.status) {
+    case 200:
+      return 'Ok'
+    case 401:
+      throw new Error('InvalidUser')
+    case 404:
+      throw new Error('NotFound')
     default:
       throw new Error('NetworkError')
   }
--- src/lib/accounts/users.ts
+++ src/lib/accounts/users.ts
@@ -1,7 +1,6 @@
 import { usePreferences } from '../preferences'
-import { CloudStorage, getStorages } from './api/storage'
-import { Subscription, getSubscription } from './api/subscription'
-import { useState, useEffect } from 'react'
+import { CloudStorage } from './api/storage'
+import { Subscription } from './api/subscription'
 
 export interface User {
   token: string
@@ -36,49 +35,6 @@ export const useUsers = (): [User[], UserRepo] => {
   }
 
   return [users, repo]
-}
-
-const cache: { [key: number]: UserCloudInfo } = {}
-export const useUserCloudInfo = (
-  user: User
-): [UserCloudInfo, () => void, boolean] => {
-  const [info, setInfo] = useState<UserCloudInfo>(() => {
-    return cache[user.id] != undefined
-      ? cache[user.id]
-      : { storages: [] as CloudStorage[], usage: 1, subscription: undefined }
-  })
-  const [forceFlag, setForceFlag] = useState(true)
-  const [running, setRunning] = useState(false)
-
-  useEffect(() => {
-    let subscribed = true
-    setRunning(true)
-    getUserCloudInfo(user).then(info => {
-      cache[user.id] = info
-      setRunning(false)
-      if (subscribed) {
-        setInfo(info)
-      }
-    })
-    return () => {
-      subscribed = false
-    }
-  }, [user, forceFlag])
-
-  return [info, () => setForceFlag(!forceFlag), running]
-}
-
-const getUserCloudInfo = async (user: User): Promise<UserCloudInfo> => {
-  const [storages, subscription] = await Promise.all([
-    getStorages(user),
-    getSubscription(user)
-  ])
-
-  return {
-    storages,
-    subscription,
-    usage: storages.reduce((sum, storage) => sum + storage.size, 0)
-  }
 }
 
 const removeUser = (user: User, users: User[]) => {
