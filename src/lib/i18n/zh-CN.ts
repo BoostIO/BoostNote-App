db721a9c8c1a5a3ca4c841e2e638e294597fac71
duansheli <oxvague@gmail.com>
2019-12-24 17:35:25 +0800

    Create zh-CN.ts
    https://github.com/BoostIO/BoostNote.next/issues/208

---
+++ src/lib/i18n/zh-CN.ts
@@ -0,0 +1,41 @@
+export default {
+  translation: {
+    // Preferences GeneralTab
+    'preferences.account': '账户',
+    'preferences.addAccount': '注册',
+    'preferences.loginWorking': '登录',
+    'preferences.interfaceLanguage': '界面语言',
+    'preferences.applicationTheme': '程序主题',
+    'preferences.auto': 'auto',
+    'preferences.light': 'light',
+    'preferences.dark': 'dark',
+    'preferences.sepia': 'Sepia',
+    'preferences.solarizedDark': 'Solarized Dark',
+    'preferences.noteSorting': '笔记排序',
+    'preferences.dateUpdated': '更新日期',
+    'preferences.dateCreated': '更新日期',
+    'preferences.title': '标题',
+    'preferences.analytics': '分析',
+    'preferences.analyticsDescription1':
+      'Boost Note收集匿名数据的唯一目的是改进应用程序，并且绝对不收集任何个人信息，比如您的笔记内容。您可以看到它在GitHub上是如何工作的。',
+    'preferences.analyticsDescription2':
+      '您可以启用或关闭这个选项',
+    'preferences.analyticsLabel': '启用分析以帮助Boostnote',
+    'preferences.displayTutorialsLabel': '教程和常见问题',
+
+    // Preferences EditorTab
+    'preferences.editorTheme': '编辑区主题',
+    'preferences.editorFontSize': '编辑区字体大小',
+    'preferences.editorFontFamily': '编辑区 Font Family',
+    'preferences.editorIndentType': '编辑区缩进类型',
+    'preferences.tab': '制表符',
+    'preferences.spaces': '空格',
+    'preferences.editorIndentSize': '缩进长度',
+    'preferences.editorKeymap': '编辑区快捷键',
+    'preferences.editorPreview': '编辑区预览',
+
+    // Preferences MarkdownTab
+    'preferences.previewStyle': '预览样式',
+    'preferences.markdownCodeBlockTheme': '代码块主题'
+  }
+}
