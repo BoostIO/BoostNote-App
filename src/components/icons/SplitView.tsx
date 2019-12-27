03bc259986940083bd55ccd5a88ff31d5f602d3b
Elle Kasai <elle.kasai@gmail.com>
2019-12-27 19:01:17 +0900

    Added TSX for Changing View Icons

---
+++ src/components/icons/SplitView.tsx
@@ -0,0 +1,31 @@
+import {
+  BoostnoteIconProps,
+  BoostnoteIconStyledContainer
+} from '../../lib/icons'
+import React from 'react'
+
+export const IconSplitView = (props: BoostnoteIconProps) => (
+  <BoostnoteIconStyledContainer>
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 72 72'
+      {...props}
+      style={
+        props.size != null
+          ? { ...props.style, width: props.size, height: props.size }
+          : props.style
+      }
+    >
+      <g fill='none' fillRule='evenodd'>
+        <path
+          stroke='#2C3149'
+          strokeWidth={4}
+          strokeLinejoin='round'
+          d='M9 16h54v40H9z'
+        />
+        <path fill='currentColor' d='M35 18h4v36h-4z' />
+      </g>
+    </svg>
+  </BoostnoteIconStyledContainer>
+)
