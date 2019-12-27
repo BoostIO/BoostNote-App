b529a141799f61d750dc72a94bc017948db1dcf5
Elle Kasai <elle.kasai@gmail.com>
2019-12-27 19:01:17 +0900

    Added TSX for Changing View Icons

---
+++ src/components/icons/EditView.tsx
@@ -0,0 +1,35 @@
+import {
+  BoostnoteIconProps,
+  BoostnoteIconStyledContainer
+} from '../../lib/icons'
+import React from 'react'
+
+export const IconEditView = (props: BoostnoteIconProps) => (
+  <BoostnoteIconStyledContainer>
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 72 72'
+      {...props}
+      style={
+        props.size != null
+          ? { ...props.style, width: props.size, height: props.size }
+          : props.style
+      }
+    >
+      <g fill='none' fillRule='evenodd'>
+        <path
+          stroke='#2C3149'
+          strokeWidth={4}
+          strokeLinejoin='round'
+          d='M9 16h54v40H9z'
+        />
+        <path
+          d='M28.996 41.89h-1.643c-1.279 0-2.042-.713-2.042-1.842 0-1.411.93-2.324 2.49-2.324h2.025l.714-3.67h-1.693c-1.262 0-2.042-.746-2.042-1.859 0-1.427.946-2.34 2.507-2.34h2.042l.846-4.118c.266-1.41 1.063-2.075 2.39-2.075 1.23 0 1.96.714 1.96 1.81 0 .215-.033.53-.083.747l-.714 3.635h3.719l.83-4.117c.265-1.41 1.062-2.075 2.39-2.075 1.229 0 1.943.714 1.943 1.81 0 .215-.017.53-.067.747l-.73 3.635h1.71c1.278 0 2.042.714 2.042 1.843 0 1.411-.93 2.324-2.49 2.324h-2.092l-.714 3.67h1.743c1.262 0 2.042.73 2.042 1.859 0 1.428-.946 2.34-2.507 2.34H41.48l-.896 4.417c-.266 1.41-1.063 2.075-2.39 2.075-1.23 0-1.943-.714-1.943-1.81 0-.216.017-.531.066-.747l.797-3.934h-3.752l-.88 4.432c-.265 1.395-1.062 2.059-2.39 2.059-1.229 0-1.943-.714-1.943-1.81 0-.216.017-.531.067-.747l.78-3.934zm4.748-3.751h4.416l.93-4.55h-4.433l-.913 4.55z'
+          fill='currentColor'
+          fillRule='nonzero'
+        />
+      </g>
+    </svg>
+  </BoostnoteIconStyledContainer>
+)
---
+++ src/components/icons/Preview.tsx
@@ -0,0 +1,34 @@
+import {
+  BoostnoteIconProps,
+  BoostnoteIconStyledContainer
+} from '../../lib/icons'
+import React from 'react'
+
+export const IconPreview = (props: BoostnoteIconProps) => (
+  <BoostnoteIconStyledContainer>
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 72 72'
+      {...props}
+      style={
+        props.size != null
+          ? { ...props.style, width: props.size, height: props.size }
+          : props.style
+      }
+    >
+      <g fill='none' fillRule='evenodd'>
+        <path
+          stroke='#2C3149'
+          strokeWidth={4}
+          strokeLinejoin='round'
+          d='M9 16h54v40H9z'
+        />
+        <path
+          d='M36.36 23c7.47 0 14.423 4.5 18.147 11.745l-.009-.018a1.931 1.931 0 01.148 1.424c-.037.127-.086.25-.148.367l.009-.018C50.783 43.744 43.83 48.245 36.36 48.245c-7.462 0-14.41-4.493-18.137-11.727a1.907 1.907 0 01-.2-.596l-.008-.06a1.936 1.936 0 010-.479l.009-.06c.033-.208.1-.41.198-.596C21.95 27.493 28.898 23 36.36 23zm0 3.86c-5.7 0-11.064 3.333-14.207 8.762 3.143 5.43 8.507 8.763 14.207 8.763 5.7 0 11.064-3.333 14.207-8.762-3.143-5.43-8.506-8.763-14.207-8.763zm0 2.403c3.531 0 6.403 2.853 6.403 6.359 0 3.507-2.872 6.36-6.403 6.36-3.53 0-6.403-2.853-6.403-6.36 0-3.506 2.873-6.359 6.403-6.359zm0 3.624c-1.519 0-2.755 1.227-2.755 2.735 0 1.51 1.236 2.736 2.755 2.736a2.749 2.749 0 002.755-2.736c0-1.508-1.236-2.735-2.755-2.735z'
+          fill='currentColor'
+        />
+      </g>
+    </svg>
+  </BoostnoteIconStyledContainer>
+)
