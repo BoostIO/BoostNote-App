3771ac89951c3c0e0a5617cb093325f73d1558de
Junyoung Choi <fluke8259@gmail.com>
2020-01-08 18:49:43 -0500

    Fix title focus issue

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,3 +1,21 @@
 import { ChangeEventHandler } from 'react'
 
 export type SelectChangeEventHandler = ChangeEventHandler<HTMLSelectElement>
+
+const NoteDetailFocusTitleInputEventType = 'NoteDetail:focusTitleInput'
+
+export function dispatchNoteDetailFocusTitleInputEvent() {
+  window.dispatchEvent(new CustomEvent(NoteDetailFocusTitleInputEventType))
+}
+
+export function listenNoteDetailFocusTitleInputEvent(
+  handler: (event: Event) => void
+) {
+  window.addEventListener(NoteDetailFocusTitleInputEventType, handler)
+}
+
+export function unlistenNoteDetailFocusTitleInputEvent(
+  handler: (event: Event) => void
+) {
+  window.removeEventListener(NoteDetailFocusTitleInputEventType, handler)
+}
