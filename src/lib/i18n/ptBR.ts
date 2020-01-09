d286e550496b73a83efc52dedf728d1d1eef68c1
cuelholima <matth.faria96@gmail.com>
2020-01-09 01:48:48 -0300

    Add <ptBR.ts> to language list

---
+++ src/lib/i18n/ptBR.ts
@@ -0,0 +1,160 @@
+﻿export default {
+  translation: {
+    //General
+    'general.error': 'Erro',
+    'general.cancel': 'Cancelar',
+    'general.attachments': 'Anexos',
+    'general.trash': 'Lixo',
+    'general.allnote': 'All Notes',
+    'general.signin': 'Logar',
+    'general.signOut': 'Sair',
+    'general.save': 'Salvar',
+    'general.default': 'padrão',
+    'general.networkError': 'Erro de conexão',
+
+    // Storage
+    'storage.name': 'Nome de armazenamento',
+    'storage.noStorage': 'Sem armazenamentos',
+    'storage.create': 'Criar armazenamento',
+    'storage.edit': 'Editar armazenamento',
+    'storage.rename': 'Renomear armazenamento',
+    'storage.renameMessage': 'Introduza um novo nome de armazenamento',
+    'storage.remove': 'Remover armazenamento',
+    'storage.removeMessage': 'O armazenamento será desvinculado deste aplicativo.',
+    'storage.delete': 'Deletar armazenamento',
+    'storage.move': 'Mover nota',
+    'storage.moveTitle': 'Mover nota para outro armazenamento',
+    'storage.moveMessage':
+      'Você está tentando mover uma nota para outro armazenamento.',
+    'storage.copy': 'Copiar nota',
+    'storage.typeLocal': 'Local',
+    'storage.typeCloud': 'Nuvem',
+    'storage.needSignIn': 'Você precisa logar-se para criar um armazenamento em nuvem.',
+    'storage.syncDate': 'Sincronizado pela última vez em',
+
+    //Folder
+    'folder.create': 'Nova pasta',
+    'folder.rename': 'Renomear pasta',
+    'folder.renameMessage':
+      'Insira um novo nome de pasta, o caminho de cada nota e sub-pasta será também atualizado.',
+    'folder.renameErrorMessage': 'Não foi possível renomear a pasta',
+    'folder.remove': 'Remover pasta',
+    'folder.removeMessage': 'Todas as notas e sub-pastas serão deletadas.',
+
+    //Tag
+    'tag.tag': 'Tags',
+    'tag.remove': 'Remover Tag',
+    'tag.removeMessage': 'A Tag será removida de todas as notas.',
+
+    //Note
+    'note.duplicate': 'Duplicar',
+    'note.delete': 'Deletar',
+    'note.delete2': 'Deleter uma nota',
+    'note.deleteMessage': 'A nota será permanentemente deletada',
+    'note.empty': 'Nota vazia',
+    'note.unselect': 'Nenhuma nota está seleciona',
+    'note.search': 'Procurar notas',
+    'note.nothing': 'Sem notas',
+    'note.nothingMessage': 'Nenhuma nota foi encontrada.',
+    'note.noTitle': 'Sem título',
+    'note.date': 'Atrás',
+    'note.createKeyMac': 'Mac: Command(⌘) + n',
+    'note.createKeyWinLin': 'Windows/Linux: Ctrl + n',
+    'note.createkeymessage1': 'Para criar uma nova nota',
+    'note.createkeymessage2': 'Selecionar um armazenamento',
+    'note.createkeymessage3': 'Para criar uma nota',
+
+    //Bookmark
+    'bookmark.remove': 'Remover marcador',
+    'bookmark.add': 'Marcador',
+
+    //About
+    'about.about': 'Sobre',
+    'about.boostnoteDescription':
+      'Um programa de anotações de código aberto para programadores como você.',
+    'about.website': 'Site oficial',
+    'about.boostWiki': 'Time Boost Note',
+    'about.platform': 'Multi plataforma',
+    'about.community': 'Comunidade',
+    'about.github': 'Repositório no GitHub',
+    'about.bounty': 'Recompensa no IssueHunt',
+    'about.blog': 'Blog',
+    'about.slack': 'Grupo do Slack',
+    'about.twitter': 'Twitter',
+    'about.facebook': 'Grupo do Facebook',
+    'about.reddit': 'Reddit',
+
+    //Billing
+    'billing.billing': 'Seu plano',
+    'billing.message': 'Para poder atualizar o plano, faça o login.',
+    'billing.basic': 'Básico',
+    'billing.current': 'Atual',
+    'billing.premium': 'Premium',
+    'billing.price': '$3/Mês (Dólar) *',
+    'billing.browser': 'App Web',
+    'billing.desktop': 'App de Computador (Mac/Windows/Linux)',
+    'billing.mobile': 'App Móvel (Será lançado em janeiro de 2020)',
+    'billing.sync': 'Sincroniza múltiplos dispositivos',
+    'billing.local': 'Armazenamento local',
+    'billing.cloud': 'Armazenamento em nuvem',
+    'billing.storageSize': 'Tamanho de armazenamento em nuvem',
+    'billing.addStorageDescription':
+      '* Se você precisar de mais armazenamento em nuvem, poderá o adicionar a qualquer momento pagando $5 (Dólares) para cada 5GB. Clique no botão "Adicionar armazenamento extra" logo abaixo.',
+    'billing.addStorage': 'Adicionar armazenamento extra',
+
+    // Preferences
+    'preferences.general': 'Preferências',
+
+    // Preferences GeneralTab
+    'preferences.account': 'Conta',
+    'preferences.addAccount': 'Logar-se',
+    'preferences.loginWorking': 'Logando...',
+    'preferences.interfaceLanguage': 'Idioma da interface',
+    'preferences.applicationTheme': 'Tema do aplicativo',
+    'preferences.auto': 'Auto',
+    'preferences.light': 'Claro',
+    'preferences.dark': 'Escuro',
+    'preferences.sepia': 'Sepia',
+    'preferences.solarizedDark': 'Solarized Dark',
+    'preferences.noteSorting': 'Classificação de notas',
+    'preferences.dateUpdated': 'Data de atualização',
+    'preferences.dateCreated': 'Data de cricação',
+    'preferences.title': 'Título',
+    'preferences.analytics': 'Analítico',
+    'preferences.analyticsDescription1':
+      'O Boost Note coleta dados anônimos com o único objetivo de melhorar o aplicativo. Não é coletado nenhuma informação pessoal, como o conteúdo de suas anotações. Você pode ver exatamente como o código do programa funciona no GitHub.',
+    'preferences.analyticsDescription2':
+      'Você pode escolher ativar ou desativar esta opção.',
+    'preferences.analyticsLabel': 'Habilitar analítico para ajudar na melhora de Boost Note',
+    'preferences.displayTutorialsLabel': 'Tutoriais e FAQ',
+
+    // Preferences EditorTab
+    'preferences.editorTheme': 'Tema do editor',
+    'preferences.editorFontSize': 'Tamanho de fonte',
+    'preferences.editorFontFamily': 'Fonte',
+    'preferences.editorIndentType': 'Tipo de recuo',
+    'preferences.tab': 'Tab',
+    'preferences.spaces': 'Espaços',
+    'preferences.editorIndentSize': 'Tamanho do recup',
+    'preferences.editorKeymap': 'Funções das teclas',
+    'preferences.editorPreview': 'Pré visualização',
+
+    // Preferences MarkdownTab
+    'preferences.previewStyle': 'Prever estilo',
+    'preferences.markdownCodeBlockTheme': 'Code Block Theme',
+    'preferences.defaultTheme': 'Usar estilo padrão',
+    'preferences.markdownPreview': 'Pré-visualizar Markdown',
+
+    // Preferences ImportTab
+    'preferences.import': 'Import',
+    'preferences.description': 'Importar arquivos .cson do antigo Boostnote.',
+    'preferences.importFlow1': '1. Abrir pasta do antigo Boostnote em seu PC.',
+    'preferences.importFlow2':
+      '2. Arraste e solte os arquivos .cson no formulário abaixo.',
+    'preferences.importFlow3':
+      '3. Escolha o armazenamento e pasta que você quer mover seus arquivos antigos.',
+    'preferences.importFlow4': '4. Upload!',
+    'preferences.importRemove': 'Remover',
+    'preferences.importUpload': 'Upload'
+  }
+}
