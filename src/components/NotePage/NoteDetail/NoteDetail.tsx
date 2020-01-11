d628990c626eba4e3d02531d968697e0dfa26659
funzin <nakazawa.fumito@gmail.com>
2020-01-11 23:11:44 +0900

    Fix return immediately

--- src/components/NotePage/NoteDetail/NoteDetail.tsx
+++ src/components/NotePage/NoteDetail/NoteDetail.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { includes } from 'ramda'
 import {
   NoteDoc,
   NoteDocEditibleProps,
@@ -299,18 +300,17 @@ export default class NoteDetail extends React.Component<
   }
 
   appendNewTag = () => {
-    const includes = this.state.tags.includes(this.state.newTagName)
-    if (isTagNameValid(this.state.newTagName) && !includes) {
-      this.setState(
-        prevState => ({
-          newTagName: '',
-          tags: [...prevState.tags, prevState.newTagName]
-        }),
-        () => {
-          this.queueToSave()
-        }
-      )
-    }
+    if (includes(this.state.newTagName, this.state.tags)) { return }
+    if (!isTagNameValid(this.state.newTagName)) { return }
+    this.setState(
+      prevState => ({
+        newTagName: '',
+        tags: [...prevState.tags, prevState.newTagName]
+      }),
+      () => {
+        this.queueToSave()
+      }
+    )
   }
 
   removeTagByName = (tagName: string) => {
