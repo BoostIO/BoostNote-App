c6057b79d3defa162fcd2f957bf761381622a07c
senseei <chrisblue1415@gmail.com>
2020-01-15 17:19:16 +0100

    Add german translation

---
+++ src/lib/i18n/de.ts
@@ -0,0 +1,161 @@
+export default {
+  translation: {
+    //General
+    'general.error': 'Fehler',
+    'general.cancel': 'Abbrechen',
+    'general.attachments': 'Anhänge',
+    'general.trash': 'Papierkorb',
+    'general.allnote': 'Alle Notizen',
+    'general.signin': 'Anmleden',
+    'general.signOut': 'Abmelden',
+    'general.save': 'Speichern',
+    'general.default': 'Standard',
+    'general.networkError': 'Netzwerk-Fehler',
+
+    // Storage
+    'storage.name': 'Speicher Name',
+    'storage.noStorage': 'Keine Speicher',
+    'storage.create': 'Speicher erstellen',
+    'storage.edit': 'Speicher ändern',
+    'storage.rename': 'Speicher umbenennen',
+    'storage.renameMessage': 'Name für Speicher ändern',
+    'storage.remove': 'Speicher entfernen',
+    'storage.removeMessage': 'Speicher wird von App entknüpft',
+    'storage.delete': 'Speicher löschen',
+    'storage.move': 'Notiz verschieben',
+    'storage.moveTitle': 'Notiz in anderen Speicher verschieben',
+    'storage.moveMessage':
+      'Es wird versucht eine Notiz in einen anderen Speicher zu verschieben.',
+    'storage.copy': 'Notiz kopieren',
+    'storage.typeLocal': 'Lokal',
+    'storage.typeCloud': 'Cloud',
+    'storage.needSignIn': 'Du musst dich anmelden um einen Cloud-Speicher zu erstellen.',
+    'storage.syncDate': 'Zuletzt synchronisiert am',
+
+    //Folder
+    'folder.create': 'Neuer Ordner',
+    'folder.rename': 'Ordner umbenennen',
+    'folder.renameMessage':
+      'Gib den neuen Ordnernamen an, jede Notiz und Unterordner-Pfad wird aktualisiert.',
+    'folder.renameErrorMessage': 'Ordner konnte nicht umbenannt werden',
+    'folder.remove': 'Ordner löschen',
+    'folder.removeMessage': 'Alle Notizen und Unterordner werden gelöscht.',
+
+    //Tag
+    'tag.tag': 'Etikett',
+    'tag.remove': 'Etikett löschen',
+    'tag.removeMessage': 'Das Etikett wird von allen Notizen entfernt.',
+
+    //Note
+    'note.duplicate': 'Duplizieren',
+    'note.delete': 'Löschen',
+    'note.delete2': 'Notiz löschen',
+    'note.deleteMessage': 'Die Notiz wird endgültig gelöscht',
+    'note.empty': 'Leere Notiz',
+    'note.unselect': 'Keine Notiz ausgewählt',
+    'note.search': 'Notizen durchsuchen',
+    'note.nothing': 'Keine Notizen',
+    'note.nothingMessage': 'Keine Notizen gefunden.',
+    'note.noTitle': 'Kein Titel',
+    'note.date': 'her',
+    'note.createKeyMac': 'Mac: Befehl(⌘) + n',
+    'note.createKeyWinLin': 'Windows/Linux: Ctrl + n',
+    'note.createkeymessage1': 'um eine neue Notiz zu erstellen',
+    'note.createkeymessage2': 'Select a storage',
+    'note.createkeymessage3': 'um eine neue Notiz zu erstellen',
+    'note.restore': 'Wiederherstellen',
+
+    //Bookmark
+    'bookmark.remove': 'Lesezeichen entfernen',
+    'bookmark.add': 'Lesezeichen hinzufügen',
+
+    //About
+    'about.about': 'Über',
+    'about.boostnoteDescription':
+      'Ein quelloffenes Notiz-Programm geschaffen für Programmierer wie dir.',
+    'about.website': 'Offizielle Webseite',
+    'about.boostWiki': 'Boost Note für Team',
+    'about.platform': 'Cross-platform',
+    'about.community': 'Community',
+    'about.github': 'GitHub Repository',
+    'about.bounty': 'Bounty on IssueHunt',
+    'about.blog': 'Blog',
+    'about.slack': 'Slack Gruppe',
+    'about.twitter': 'Twitter Account',
+    'about.facebook': 'Facebook Gruppe',
+    'about.reddit': 'Reddit',
+
+    //Billing
+    'billing.billing': 'Billing',
+    'billing.message': 'Bitte melde dich an um deinen Plan zu ugpraden.',
+    'billing.basic': 'Basic',
+    'billing.current': 'Aktuell',
+    'billing.premium': 'Premium',
+    'billing.price': '$3/Monat (USD) *',
+    'billing.browser': 'Browser App',
+    'billing.desktop': 'Desktop App (Mac/Windows/Linux)',
+    'billing.mobile': 'Mobile App (Wird gelauncht am Jan, 2020)',
+    'billing.sync': 'Mehrere Geräte synchronisieren',
+    'billing.local': 'Lokaler Speicher',
+    'billing.cloud': 'Cloud Speicher',
+    'billing.storageSize': 'Cloud Speicher Größe',
+    'billing.addStorageDescription':
+      '* Wenn du mehr Cloud-Speicher brauchst, zahlst du 5$ (USD) für jede zusätzliche 5GB. Drücke auf den "Extra Speicher hinzufügen"-Knopf unten.',
+    'billing.addStorage': 'Extra Speicher hinzufügen',
+
+    // Preferences
+    'preferences.general': 'Einstellungen',
+
+    // Preferences GeneralTab
+    'preferences.account': 'Account',
+    'preferences.addAccount': 'Anmelden',
+    'preferences.loginWorking': 'Wird angemeldet...',
+    'preferences.interfaceLanguage': 'Oberflächen Sprache',
+    'preferences.applicationTheme': 'Anwendungs-Theme',
+    'preferences.auto': 'Auto',
+    'preferences.light': 'Hell',
+    'preferences.dark': 'Dunkel',
+    'preferences.sepia': 'Sepia',
+    'preferences.solarizedDark': 'Solarized Dark',
+    'preferences.noteSorting': 'Notiz-Sortierung',
+    'preferences.dateUpdated': 'Datum aktualisiert',
+    'preferences.dateCreated': 'Datum erstellt',
+    'preferences.title': 'Titel',
+    'preferences.analytics': 'Analytik',
+    'preferences.analyticsDescription1':
+      'Boostnote sammelt anonyme Daten um das Nutzererlebnis zu verbessern, und es wird streng darauf geachtet keine persönlichen Informationen wie die Inhalte der Notizen zu senden. Um zu sehen wie das läuft, besuche unsere Github Seite.',
+    'preferences.analyticsDescription2':
+      'Du kannst wählen diese Option zu aktivieren oder deaktivieren.',
+    'preferences.analyticsLabel': 'Analytik aktivieren um mitzuhelfen Boostnote zu verbessern',
+    'preferences.displayTutorialsLabel': 'Tutorials and FAQ',
+
+    // Preferences EditorTab
+    'preferences.editorTheme': 'Editor Theme',
+    'preferences.editorFontSize': 'Editor Font Größe',
+    'preferences.editorFontFamily': 'Editor Font Familie',
+    'preferences.editorIndentType': 'Editor Einrückungs-Typ',
+    'preferences.tab': 'Tab',
+    'preferences.spaces': 'Leertasten',
+    'preferences.editorIndentSize': 'Editor Einrückungs-Tiefe',
+    'preferences.editorKeymap': 'Editor Keymap',
+    'preferences.editorPreview': 'Editor Vorschau',
+
+    // Preferences MarkdownTab
+    'preferences.previewStyle': 'Vorschau Style',
+    'preferences.markdownCodeBlockTheme': 'Code Block Theme',
+    'preferences.defaultTheme': 'Verwende standard Style',
+    'preferences.markdownPreview': 'Markdown Vorschau',
+
+    // Preferences ImportTab
+    'preferences.import': 'Importieren',
+    'preferences.description': 'Importiert .cson files vom alten Boostnote.',
+    'preferences.importFlow1': '1. Öffne den alten Boostnote Ordner auf deinem PC.',
+    'preferences.importFlow2':
+      '2. Ziehe die .cson Dateien in das Feld unten.',
+    'preferences.importFlow3':
+      '3. Wähle den Speicher und den Ordner in die du die Dateien verschieben möchtest.',
+    'preferences.importFlow4': '4. Upload!',
+    'preferences.importRemove': 'entfernen',
+    'preferences.importUpload': 'Upload'
+  }
+}
