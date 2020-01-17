3c864449690365d03df51d9dbf81a4d6bbb67b95
noklam <channoklam@yahoo.com.hk>
2020-01-17 12:48:48 +0800

    add translation

---
+++ src/lib/i18n/zhHK.ts
@@ -0,0 +1,158 @@
+export default {
+  translation: {
+    //General
+    'general.error': '錯誤',
+    'general.cancel': '取消',
+    'general.attachments': '附件',
+    'general.trash': '垃圾筒',
+    'general.allnote': '所有筆記',
+    'general.signin': '登入',
+    'general.signOut': '登出',
+    'general.save': '儲存',
+    'general.default': '默認',
+    'general.networkError': '網絡錯誤',
+
+    // Storage
+    'storage.name': '儲存空間名稱',
+    'storage.noStorage': '沒有儲存空間',
+    'storage.create': '創建新的儲存空間',
+    'storage.edit': '編輯',
+    'storage.rename': '重新命名',
+    'storage.renameMessage': '輸入新的儲存空間名稱',
+    'storage.remove': '移除儲存空間',
+    'storage.removeMessage': '這個筆記將會從這個 APP 解除聯繫.',
+    'storage.delete': '刪除儲存空間',
+    'storage.move': '移動儲存空間',
+    'storage.moveTitle': '移動筆記到其他儲存空間',
+    'storage.moveMessage': '你正在嘗試把筆記移到另一個儲存空間.',
+    'storage.copy': '複製筆記',
+    'storage.typeLocal': 'Local',
+    'storage.typeCloud': 'Cloud',
+    'storage.needSignIn': '你需要登入來創建Cloud Storage.',
+    'storage.syncDate': '上一次同步時間',
+
+    //Folder
+    'folder.create': '新資料夾',
+    'folder.rename': '重新命名資料夾',
+    'folder.renameMessage':
+      '輸入新資料夾名稱，所有的筆記和子資料夾路徑也將被更新.',
+    'folder.renameErrorMessage': '你不能重新命名該資料夾',
+    'folder.remove': '移除資料夾',
+    'folder.removeMessage': '所有筆記和子資料夾將被刪除.',
+
+    //Tag
+    'tag.tag': '標籤',
+    'tag.remove': '移除標籤',
+    'tag.removeMessage': '這個標籤將從所有筆記中被移除.',
+
+    //Note
+    'note.duplicate': '複製',
+    'note.delete': '刪除',
+    'note.delete2': '刪除筆記',
+    'note.deleteMessage': '該筆記將被永久刪除',
+    'note.empty': '空的筆記',
+    'note.unselect': '沒有選擇任何筆記',
+    'note.search': '搜索筆記',
+    'note.nothing': '無筆記',
+    'note.nothingMessage': '搵唔到你要嘅筆記.',
+    'note.noTitle': '無標題',
+    'note.date': '之前',
+    'note.createKeyMac': 'Mac: Command(⌘) + Enter',
+    'note.createKeyWinLin': 'Windows/Linux: Ctrl + Enter',
+    'note.createkeymessage1': '新筆記',
+    'note.createkeymessage2': '選擇儲存空間',
+    'note.createkeymessage3': '新筆記',
+    'note.restore': 'Restore',
+
+    //Bookmark
+    'bookmark.remove': '移除書簽',
+    'bookmark.add': '添加書簽',
+
+    //About
+    'about.about': '關於',
+    'about.boostnoteDescription': 'An open source note-taking app made for programmers just like you.',
+    'about.website': '官網',
+    'about.boostWiki': 'Boost Note 團隊版',
+    'about.platform': '跨平台',
+    'about.community': '群組',
+    'about.github': 'GitHub Repository',
+    'about.bounty': 'IssueHunt 獎金',
+    'about.blog': 'Blog',
+    'about.slack': 'Slack Group',
+    'about.twitter': 'Twitter 帳號',
+    'about.facebook': 'Facebook Group',
+    'about.reddit': 'Reddit',
+
+    //Billing
+    'billing.billing': '價格表',
+    'billing.message': '請先登錄來升級你的套餐.',
+    'billing.basic': '基本套餐',
+    'billing.current': '現在的',
+    'billing.premium': '高級套餐',
+    'billing.price': '$3/月(USD) ',
+    'billing.browser': '瀏覽器',
+    'billing.desktop': '桌面軟體 (Mac/Windows/Linux)',
+    'billing.mobile': '手機軟體 (會在2020年一月發佈)',
+    'billing.sync': '多端同步',
+    'billing.local': '本地儲存',
+    'billing.cloud': '雲儲存',
+    'billing.storageSize': '雲儲存容量',
+    'billing.addStorageDescription':
+      '你可以在任何時間追加雲儲存容量，費用是每5GB 5美元.',
+    'billing.addStorage': 'Add Extra Storage',
+
+    // Preferences
+    'preferences.general': '偏好',
+
+    // Preferences GeneralTab
+    'preferences.account': '帳戶',
+    'preferences.addAccount': '添加帳戶',
+    'preferences.loginWorking': '登入...',
+    'preferences.interfaceLanguage': '介面語言',
+    'preferences.applicationTheme': '軟體主題',
+    'preferences.auto': '自動',
+    'preferences.light': '亮',
+    'preferences.dark': '暗',
+    'preferences.sepia': 'Sepia',
+    'preferences.solarizedDark': 'Solarized Dark',
+    'preferences.noteSorting': '筆記分類',
+    'preferences.dateUpdated': '更新日期',
+    'preferences.dateCreated': '創建日期',
+    'preferences.title': '標題',
+    'preferences.analytics': 'Analytics(分析)',
+    'preferences.analyticsDescription1':
+      'Boost Note 只為了提升軟體收集匿名資料, 並且我們絕不收集你筆記中的個人資訊.你可以在 GitHub 上看這是如何運作的.',
+    'preferences.analyticsDescription2': '你可以選擇開啟或者關閉這個選項.',
+    'preferences.analyticsLabel': '開啟分析來幫助我們完善 Boostnote',
+    'preferences.displayTutorialsLabel': '教學與 FAQ',
+
+    // Preferences EditorTab
+    'preferences.editorTheme': '編輯器主題',
+    'preferences.editorFontSize': '編輯器字型大小',
+    'preferences.editorFontFamily': '編輯器字體粗細',
+    'preferences.editorIndentType': '編輯器縮進文字',
+    'preferences.tab': '標籤',
+    'preferences.spaces': '空格',
+    'preferences.editorIndentSize': '編輯器縮進尺寸',
+    'preferences.editorKeymap': '編輯器快速鍵',
+    'preferences.editorPreview': '編輯器預覽',
+
+    // Preferences MarkdownTab
+    'preferences.previewStyle': '預覽樣式',
+    'preferences.markdownCodeBlockTheme': '代碼塊主題',
+    'preferences.defaultTheme': '使用預設主題',
+    'preferences.markdownPreview': 'Markdown 預覽',
+
+    // Preferences ImportTab
+    'preferences.import': '導入',
+    'preferences.description': '從舊版 Boostnote 導入 .cson 文件.',
+    'preferences.importFlow1': '1. 在你的電腦打開舊版 Boostnote 資料夾.',
+    'preferences.importFlow2': '2. 拖拽 .cson 檔到以下表單.',
+    'preferences.importFlow3': '3. 選擇你想要移動舊資料的存儲和資料夾.',
+    'preferences.importFlow4': '4. 上傳!',
+    'preferences.importRemove': '移除',
+    'preferences.importUpload': '上傳'
+  }
+}
+
+
