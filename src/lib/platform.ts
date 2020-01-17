fb55f6d02696bf77b7ab7f0b05c99a06081fb4f7
Junyoung Choi <fluke8259@gmail.com>
2020-01-17 17:36:40 -0500

    Move osName to platform

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -11,3 +11,16 @@ export const openNew = (url: string) => {
     window.open(url, '_blank')
   }
 }
+
+export type OsNameOptions = 'windows' | 'macos' | 'unix' | 'linux' | 'unknown'
+
+function getOsName(): OsNameOptions {
+  if (navigator.appVersion.indexOf('Win') != -1) return 'windows'
+  if (navigator.appVersion.indexOf('Mac') != -1) return 'macos'
+  if (navigator.appVersion.indexOf('X11') != -1) return 'unix'
+  if (navigator.appVersion.indexOf('Linux') != -1) return 'linux'
+
+  return 'unknown'
+}
+
+export const osName = getOsName()
