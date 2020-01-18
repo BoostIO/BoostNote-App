89ee0ff9215e1039f51c54b7cd65d22573930176
Junyoung Choi <fluke8259@gmail.com>
2020-01-18 11:15:08 -0500

    Flatten lib/modal

--- src/lib/modal.ts
+++ src/lib/modal.ts
@@ -1,7 +1,20 @@
-import { createStoreContext } from '../context'
+import { createStoreContext } from './context'
 import { useState, useCallback } from 'react'
-import { ModalsContentOptions, ModalsContext } from './types'
-export * from './types'
+
+export type ModalsContentOptions = 'download-app'
+
+export interface ModalsRenderingOptions {
+  closable: boolean
+  skippable: boolean
+  onClose?(): void
+  body: JSX.Element
+}
+
+export interface ModalsContext {
+  modalContent: ModalsContentOptions | null
+  openModal: (options: ModalsContentOptions) => void
+  closeModal: () => void
+}
 
 function useModalStore(): ModalsContext {
   const [modalContent, setModalContent] = useState<ModalsContentOptions | null>(
