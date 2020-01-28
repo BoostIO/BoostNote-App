3050984a4c7a46149f1afc40a1ac96781e428400
Junyoung Choi <fluke8259@gmail.com>
2020-01-29 07:24:02 +0900

    Refactor db and router to share their code to mobile app

--- src/lib/router/types.ts
+++ src/lib/router/types.ts
@@ -5,3 +5,74 @@ export interface Location {
   hash: string
   query: ParsedUrlQuery
 }
+
+export interface BaseRouteParams {
+  name: string
+}
+
+export interface StorageCreate extends BaseRouteParams {
+  name: 'storages.create'
+}
+
+export interface StorageEdit extends BaseRouteParams {
+  name: 'storages.edit'
+  storageId: string
+}
+
+export interface StorageAllNotes extends BaseRouteParams {
+  name: 'storages.allNotes'
+  storageId?: string
+  noteId?: string
+}
+
+export interface StorageBookmarkNotes extends BaseRouteParams {
+  name: 'storages.bookmarks'
+  storageId?: string
+  noteId?: string
+}
+
+export interface StorageNotesRouteParams extends BaseRouteParams {
+  name: 'storages.notes'
+  storageId: string
+  folderPathname: string
+  noteId?: string
+}
+
+export interface StorageTrashCanRouteParams extends BaseRouteParams {
+  name: 'storages.trashCan'
+  storageId: string
+  noteId?: string
+}
+
+export interface StorageTagsRouteParams extends BaseRouteParams {
+  name: 'storages.tags.show'
+  storageId: string
+  tagName: string
+  noteId?: string
+}
+
+export interface StorageAttachmentsRouteParams extends BaseRouteParams {
+  name: 'storages.attachments'
+  storageId: string
+}
+
+export interface TutorialsRouteParams extends BaseRouteParams {
+  name: 'tutorials.show'
+  path: string
+}
+
+export interface UnknownRouteparams extends BaseRouteParams {
+  name: 'unknown'
+}
+
+export type AllRouteParams =
+  | StorageCreate
+  | StorageEdit
+  | StorageAllNotes
+  | StorageBookmarkNotes
+  | StorageNotesRouteParams
+  | StorageTrashCanRouteParams
+  | StorageTagsRouteParams
+  | StorageAttachmentsRouteParams
+  | UnknownRouteparams
+  | TutorialsRouteParams
