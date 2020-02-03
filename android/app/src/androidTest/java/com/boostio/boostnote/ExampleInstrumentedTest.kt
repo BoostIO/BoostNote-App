14eeb42bacb67d7068ab775db85c5800972f0481
Junyoung Choi <fluke8259@gmail.com>
2020-02-03 09:22:08 +0900

    Update android project name

--- android/app/src/androidTest/java/com/boostio/boostnote/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/com/boostio/boostnote/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.boostio.boostnote
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- android/app/src/main/java/com/boostio/boostnote/MainActivity.kt
+++ android/app/src/main/java/com/boostio/boostnote/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.boostio.boostnote
 
 import android.content.Context
 import android.content.Intent
