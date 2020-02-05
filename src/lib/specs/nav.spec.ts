8d17346e36c95f41474a5ca6bbe2720a7f9650c8
Junyoung Choi <fluke8259@gmail.com>
2020-02-05 16:31:17 +0900

    Format

--- src/lib/specs/nav.spec.ts
+++ src/lib/specs/nav.spec.ts
@@ -1,23 +1,21 @@
-import {
-  getStorageItemId,
-  getFolderItemId,
-  getTagListItemId
-} from '../nav'
+import { getStorageItemId, getFolderItemId, getTagListItemId } from '../nav'
 
 describe('getStorageItemId', () => {
   it('returns a correct storage path', () => {
-    expect(getStorageItemId("test_storage")).toBe("storage:test_storage")
+    expect(getStorageItemId('test_storage')).toBe('storage:test_storage')
   })
 })
 
 describe('getFolderItemId', () => {
   it('returns a correct folder item path', () => {
-    expect(getFolderItemId("test_storage", "test_folder")).toBe("storage:test_storage/folder:test_folder")
+    expect(getFolderItemId('test_storage', 'test_folder')).toBe(
+      'storage:test_storage/folder:test_folder'
+    )
   })
 })
 
 describe('getTagListItemId', () => {
   it('returns a correct tags path', () => {
-    expect(getTagListItemId("test_storage")).toBe("storage:test_storage/tags")
+    expect(getTagListItemId('test_storage')).toBe('storage:test_storage/tags')
   })
 })
