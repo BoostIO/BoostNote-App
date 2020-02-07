8c88d26f5730b3f1d4ee6015fc3f4d8f6ce0148c
Junyoung Choi <fluke8259@gmail.com>
2020-02-07 16:02:36 +0900

    Add ios base project

---
+++ ios/BoostNote/BoostNoteTests/BoostNoteTests.swift
@@ -0,0 +1,34 @@
+//
+//  BoostNoteTests.swift
+//  BoostNoteTests
+//
+//  Created by Junyoung Choi on 2020/02/07.
+//  Copyright © 2020 BoostIO. All rights reserved.
+//
+
+import XCTest
+@testable import BoostNote
+
+class BoostNoteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
