d2fe2fdf0bcb8f40ff787fd04f34b32d9f551d34
Junyoung Choi <fluke8259@gmail.com>
2020-02-07 15:19:16 +0900

    Add color prop to Icon

--- src/mobile/components/atoms/Icon.tsx
+++ src/mobile/components/atoms/Icon.tsx
@@ -3,16 +3,17 @@ import { Icon as MdiIcon } from '@mdi/react'
 
 interface IconProps {
   path: string
+  color?: string
 }
 
-const Icon = ({ path }: IconProps) => (
+const Icon = ({ path, color = 'currentColor' }: IconProps) => (
   <MdiIcon
     path={path}
     style={{
       width: '1em',
       height: '1em'
     }}
-    color='currentColor'
+    color={color}
   />
 )
 
