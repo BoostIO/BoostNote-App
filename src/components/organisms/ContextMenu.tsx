eff95740c39b4e87bcab694e0b50393a9ce7fe43
Junyoung Choi <fluke8259@gmail.com>
2020-02-14 10:59:39 +0900

    Move ContextMenu to organisms

--- src/components/organisms/ContextMenu.tsx
+++ src/components/organisms/ContextMenu.tsx
@@ -1,10 +1,59 @@
 import React from 'react'
-import { StyledContextMenu, StyledContextMenuItem } from './styled'
 import {
   useContextMenu,
   MenuTypes,
   ContextMenuContext
 } from '../../lib/contextMenu'
+import styled from '../../lib/styled'
+import {
+  menuHeight,
+  menuVerticalPadding,
+  menuZIndex
+} from '../../lib/contextMenu'
+import {
+  uiTextColor,
+  contextMenuShadow,
+  borderColor,
+  backgroundColor,
+  activeBackgroundColor
+} from '../../lib/styled/styleFunctions'
+
+const StyledContextMenu = styled.div`
+  min-width: 130px;
+  position: fixed;
+  z-index: ${menuZIndex};
+  ${backgroundColor}
+  ${borderColor}
+  border-style: solid;
+  border-width: 1px;
+  padding: ${menuVerticalPadding}px 0;
+  font-size: 14px;
+  box-sizing: border-box;
+  border-radius: 2px;
+  ${contextMenuShadow}
+  outline: none;
+`
+
+const StyledContextMenuItem = styled.button`
+  height: ${menuHeight}px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  background-color: transparent;
+  border: none;
+  display: block;
+  width: 100%;
+  text-align: left;
+  ${uiTextColor};
+  &:hover,
+  &:focus,
+  &:active,
+  &.active {
+    ${activeBackgroundColor}
+  }
+  &:disabled {
+    background-color: transparent;
+  }
+`
 
 interface ContextMenuProps {
   contextMenu: ContextMenuContext
@@ -84,6 +133,7 @@ class ContextMenu extends React.Component<ContextMenuProps> {
     )
   }
 }
+
 export default () => {
   const contextMenu = useContextMenu()
   return <ContextMenu contextMenu={contextMenu} />
