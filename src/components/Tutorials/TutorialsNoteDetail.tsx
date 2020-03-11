f544a28a4a735665a6e5d5096f3df205db6f4627
Junyoung Choi <rokt33r.choi@gmail.com>
2020-03-11 15:45:43 +0900

    Move StorageCreatPage and StorageEditPage to pages

--- src/components/Tutorials/TutorialsNoteDetail.tsx
+++ src/components/Tutorials/TutorialsNoteDetail.tsx
@@ -5,7 +5,7 @@ import ToolbarIconButton from '../atoms/ToolbarIconButton'
 import Toolbar from '../atoms/Toolbar'
 import ToolbarSeparator from '../atoms/ToolbarSeparator'
 import { TutorialsNavigatorTreeItem } from '../../lib/tutorials'
-import { StyledNoteDetailContainer } from '../organisms/NoteDetail'
+import { StyledNoteDetailContainer } from '../organisms/NoteDetail/NoteDetail'
 import { ViewModeType } from '../../lib/generalStatus'
 import { IconEye, IconSplit, IconEdit } from '../icons'
 
