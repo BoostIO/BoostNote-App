7571ee12c2cb0dbda7607c4089d804b0cc32cf20
Junyoung Choi <rokt33r.choi@gmail.com>
2020-03-15 19:19:28 +0900

    Clean up StorageCreatePage

--- src/components/pages/StorageCreatePage.tsx
+++ src/components/pages/StorageCreatePage.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react'
-import { usePreferences } from '../../lib/preferences'
-import { TopMargin, SectionPrimaryButton } from '../PreferencesModal/styled'
 import { useTranslation } from 'react-i18next'
-import { useDb } from '../../lib/db'
-import { useToast } from '../../lib/toast'
-import { useRouter } from '../../lib/router'
 import PageContainer from '../atoms/PageContainer'
 import {
   FormHeading,
@@ -17,31 +12,8 @@ import LocalStorageCreateForm from '../organisms/LocalStorageCreateForm'
 import CloudStorageCreateForm from '../organisms/CloudStorageCreateForm'
 
 export default () => {
-  const db = useDb()
-  const { preferences } = usePreferences()
-  const { pushMessage } = useToast()
   const { t } = useTranslation()
   const [storageType, setStorageType] = useState<'cloud' | 'local'>('cloud')
-  const { push } = useRouter()
-
-  const user = preferences['general.accounts'][0]
-
-  const isLoggedIn = user != null
-
-  const createStorageCallback = async () => {
-    // editStoragePage edits cloud storage directly
-    // update local -> update cloud -> on fail -> revert local
-    try {
-      const storage = await db.createStorage(name, storageType)
-      push(`/app/storages/${storage.id}/notes`)
-    } catch {
-      pushMessage({
-        title: 'Cloud Error',
-        description:
-          'An error occured while attempting to create a cloud storage'
-      })
-    }
-  }
 
   return (
     <div>
