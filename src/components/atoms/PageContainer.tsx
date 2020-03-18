f074c6d993bfbdd8310f493de4118f955a66bf10
Junyoung Choi <rokt33r.choi@gmail.com>
2020-03-18 10:29:47 +0900

    Make PageContainer scrollable

--- src/components/atoms/PageContainer.tsx
+++ src/components/atoms/PageContainer.tsx
@@ -1,8 +1,9 @@
 import styled from '../../lib/styled'
 
 const PageContainer = styled.div`
-  margin: 100px;
-  max-width: 600px;
+  padding: 3rem;
+  height: 100%;
+  overflow-y: auto;
 `
 
 export default PageContainer
