b67ead7d6c832007183026a67523a922a865e155
Junyoung Choi <rokt33r.choi@gmail.com>
2020-03-23 04:12:08 +0900

    Renew StorageCreatePage and StorageEditPage

---
+++ src/mobile/components/organisms/LocalStorageCreateForm.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import {
+  FormGroup,
+  FormLabel,
+  FormPrimaryButton,
+  FormTextInput
+} from '../../../components/atoms/form'
+import { useTranslation } from 'react-i18next'
+import { useRouter } from '../../lib/router'
+import { useDb } from '../../lib/db'
+import { useToast } from '../../../lib/toast'
+
+const LocalStorageCreateForm = () => {
+  const [name, setName] = useState('')
+  const { t } = useTranslation()
+  const { push } = useRouter()
+  const db = useDb()
+  const { pushMessage } = useToast()
+  const createStorageCallback = async () => {
+    try {
+      const storage = await db.createStorage(name)
+      push(`/app/storages/${storage.id}/notes`)
+    } catch (error) {
+      pushMessage({
+        title: 'Something went wrong',
+        description: error.toString()
+      })
+    }
+  }
+  return (
+    <>
+      <FormGroup>
+        <FormLabel>{t('storage.name')}</FormLabel>
+        <FormTextInput
+          type='text'
+          value={name}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setName(e.target.value)
+          }
+        />
+      </FormGroup>
+      <FormGroup>
+        <FormPrimaryButton onClick={createStorageCallback}>
+          Create Storage
+        </FormPrimaryButton>
+      </FormGroup>
+    </>
+  )
+}
+
+export default LocalStorageCreateForm
