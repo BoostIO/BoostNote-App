8f74bd0442aa564336a6222f68a74171c48b378d
Junyoung Choi <rokt33r.choi@gmail.com>
2020-03-23 02:17:45 +0900

    Discard unused code

--- src/components/PreferencesModal/GeneralTab.tsx
+++ src/components/PreferencesModal/GeneralTab.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, ChangeEventHandler } from 'react'
+import React, { useCallback } from 'react'
 import {
   Section,
   SectionHeader,
--- src/lib/modal.ts
+++ src/lib/modal.ts
@@ -3,13 +3,6 @@ import { useState, useCallback } from 'react'
 
 export type ModalsContentOptions = 'download-app'
 
-export interface ModalsRenderingOptions {
-  closable: boolean
-  skippable: boolean
-  onClose?(): void
-  body: JSX.Element
-}
-
 export interface ModalsContext {
   modalContent: ModalsContentOptions | null
   openModal: (options: ModalsContentOptions) => void
