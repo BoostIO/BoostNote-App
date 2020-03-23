02e100448de491c4774edc8e576c9aeb6b78d433
Junyoung Choi <fluke8259@gmail.com>
2020-03-23 19:33:22 +0900

    Add mobile app download links

--- src/components/Modal/contents/DownloadOurAppModal.tsx
+++ src/components/Modal/contents/DownloadOurAppModal.tsx
@@ -8,6 +8,7 @@ import {
 } from './styled'
 import Image from '../../atoms/Image'
 import AppLink from '../../atoms/AppLink'
+import { openNew } from '../../../lib/platform'
 
 const DownloadOurAppModal = () => {
   return (
@@ -24,12 +25,29 @@ const DownloadOurAppModal = () => {
           </div>
           <div className='center'>
             <Image src='/app/static/Mobile.svg' />
-            <button className='button darker' disabled={true}>
-              Launching Soon
-            </button>
-            <span className='subtext'>
-              We are planning for a launch by the end of January.
-            </span>
+            <div>
+              <button
+                className='button primary'
+                onClick={() => {
+                  openNew(
+                    'https://apps.apple.com/us/app/boostnote-mobile/id1498182749'
+                  )
+                }}
+                style={{ marginBottom: '1em' }}
+              >
+                Download iOS
+              </button>
+              <button
+                className='button primary'
+                onClick={() => {
+                  openNew(
+                    'https://play.google.com/store/apps/details?id=com.boostio.boostnote'
+                  )
+                }}
+              >
+                Download Android
+              </button>
+            </div>
           </div>
         </ModalFlex>
       </ModalBody>
