9b4f502b268f560968ded200e9029b568eb0e56a
Junyoung Choi <fluke8259@gmail.com>
2020-03-23 17:54:47 +0900

    Implement TableView like atoms

---
+++ src/mobile/components/atoms/TableViewCell.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from '../../../lib/styled'
+import {
+  borderBottom,
+  uiTextColor,
+  sideBarBackgroundColor
+} from '../../../lib/styled/styleFunctions'
+import Icon from './Icon'
+
+const TableViewCellContainerButton = styled.button`
+  height: 44px;
+  width: 100%;
+  border: none;
+  padding: 0 1em;
+  display: flex;
+  text-align: left;
+  font-size: 15px;
+  ${uiTextColor}
+  ${sideBarBackgroundColor}
+  ${borderBottom}
+`
+
+const TableViewCellTitle = styled.div`
+  flex: 1;
+`
+
+interface TableViewCellProps {
+  iconPath?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+const TableViewCell: React.FC<TableViewCellProps> = ({
+  children,
+  iconPath,
+  onClick
+}) => {
+  return (
+    <TableViewCellContainerButton onClick={onClick}>
+      <TableViewCellTitle>{children}</TableViewCellTitle>
+      {iconPath != null && <Icon path={iconPath} />}
+    </TableViewCellContainerButton>
+  )
+}
+
+export default TableViewCell
