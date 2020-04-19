94d830512da0efebb4a21ed9cff03d21a4a653f5
Junyoung Choi <rokt33r.choi@gmail.com>
2020-04-20 03:13:47 +0900

    Fix scrolling of StorageCreatePage

--- src/components/pages/StorageCreatePage.tsx
+++ src/components/pages/StorageCreatePage.tsx
@@ -16,39 +16,37 @@ export default () => {
   const [storageType, setStorageType] = useState<'cloud' | 'local'>('cloud')
 
   return (
-    <div>
-      <PageContainer>
-        <FormHeading depth={1}>{t('Create new storage')}</FormHeading>
-        <FormGroup>
-          <FormLabel>Storage Type</FormLabel>
-          <FormCheckList>
-            <FormCheckInlineItem
-              id='radio-cloudStorageType'
-              type='radio'
-              checked={storageType === 'cloud'}
-              onChange={() => {
-                setStorageType('cloud')
-              }}
-            >
-              {t('storage.typeCloud')}
-            </FormCheckInlineItem>
-            <FormCheckInlineItem
-              id='radio-localStorageType'
-              type='radio'
-              checked={storageType === 'local'}
-              onChange={() => setStorageType('local')}
-            >
-              {t('storage.typeLocal')}
-            </FormCheckInlineItem>
-          </FormCheckList>
-        </FormGroup>
+    <PageContainer>
+      <FormHeading depth={1}>{t('Create new storage')}</FormHeading>
+      <FormGroup>
+        <FormLabel>Storage Type</FormLabel>
+        <FormCheckList>
+          <FormCheckInlineItem
+            id='radio-cloudStorageType'
+            type='radio'
+            checked={storageType === 'cloud'}
+            onChange={() => {
+              setStorageType('cloud')
+            }}
+          >
+            {t('storage.typeCloud')}
+          </FormCheckInlineItem>
+          <FormCheckInlineItem
+            id='radio-localStorageType'
+            type='radio'
+            checked={storageType === 'local'}
+            onChange={() => setStorageType('local')}
+          >
+            {t('storage.typeLocal')}
+          </FormCheckInlineItem>
+        </FormCheckList>
+      </FormGroup>
 
-        {storageType === 'local' ? (
-          <LocalStorageCreateForm />
-        ) : (
-          <CloudStorageCreateForm />
-        )}
-      </PageContainer>
-    </div>
+      {storageType === 'local' ? (
+        <LocalStorageCreateForm />
+      ) : (
+        <CloudStorageCreateForm />
+      )}
+    </PageContainer>
   )
 }
