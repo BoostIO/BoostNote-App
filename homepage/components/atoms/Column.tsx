b79969baa472372b94383eb055d843f9c353262b
Junyoung Choi <rokt33r.choi@gmail.com>
2020-05-07 06:22:03 +0900

    Improve Column padding style

--- homepage/components/atoms/Column.tsx
+++ homepage/components/atoms/Column.tsx
@@ -1,14 +1,19 @@
 import styled from '../../lib/styled'
-import { space, layout, flexbox } from 'styled-system'
+import {
+  space,
+  layout,
+  flexbox,
+  LayoutProps,
+  SpaceProps,
+  FlexboxProps,
+} from 'styled-system'
 
-const Column = styled.div`
+const Column = styled.div<SpaceProps & LayoutProps & FlexboxProps>`
+  padding-left: ${({ theme }) => theme.space[3]}px;
+  padding-right: ${({ theme }) => theme.space[3]}px;
   ${space}
   ${layout}
   ${flexbox}
-
-  box-sizing: border-box;
-  padding-left: ${({ theme }) => theme.space[3]}px;
-  padding-right: ${({ theme }) => theme.space[3]}px;
 `
 
 export default Column
