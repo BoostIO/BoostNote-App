98cee296de02cfbbf1f9d49cbb201f5cd8835dbf
Junyoung Choi <rokt33r.choi@gmail.com>
2020-05-07 06:22:48 +0900

    Add PricingPlansSection

--- homepage/pages/index.tsx
+++ homepage/pages/index.tsx
@@ -3,6 +3,7 @@ import DefaultLayout from '../components/DefaultLayout'
 import HeroSection from '../components/organisms/HeroSection'
 import RoadmapSection from '../components/organisms/RoadmapSection'
 import FeaturesSection from '../components/organisms/FeaturesSection'
+import PricingPlansSection from '../components/organisms/PricingPlansSection'
 import BoostHubSection from '../components/organisms/BoostHubSection'
 import LegacyAppSection from '../components/organisms/LegacyAppSection'
 import CommunitySection from '../components/organisms/CommunitySection'
@@ -13,6 +14,7 @@ const HomePage = () => {
       <HeroSection />
       <RoadmapSection />
       <FeaturesSection />
+      <PricingPlansSection />
       <BoostHubSection />
       <LegacyAppSection />
       <CommunitySection />
