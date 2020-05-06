99848f50f38111ccbbcb6c54a8ac1cfc5aae346e
Junyoung Choi <rokt33r.choi@gmail.com>
2020-05-07 05:42:59 +0900

    Fix viewport

--- homepage/pages/_document.tsx
+++ homepage/pages/_document.tsx
@@ -48,11 +48,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charSet='utf-8' />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1, maximum-scale=1'
-          />
           <script
             async
             src='https://www.googletagmanager.com/gtag/js?id=UA-50038801-5'
