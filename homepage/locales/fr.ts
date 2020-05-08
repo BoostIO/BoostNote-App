785673e9c024b2569c208450f2b9e5652f51d0f6
Améthyste <brewal_derouet@hotmail.fr>
2020-05-08 02:27:39 +0200

    Add french translation for homepage

--- homepage/locales/fr.ts
+++ homepage/locales/fr.ts
@@ -2,75 +2,82 @@ export default {
   translation: {
     common: {
       openInBrowser: 'Ouvrir dans le navigateur',
-      downloadApp: 'Télécharger App',
-      // fileSystemBasedStorage: 'File System Based Storage',
-      // comingSoon: 'Coming Soon',
+      downloadApp: "Télécharger l'application",
+      fileSystemBasedStorage: 'Stockage basé sur un système de fichiers',
+      comingSoon: 'Prochaînement',
       webApp: 'Application Web',
-      desktopApp: 'Application de Bureau',
+      desktopApp: 'Application Desktop',
       mobileApp: 'Application Mobile',
     },
     header: {
-      forTeams: "Pour l'équipe",
+      forTeams: 'Pour les équipes',
     },
     hero: {
-      // title: 'Boost Happiness, Productivity and Creativity.',
-      // subtitle:
-      //   "An intuitive and stylish markdown note app for developers. It's fully open-source.",
+      title: 'Stimuler le bonheur, la productivité et la créativité.',
+      subtitle:
+        'Une application de prise de notes Markdown intuitive et élégante pour les développeurs, le tout entièrement open-source.',
     },
     boostHub: {
-      // title: 'Boost Note for Teams',
-      // description1:
-      //   'We\'ll launch the clean and simple wiki specially optimized for developers called "Boost Hub" at June 2020!',
-      // description2:
-      //   'Boost Hub will aim to be a collaborative wiki tool for teams to centralize and amplify the availability and search ability of both first-party and third-party information.',
+      title: 'Boost Note pour les équipes',
+      description1:
+        'Nous lancerons le wiki propre et simple spécialement optimisé pour les développeurs appelé "Boost Hub" en juin 2020 !',
+      description2:
+        "Boost Hub se veut être un outil de wiki collaboratif permettant aux équipes de centraliser et d'amplifier la disponibilité et la capacité de recherche des informations de sources directes et tiers",
     },
     features: {
-      // title: 'Features',
-      // cloudStorage: 'Cloud Storage',
-      // cloudStorageDescription:
-      //   'Notes in a cloud storage will be stored safely and accessible from other devices.',
-      // multiplePlatforms: 'Multiple Platforms',
-      // multiplePlatformsDescription:
-      //   'Boost Note app is available in browsers, desktop app and mobile app.',
-      // syntaxHighlight: 'Syntax Highlight',
-      // syntaxHighlightDescription:
-      //   'Boost Note can highlight more than 100 programming languages.',
-      // mathEquations: 'Math Equations',
-      // mathEquationsDescription:
-      //   'Boost Note supports math blocks. In the blocks, you can write math equations with LaTeX syntax.',
-      // customizableTheme: 'Customizable Theme',
-      // customizableThemeDescription:
-      //   'You can customize style of the app UI, its editor and rendered markdown contents.',
-      // fileSystemBasedStorageDescription:
-      //   'You can have full control of your data. Share your notes with your favorite cloud storage service.',
-      // extensibleMarkdown: 'Extensible Markdown',
-      // extensibleMarkdownDescription:
-      //   'You can introduce custom markdown syntax and configure how to render it.',
+      title: 'Fonctionnalités',
+      cloudStorage: 'Stockage en ligne',
+      cloudStorageDescription:
+        "Les notes dans un stockage en ligne seront stockées en toute sécurité et accessibles depuis d'autres appareils.",
+      multiplePlatforms: 'Multiplateforme',
+      multiplePlatformsDescription:
+        "L'application Boost Note est disponible sur navigateur, desktop et mobile.",
+      syntaxHighlight: 'Coloration syntaxique',
+      syntaxHighlightDescription:
+        'Boost Note reconnait plus de 100 langages de programmation.',
+      mathEquations: 'Équations mathématiques',
+      mathEquationsDescription:
+        'Boost Note prend en charge les blocs mathématiques. Dans les blocs, vous pouvez écrire des équations mathématiques avec la syntaxe LaTeX.',
+      customizableTheme: 'Thème personnalisable',
+      customizableThemeDescription:
+        "Vous pouvez personnaliser l'interface utilisateur de l'application, son éditeur et le rendu du contenu Markdown.",
+      fileSystemBasedStorageDescription:
+        'Vous pouvez avoir un total contrôle de vos données. Partagez vos notes avec votre service de stockage en ligne préféré.',
+      extensibleMarkdown: 'Markdown extensible',
+      extensibleMarkdownDescription:
+        'Vous pouvez introduire une syntaxe Markdown personnalisée et configurer son rendu.',
     },
     pricing: {
-      // title: 'Pricing Plans',
+      title: 'Tarifs',
       feature: 'Fonctionnalité',
       basic: 'Basique',
       premium: 'Premium',
-      sync: 'Synchroniser multiples appareils',
-      localStorageSize: 'Taille de stockage local',
+      sync: 'Synchroniser plusieurs appareils',
+      localStorageSize: 'Taille de stockage en local',
       unlimited: 'Illimitée',
       cloudStorageSize: 'Taille de stockage en ligne',
-      // price: 'Price',
-      // free: 'Free',
+      price: 'Prix',
+      free: 'Gratuit',
       month: 'Mois',
-      furtherPlan: 'Après cela, 5$/mois pour chaque tranche de 5Go.',
+      furtherPlan: 'Après cela, 5$/mois par tranche de 5Go.',
+    },
+    download: {
+      legacyApp: 'Ancienne application',
+      legacyAppDescription:
+        "Nous allons continuer à maintenir l'ancienne application jusqu'à ce que l'actuel Boost Note.next supporte la plupart des fonctionnalités de l'ancienne application comme le stockage basé sur le système de fichiers et les extensions Markdown. Ne vous forcez donc pas trop à migrer vers la nouvelle application.",
+      legacyDownloadLinks: "Liens de l'ancienne application",
+      legacyRepository: "Dépôt de l'ancienne application",
     },
     community: {
       title: 'Faites partie de notre communauté',
       description:
-        'Notre communauté de développeurs soutient BoostNote. Elle a gagné une grande attention venant de Github et est supportée par un grand nombre de contributeur. Nous serions ravis de vous avoir parmi nous, joignez nous!',
+        'Notre communauté de développeurs soutient Boost Note. Elle a gagné une grande attention venant de Github et est supportée par un grand nombre de contributeurs. Nous serions ravis de vous avoir parmi nous, alors rejoignez-nous !',
     },
     footer: {
-      // mission: 'Mission',
-      // pressKit: 'Press Kit',
-      // userTerms: 'User Terms',
-      // privacyPolicy: 'Privacy Policy',
+      mission: 'Mission',
+      pressKit: 'Kit de presse',
+      userTerms: "Conditions d'utilisation",
+      privacyPolicy: 'Politique de confidentialité',
     },
   },
 }
