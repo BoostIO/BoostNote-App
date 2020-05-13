89c68a6cde71b29a28a2279090bbded57c7b7452
ZeroX-DG <viethungax@gmail.com>
2020-05-13 14:13:13 +1200

    added vietnamese translation for homepage

---
+++ homepage/locales/vn.ts
@@ -0,0 +1,83 @@
+export default {
+  translation: {
+    common: {
+      openInBrowser: 'Mở trong trình duyệt',
+      downloadApp: 'Tải ứng dụng',
+      fileSystemBasedStorage: 'Lưu trữ trên file',
+      comingSoon: 'Sắp ra mắt',
+      webApp: 'Ứng dụng web',
+      desktopApp: 'Ứng dụng desktop',
+      mobileApp: 'Ứng dụng điện thoại',
+    },
+    header: {
+      forTeams: 'Dành cho teams',
+    },
+    hero: {
+      title: 'Tăng sự hài lòng, năng suất và sáng tạo.',
+      subtitle:
+        "Một ứng dụng ghi chú markdown hiệu quả cho lập trình viên và hoàn toàn open-source.",
+    },
+    boostHub: {
+      title: 'Boost Note dành cho teams',
+      description1:
+        'Chúng tôi sẽ cho ra mắt 1 ứng dụng wiki đơn giản dành riêng cho lập trình viên, gọi là "BoostHub" vào tháng 6 năm 2020!',
+      description2:
+        'BoostHub hướng đến việc trở thành 1 công cụ wiki cho phép các team tương tác với nhau. Công cụ này sẽ giúp các teams tập trung mọi dữ liệu và cung cấp khả năng tìm kiếm trên dữ liệu của cả bên thứ nhất và bên thứ ba.',
+    },
+    features: {
+      title: 'Các chức năng',
+      cloudStorage: 'Lưu trữ đám mây',
+      cloudStorageDescription:
+        'Mọi ghi chú sẽ được lưu trữ an toàn trên đám mây và có thể truy cập trên mọi thiết bị.',
+      multiplePlatforms: 'Đa nền tảng',
+      multiplePlatformsDescription:
+        'Ứng dụng Boost Note có thể sự dụng dưới dạng: ứng dụng web, ứng dụng desktop và ứng dụng điện thoại.',
+      syntaxHighlight: 'Syntax Highlight',
+      syntaxHighlightDescription:
+        'Boost Note hỗ trợ highlight hơn 100 ngôn ngữ lập trình.',
+      mathEquations: 'Phương trình toán học',
+      mathEquationsDescription:
+        'Boost Note hỗ trợ hiển thị phương trình toán học. Bạn có thể viết phương trình toán học lên ghi chú với cú pháp của LaTeX.',
+      customizableTheme: 'Tuỳ chỉnh giao diện',
+      customizableThemeDescription:
+        'Bạn có thể tuỳ chỉnh giao diện của ứng dụng, ô soạn thảo của ứng dụng và cả giao diện hiển thị Markdown.',
+      fileSystemBasedStorageDescription:
+        'Bạn sẽ có toàn quyền kiểm soát ghi chú của bạn. Bạn có thể lưu trữ ghi chú với dịch vụ lưu trữ đám mây mà bạn yêu thích.',
+      extensibleMarkdown: 'Mở rộng Markdown',
+      extensibleMarkdownDescription:
+        'Bạn có thể thêm những cú pháp Markdown của riêng bạn và tuỳ chỉnh chúng được hiển thị như thế nào.',
+    },
+    pricing: {
+      title: 'Gói dịch vụ',
+      feature: 'Chức năng',
+      basic: 'Basic',
+      premium: 'Premium',
+      sync: 'Đồng bộ hoá đa nền tảng',
+      localStorageSize: 'Local Storage Size',
+      unlimited: 'Không giới hạn',
+      cloudStorageSize: 'Dung lượng lưu trữ đám mây',
+      price: 'Giá',
+      free: 'Miễn phí',
+      month: 'Tháng',
+      furtherPlan: 'Sau đó, $5 (USD) cho mỗi 5GB.',
+    },
+    download: {
+      legacyApp: 'Phiên bản cũ',
+      legacyAppDescription:
+        "Chúng tôi sẽ tiếp tục duy trì phiên bản cũ cho đến khi phiên bản mới là Boost Note.Next hoàn thiện các chức năng mà phiên bản cũ hỗ trợ như: lưu trữ trên file và tiện ích cho markdown. Vì thế bạn chưa cần phải chuyển hoàn toàn sang phiên bản mới.",
+      legacyDownloadLinks: 'Links download phiên bản cũ',
+      legacyRepository: 'Repository phiên bản cũ',
+    },
+    community: {
+      title: 'Tham gia cộng đồng chúng tôi',
+      description:
+        'Chúng tôi có công đồng lập trình viên lớn mạnh đằng sau Boostnote. Cộng đồng chúng tôi có được rất nhiều sự chú ý trên Github và được hỗ trợ bởi đông đảo lập trình viên khác trên hơn 200 quốc gia và vùng lãnh thổ. Chúng tôi luôn luôn hoan nghênh bạn. Hãy tham gia cũng chúng tôi!',
+    },
+    footer: {
+      mission: 'Sứ mệnh',
+      pressKit: 'Press Kit',
+      userTerms: 'Điều khoản người dùng',
+      privacyPolicy: 'Quy định về quyền riêng tư',
+    },
+  },
+}
