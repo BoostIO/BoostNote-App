5c46c35cea15edc96cffa7c9e8e308979dc2ec9d
Junyoung Choi <rokt33r.choi@gmail.com>
2020-06-19 04:19:21 +0900

    Stop wrapping db store for desktop app

--- src/lib/db/store.ts
+++ src/lib/db/store.ts
@@ -2,19 +2,16 @@ import { createStoreContext } from '../context'
 import { localLiteStorage } from 'ltstrg'
 
 import { useRouter, usePathnameWithoutNoteId } from '../router'
-import { wrapDbStoreWithAnalytics } from '../analytics'
 import { createDbStoreCreator } from './createStore'
 
 export const {
   StoreProvider: DbProvider,
   useStore: useDb,
 } = createStoreContext(
-  wrapDbStoreWithAnalytics(
-    createDbStoreCreator(
-      localLiteStorage,
-      useRouter,
-      usePathnameWithoutNoteId,
-      'idb'
-    )
+  createDbStoreCreator(
+    localLiteStorage,
+    useRouter,
+    usePathnameWithoutNoteId,
+    'idb'
   )
 )
