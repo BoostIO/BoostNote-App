7138dd8be14854d7a8a9273a4a705082fb546248
Elle Kasai <elle.kasai@gmail.com>
2020-07-20 20:59:46 +0900

    Added Header Alert

--- homepage/components/atoms/HomeLogoLink.tsx
+++ homepage/components/atoms/HomeLogoLink.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
+import styled from 'styled-components'
+
+const HomeLogoImg = styled.a`
+  display: block;
+
+  img {
+    vertical-align: middle;
+  }
+`
 
 const HomeLogoLink = () => {
   return (
     <Link href='/'>
-      <a>
+      <HomeLogoImg>
         <img alt='Boost Note Logo' src='/static/logo.svg' />
-      </a>
+      </HomeLogoImg>
     </Link>
   )
 }
