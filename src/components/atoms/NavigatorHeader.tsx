23629590745d4e5e92ec8febd4bb0732ce7761fb
Junyoung Choi <rokt33r.choi@gmail.com>
2020-07-26 14:40:14 +0900

    Implement BookmarkNavFragment

--- src/components/atoms/NavigatorHeader.tsx
+++ src/components/atoms/NavigatorHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from '../../lib/styled'
-import { textOverflow } from '../../lib/styled/styleFunctions'
+import { textOverflow, borderBottom } from '../../lib/styled/styleFunctions'
+import Icon from './Icon'
+import { mdiChevronRight, mdiChevronDown } from '@mdi/js'
 
 const HeaderContainer = styled.header`
   position: relative;
@@ -10,11 +12,13 @@ const HeaderContainer = styled.header`
   justify-content: space-between;
   font-weight: bold;
   align-items: center;
+  background-color: ${({ theme }) => theme.inputBackground};
+  ${borderBottom}
 `
 
 const Label = styled.div`
-  padding: 0 0 0 0.5em;
   color: ${({ theme }) => theme.navLabelColor};
+  flex: 1;
   ${textOverflow}
 `
 
@@ -22,20 +26,35 @@ const Control = styled.div`
   display: flex;
 `
 
+const ClickableContainer = styled.div`
+  display: flex;
+  flex: 1;
+  cursor: pointer;
+`
+
 interface NavigatorHeaderProps {
   label: string
   control?: React.ReactNode
-  onContextMenu: React.MouseEventHandler<HTMLDivElement>
+  onContextMenu?: React.MouseEventHandler<HTMLDivElement>
+  folded?: boolean
+  onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
 const NavigatorHeader = ({
+  folded,
   label,
   onContextMenu,
+  onClick,
   control,
 }: NavigatorHeaderProps) => {
   return (
     <HeaderContainer onContextMenu={onContextMenu}>
-      <Label>{label}</Label>
+      <ClickableContainer onClick={onClick}>
+        {folded != null && (
+          <Icon path={folded ? mdiChevronRight : mdiChevronDown} size={18} />
+        )}
+        <Label>{label}</Label>
+      </ClickableContainer>
       {control && <Control>{control}</Control>}
     </HeaderContainer>
   )
---
+++ src/components/molecules/BookmarkNavigatorFragment.tsx
@@ -0,0 +1,108 @@
+import React, { useMemo, useCallback } from 'react'
+import { NoteDoc, NoteStorage } from '../../lib/db/types'
+import { useDb } from '../../lib/db'
+import NavigatorItem from '../atoms/NavigatorItem'
+import NavigatorButton from '../atoms/NavigatorButton'
+import NavigatorHeader from '../atoms/NavigatorHeader'
+import { mdiTextBoxOutline, mdiClose } from '@mdi/js'
+import { useRouter, useRouteParams } from '../../lib/router'
+import { useGeneralStatus } from '../../lib/generalStatus'
+import styled from '../../lib/styled'
+import { borderBottom } from '../../lib/styled/styleFunctions'
+
+const navItemName = 'bookmarks'
+
+const Border = styled.div`
+  height: 1px;
+  box-sizing: border-box;
+  ${borderBottom};
+`
+
+interface BookmarkNavigatorFragmentProps {
+  storageEntries: [string, NoteStorage][]
+}
+
+const BookmarkNavigatorFragment = ({
+  storageEntries,
+}: BookmarkNavigatorFragmentProps) => {
+  const { unbookmarkNote } = useDb()
+  const { push } = useRouter()
+  const { sideNavOpenedItemSet, toggleSideNavOpenedItem } = useGeneralStatus()
+  const opened = sideNavOpenedItemSet.has(navItemName)
+
+  const toggleBookmarks = useCallback(() => {
+    toggleSideNavOpenedItem(navItemName)
+  }, [toggleSideNavOpenedItem])
+
+  const params = useRouteParams()
+
+  const bookmarkedList = useMemo(() => {
+    return storageEntries.reduce<[string, NoteDoc][]>((list, entry) => {
+      const storage = entry[1]
+      const bookmarkedNoteList = storage.bookmarkedItemIds
+        .map((id) => {
+          return storage.noteMap[id]
+        })
+        .filter((note) => note != null) as NoteDoc[]
+
+      return [
+        ...list,
+        ...bookmarkedNoteList.map(
+          (note) => [storage.id, note] as [string, NoteDoc]
+        ),
+      ]
+    }, [])
+  }, [storageEntries])
+
+  if (bookmarkedList.length === 0) {
+    return null
+  }
+
+  return (
+    <>
+      <NavigatorHeader
+        label='Bookmarks'
+        folded={!opened}
+        onClick={toggleBookmarks}
+      />
+      {opened && (
+        <>
+          {bookmarkedList.map(([storageId, note]) => {
+            const active =
+              params.name === 'storages.notes' &&
+              params.storageId === storageId &&
+              params.noteId === note._id
+            return (
+              <NavigatorItem
+                iconPath={mdiTextBoxOutline}
+                depth={0}
+                key={storageId + note._id}
+                label={note.title}
+                active={active}
+                onClick={() => {
+                  push(
+                    note.folderPathname === '/'
+                      ? `/app/storages/${storageId}/notes/${note._id}`
+                      : `/app/storages/${storageId}/notes${note.folderPathname}/${note._id}`
+                  )
+                }}
+                control={
+                  <NavigatorButton
+                    iconPath={mdiClose}
+                    title='Unbookmark'
+                    onClick={() => {
+                      unbookmarkNote(storageId, note._id)
+                    }}
+                  />
+                }
+              />
+            )
+          })}
+          <Border />
+        </>
+      )}
+    </>
+  )
+}
+
+export default BookmarkNavigatorFragment
