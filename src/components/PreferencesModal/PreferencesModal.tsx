e24fa0b0bb83922c9dae0d20a700491f0d576a93
Junyoung Choi <rokt33r.choi@gmail.com>
2020-07-26 20:29:06 +0900

    Improve PreferencesModal layout

--- src/components/PreferencesModal/PreferencesModal.tsx
+++ src/components/PreferencesModal/PreferencesModal.tsx
@@ -12,55 +12,87 @@ import ImportTab from './ImportTab'
 import {
   backgroundColor,
   closeIconColor,
+  borderLeft,
+  border,
+  flexCenter,
+  borderBottom,
 } from '../../lib/styled/styleFunctions'
 import { useTranslation } from 'react-i18next'
 import Icon from '../atoms/Icon'
-import { mdiClose } from '@mdi/js'
+import { mdiClose, mdiHammerWrench } from '@mdi/js'
 
-const contentWidth = 1070
-
-const Container = styled.div`
+const FullScreenContainer = styled.div`
   z-index: 7000;
   position: absolute;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  ${backgroundColor}
-  overflow: auto;
-  padding: 0 calc((100% - ${contentWidth}px) / 2);
 `
 
-const Header = styled.h1`
+const BackgroundShadow = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+`
+
+const ContentContainer = styled.div`
+  z-index: 7001;
+  position: relative;
+  height: calc(100% - 2em);
+  width: calc(100%-2em);
+  display: flex;
+  flex-direction: column;
+  max-width: 1080px;
+  margin: 1em auto;
+  ${backgroundColor}
+  ${border}
+`
+
+const ModalHeader = styled.div`
+  height: 40px;
+  ${borderBottom}
+  display: flex;
+`
+
+const ModalTitle = styled.h1`
   margin: 0;
-  padding: 1em 0;
+  line-height: 40px;
+  font-size: 1em;
   font-weight: bold;
+  padding: 0 10px;
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+
+const ModalBody = styled.div`
+  display: flex;
+  overflow: hidden;
 `
 
 const TabNav = styled.nav`
   width: 200px;
-  position: fixed;
 `
 
 const TabContent = styled.div`
   flex: 1;
   overflow-y: auto;
-  padding-left: 4px;
-  padding-top: 4em;
-  position: relative;
-  left: 200px;
-  width: ${contentWidth - 200}px;
+  padding: 1em;
+
+  ${borderLeft}
 `
 
 const CloseButton = styled.button`
-  position: fixed;
-  top: 20px;
-  right: calc((100% - ${contentWidth}px) / 2 - 40px);
   width: 40px;
   height: 40px;
   background-color: transparent;
   border: none;
   font-size: 24px;
+  ${flexCenter}
   ${closeIconColor}
 `
 
@@ -101,57 +133,67 @@ const PreferencesModal = () => {
   }
 
   return (
-    <Container>
-      <TabNav>
-        <Header>{t('preferences.general')}</Header>
-        <TabButton
-          label='General'
-          tab='general'
-          active={tab === 'general'}
-          setTab={setTab}
-        />
-        <TabButton
-          label='Editor'
-          tab='editor'
-          active={tab === 'editor'}
-          setTab={setTab}
-        />
-        <TabButton
-          label='Markdown'
-          tab='markdown'
-          active={tab === 'markdown'}
-          setTab={setTab}
-        />
-        {/* <TabButton
+    <FullScreenContainer>
+      <ContentContainer>
+        <ModalHeader>
+          <ModalTitle>
+            <Icon size={24} path={mdiHammerWrench} />
+            {t('preferences.general')}
+          </ModalTitle>
+          <CloseButton onClick={toggleClosed}>
+            <Icon path={mdiClose} />
+          </CloseButton>
+        </ModalHeader>
+        <ModalBody>
+          <TabNav>
+            <TabButton
+              label='General'
+              tab='general'
+              active={tab === 'general'}
+              setTab={setTab}
+            />
+            <TabButton
+              label='Editor'
+              tab='editor'
+              active={tab === 'editor'}
+              setTab={setTab}
+            />
+            <TabButton
+              label='Markdown'
+              tab='markdown'
+              active={tab === 'markdown'}
+              setTab={setTab}
+            />
+            {/* <TabButton
           label='Hotkeys'
           tab='hotkeys'
           active={tab === 'hotkeys'}
           setTab={setTab}
         /> */}
-        <TabButton
-          label='About'
-          tab='about'
-          active={tab === 'about'}
-          setTab={setTab}
-        />
-        <TabButton
-          label='Billing'
-          tab='billing'
-          active={tab === 'billing'}
-          setTab={setTab}
-        />
-        <TabButton
-          label='Import'
-          tab='import'
-          active={tab === 'import'}
-          setTab={setTab}
-        />
-      </TabNav>
-      <TabContent>{content}</TabContent>
-      <CloseButton onClick={toggleClosed}>
-        <Icon path={mdiClose} />
-      </CloseButton>
-    </Container>
+            <TabButton
+              label='About'
+              tab='about'
+              active={tab === 'about'}
+              setTab={setTab}
+            />
+            <TabButton
+              label='Billing'
+              tab='billing'
+              active={tab === 'billing'}
+              setTab={setTab}
+            />
+            <TabButton
+              label='Import'
+              tab='import'
+              active={tab === 'import'}
+              setTab={setTab}
+            />
+          </TabNav>
+          <TabContent>{content}</TabContent>
+        </ModalBody>
+      </ContentContainer>
+      <BackgroundShadow onClick={toggleClosed} />
+    </FullScreenContainer>
   )
 }
 
