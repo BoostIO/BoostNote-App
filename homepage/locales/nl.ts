7ed2dd8cfb3b6d0d7e2b1f396de45ed450839cfd
Laurens Deprost <ldeprost@gmail.com>
2020-08-22 21:51:13 +0200

    chore: Update Dutch homepage locale

--- homepage/locales/nl.ts
+++ homepage/locales/nl.ts
@@ -3,81 +3,88 @@ export default {
     common: {
       openInBrowser: 'Open in Browser',
       downloadApp: 'Download App',
-      // fileSystemBasedStorage: 'File System Based Storage',
-      // comingSoon: 'Coming Soon',
+      fileSystemBasedStorage: 'Lokale Opslag',
+      comingSoon: 'Binnenkort Beschikbaar',
       webApp: 'Web App',
       desktopApp: 'Desktop App',
       mobileApp: 'Mobiele App',
-      boostHub: 'Learn More About Boost Hub',
+      boostHub: 'Meer Info Over Boost Hub',
     },
     header: {
       forTeams: 'Voor Teams',
     },
     hero: {
-      // title: 'Boost Happiness, Productivity and Creativity.',
-      // subtitle:
-      //   "An intuitive and stylish markdown note app for developers. It's fully open-source.",
+      title: 'Boost Geluk, Productiviteit en Creativiteit.',
+      subtitle:
+        "Een intuïtieve en stijlvolle markdown notitie-app voor ontwikkelaars. Volledig open-source.",
     },
     boostHub: {
-      // title: 'Do you know Boost Hub?',
-      // description:
-      //   "We've developed this workspace app as another project, and we focused on the features to make it customizable and easy to optimize for every developer team.",
-      // feature1Name: 'Custom Blocks',
-      // feature1Detail: 'Display and interact the third party info',
-      // feature2Name: 'Real-time Coauthoring',
-      // feature2Detail: 'Edit documents with team members',
-      // feature3Name: 'Versatile Markdown',
-      // feature3Detail: 'Supporting LaTex, PlantUML, etc.',
-      // feature4Name: 'Keyboard Shortcuts',
-      // feature4Detail: 'More than 20 shortcuts are prepared',
+      title: 'Kent u Boost Hub al?',
+      description:
+        "We hebben deze samenwerkingsapp ontwikkeld als een apart project, en we hebben ons hierbij gericht op functies die zorgen voor een geheel dat makkelijk aanpasbaar en eenvoudig te optimaliseren is voor elk team van ontwikkelaars.",
+      feature1Name: 'Custom blokken',
+      feature1Detail: 'Weergave van- en interactie met externe informatie',
+      feature2Name: 'Real-time Samenwerken',
+      feature2Detail: 'Bewerk documenten gelijktijdig met teamleden',
+      feature3Name: 'Veelzijdige Markdown',
+      feature3Detail: 'Ondersteund LaTex, PlantUML, enz.',
+      feature4Name: 'Sneltoetsen',
+      feature4Detail: 'Meer dan 20 sneltoetsen beschikbaar',
     },
     features: {
-      // title: 'Features',
-      // cloudStorage: 'Cloud Storage',
-      // cloudStorageDescription:
-      //   'Notes in a cloud storage will be stored safely and accessible from other devices.',
-      // multiplePlatforms: 'Multiple Platforms',
-      // multiplePlatformsDescription:
-      //   'Boost Note app is available in browsers, desktop app and mobile app.',
-      // syntaxHighlight: 'Syntax Highlight',
-      // syntaxHighlightDescription:
-      //   'Boost Note can highlight more than 100 programming languages.',
-      // mathEquations: 'Math Equations',
-      // mathEquationsDescription:
-      //   'Boost Note supports math blocks. In the blocks, you can write math equations with LaTeX syntax.',
-      // customizableTheme: 'Customizable Theme',
-      // customizableThemeDescription:
-      //   'You can customize style of the app UI, its editor and rendered markdown contents.',
-      // fileSystemBasedStorageDescription:
-      //   'You can have full control of your data. Share your notes with your favorite cloud storage service.',
-      // extensibleMarkdown: 'Extensible Markdown',
-      // extensibleMarkdownDescription:
-      //   'You can introduce custom markdown syntax and configure how to render it.',
+      title: 'Kenmerken',
+      cloudStorage: 'Cloudopslag',
+      cloudStorageDescription:
+        'Notities in de cloud worden veilig opgeslagen en zijn toegankelijk vanaf andere apparaten.',
+      multiplePlatforms: 'Meerdere platforms',
+      multiplePlatformsDescription:
+        'De Boost Note-app is beschikbaar in browsers, op de desktop en als een mobiele app.',
+      syntaxHighlight: 'Syntax Highlighting',
+      syntaxHighlightDescription:
+        'Boost Note ondersteund syntax highlighting in meer dan 100 programmeertalen.',
+      mathEquations: 'Wiskundige Vergelijkingen',
+      mathEquationsDescription:
+        'Boost Note ondersteunt wiskunde-blokken. In deze blokken kunt u wiskundige vergelijkingen schrijven met de LaTeX-syntax.',
+      customizableTheme: 'Aanpasbaar Thema',
+      customizableThemeDescription:
+        'De stijl van de gebruikersinterface van de app, de editor en de weergegeven markdown-inhoud zijn volledig aanpasbaar.',
+      fileSystemBasedStorageDescription:
+        'U heeft de volledige controle over uw gegevens. Deel uw notities met uw favoriete cloudopslagservice.',
+      extensibleMarkdown: 'Uitbreidbare Markdown',
+      extensibleMarkdownDescription:
+        'U kunt aangepaste markdown-syntax introduceren en configureren hoe deze gerendered wordt.',
     },
     pricing: {
-      // title: 'Pricing Plans',
-      feature: 'Feature',
+      title: 'Tariefplannen',
+      feature: 'Kenmerk',
       basic: 'Basis',
       premium: 'Premium',
       sync: 'Synchroniseer tussen meerdere apparaten',
       localStorageSize: 'Lokale opslagruimte',
       unlimited: 'Ongelimiteerd',
-      cloudStorageSize: 'Cloud opslagruimte',
-      // price: 'Price',
-      // free: 'Free',
-      month: 'Mnd',
-      furtherPlan: 'Daarna, $5,00 voor elke 5 GB.',
+      cloudStorageSize: 'Cloudopslagruimte',
+      price: 'Prijs',
+      free: 'Gratis',
+      month: 'Maand',
+      furtherPlan: 'Daarna $5 (USD) voor elke 5GB.',
+    },
+    download: {
+      legacyApp: 'Legacy App',
+      legacyAppDescription:
+        "We blijven de oude app onderhouden totdat de huidige Boost Note.next het merendeel van de functies van de oude app ondersteunt, zoals lokale opslag en markdown-extensies. Push uzelf dus niet te hard om naar de nieuwe app te migreren.",
+      legacyDownloadLinks: 'Legacy Downloadlinks',
+      legacyRepository: 'Legacy Repository',
     },
     community: {
       title: 'Word onderdeel van onze community',
       description:
-        'Onze community van developers ondersteunt Boostnote. Boostnote heeft aanzienlijke populariteit op GitHub en wordt door vele programmeurs onderhouden. Onze community verwelkomt iedereen. Doe mee!',
+        "Onze gemeenschap van developers ondersteunt Boostnote. Boostnote geniet van aanzienlijke populariteit op GitHub en wordt door vele programmeurs onderhouden. Onze community verwelkomt iedereen. Doe mee!",
     },
     footer: {
-      // mission: 'Mission',
-      // pressKit: 'Press Kit',
-      // userTerms: 'User Terms',
-      // privacyPolicy: 'Privacy Policy',
+      mission: 'Missie',
+      pressKit: 'Perskit',
+      userTerms: 'Gebruikersvoorwaarden',
+      privacyPolicy: 'Privacybeleid',
     },
   },
 }
