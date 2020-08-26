fd611c2538fca45897cb045929fdeb8b96f65a94
Junyoung Choi <rokt33r.choi@gmail.com>
2020-08-26 16:10:19 +0900

    Rollback hero section

--- homepage/components/organisms/HeroSection.tsx
+++ homepage/components/organisms/HeroSection.tsx
@@ -24,8 +24,6 @@ import {
   androidPlayStoreUrl,
   iOSAppStoreUrl,
 } from '../../lib/download'
-import Row from '../atoms/Row'
-import Column from '../atoms/Column'
 import Icon from '../atoms/Icon'
 import { mdiDownload, mdiOpenInApp } from '@mdi/js'
 import DownloadButtonLink from '../atoms/DownloadButtonLink'
@@ -33,15 +31,13 @@ import { sendGAEvent, queueNavigateToGA } from '../../lib/analytics'
 import Text from '../atoms/Text'
 import SubscribeNewsLettersForm from './SubscribeNewsLettersForm'
 
-const HeroTitle = styled.h1<TypographyProps | SpaceProps>`
+const HeroTitle = styled.h1<TypographyProps>`
   ${typography}
-  ${space}
 `
 
-const HeroSubTitle = styled.h2<TypographyProps | ColorProps | SpaceProps>`
+const HeroSubTitle = styled.h2<TypographyProps | ColorProps>`
   ${typography}
   ${color}
-  ${space}
 `
 
 const DownloadButtonLinksFragment = () => {
@@ -121,58 +117,8 @@ const MobileAppLink = styled.a<SpaceProps>`
   }
 `
 
-const HeroContents = styled.div<SpaceProps>`
-  ${space}
-  height: 100%;
-  overflow-x: hidden;
-
-  @media only screen and (min-width: 1366px) {
-    height: 650px;
-  }
-`
-
-const HeroImageMobile = styled.img`
+const HeroImage = styled.img`
   max-width: 100%;
-
-  @media only screen and (min-width: 1024px) {
-    display: none;
-  }
-`
-
-const HeroImageDesktop = styled.img`
-  display: none;
-  max-width: 100%;
-
-  @media only screen and (min-width: 1024px) {
-    display: block;
-    position: absolute;
-    right: 0;
-    margin-top: 100px;
-    height: 550px;
-  }
-
-  @media only screen and (min-width: 1280px) {
-    margin-top: 0;
-  }
-
-  @media only screen and (min-width: 1366px) {
-    height: 650px;
-  }
-`
-
-const HeroVersion = styled.span<SpaceProps | ColorProps>`
-  ${space}
-  ${color}
-`
-
-const HeroDesktopLink = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  @media only screen and (min-width: 768px) {
-    display: block;
-  }
 `
 
 const HeroSection = () => {
@@ -180,96 +126,77 @@ const HeroSection = () => {
   return (
     <section>
       <Container>
-        <HeroContents mx={[0, 5, 6]} my={[5, 5, 9]}>
-          <Row>
-            <Column width={[1, 1, 1, 1 / 2]}>
-              <Box>
-                <HeroTitle
-                  mt={[0, 5, 9]}
-                  mb={3}
-                  textAlign={['center', 'left']}
-                  fontSize={[4, 5, 6]}
-                >
-                  {t('hero.title')}
-                </HeroTitle>
-                <HeroSubTitle
-                  mb={[5, 8, 8]}
-                  textAlign={['center', 'left']}
-                  color={'gray'}
-                  fontSize={[2, 3]}
-                  fontWeight={'normal'}
-                >
-                  {t('hero.subtitle')}
-                </HeroSubTitle>
-                <FlexBox justifyContent={['center', 'left']} mt={[0, 5, 5]}>
-                  <HeroDesktopLink>
-                    <DownloadButtonLinksFragment />
-                    <ButtonLink
-                      bg='white'
-                      color='teal'
-                      mx={1}
-                      my={1}
-                      py={2}
-                      href='https://note.boostio.co'
-                      onClick={(event) => {
-                        event.preventDefault()
-                        sendGAEvent('open-in-browser')
-                        queueNavigateToGA('https://note.boostio.co')
-                      }}
-                    >
-                      <Icon path={mdiOpenInApp} /> {t('common.openInBrowser')}
-                    </ButtonLink>
-                  </HeroDesktopLink>
-                </FlexBox>
-                <FlexBox justifyContent={['center', 'left']} mt={2}>
-                  <MobileAppLink
-                    mx={1}
-                    target='_blank'
-                    href={androidPlayStoreUrl}
-                    rel='noopener noreferrer'
-                  >
-                    <img
-                      height='50'
-                      src='/static/google-play-store-badge.svg'
-                    />
-                  </MobileAppLink>
-                  <MobileAppLink
-                    mx={1}
-                    target='_blank'
-                    href={iOSAppStoreUrl}
-                    rel='noopener noreferrer'
-                  >
-                    <img height='50' src='/static/ios-app-store-badge.svg' />
-                  </MobileAppLink>
-                </FlexBox>
-                <FlexBox>
-                  <HeroVersion mx={1} my={3} color={'gray'}>
-                    Currently v0.9.0 (August, 24th)
-                  </HeroVersion>
-                </FlexBox>
-              </Box>
-              <Box>
-                <Text
-                  as='p'
-                  textAlign='center'
-                  color={'gray'}
-                  fontSize={[3]}
-                  fontWeight={'normal'}
-                >
-                  Also, please join our news letters to get the latest news and
-                  update notes.
-                </Text>
-                <SubscribeNewsLettersForm />
-              </Box>
-            </Column>
-            <Column width={[1, 1, 1, 1 / 2]}>
-              <Box>
-                <HeroImageMobile src='/static/hero-mobile.png' />
-                <HeroImageDesktop src='/static/hero-desktop.png' />
-              </Box>
-            </Column>
-          </Row>
-        </HeroContents>
+        <Box mt={[5, 5, 9]} px={3}>
+          <HeroTitle textAlign={'center'} fontSize={[4, 5, 6]}>
+            🚀 {t('hero.title')}
+          </HeroTitle>
+          <HeroSubTitle
+            textAlign={'center'}
+            color={'gray'}
+            fontSize={[3, 4]}
+            fontWeight={'normal'}
+          >
+            {t('hero.subtitle')}
+          </HeroSubTitle>
+
+          <FlexBox
+            justifyContent='center'
+            mt={5}
+            flexDirection={['column', 'row']}
+          >
+            <DownloadButtonLinksFragment />
+            <ButtonLink
+              bg='white'
+              color='teal'
+              mx={1}
+              py={2}
+              my={[1, 0]}
+              href='https://note.boostio.co'
+              onClick={(event) => {
+                event.preventDefault()
+                sendGAEvent('open-in-browser')
+                queueNavigateToGA('https://note.boostio.co')
+              }}
+            >
+              <Icon path={mdiOpenInApp} /> {t('common.openInBrowser')}
+            </ButtonLink>
+          </FlexBox>
+
+          <FlexBox justifyContent='center' mt={2}>
+            <MobileAppLink
+              mx={1}
+              target='_blank'
+              href={androidPlayStoreUrl}
+              rel='noopener noreferrer'
+            >
+              <img height='50' src='/static/google-play-store-badge.svg' />
+            </MobileAppLink>
+            <MobileAppLink
+              mx={1}
+              target='_blank'
+              href={iOSAppStoreUrl}
+              rel='noopener noreferrer'
+            >
+              <img height='50' src='/static/ios-app-store-badge.svg' />
+            </MobileAppLink>
+          </FlexBox>
+        </Box>
+        <Box>
+          <Text
+            as='p'
+            textAlign='center'
+            color={'gray'}
+            fontSize={[3]}
+            fontWeight={'normal'}
+          >
+            Also, please join our news letters to get the latest news and update
+            notes.
+          </Text>
+          <SubscribeNewsLettersForm />
+        </Box>
+        <Box>
+          <HeroImage src='/static/hero.png' />
+        </Box>
       </Container>
     </section>
   )
