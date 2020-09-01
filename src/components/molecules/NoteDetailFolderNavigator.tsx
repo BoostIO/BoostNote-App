9416a0af84c105141e6c9e690aa7fd570e602eb7
Laurens Deprost <ldeprost@gmail.com>
2020-09-01 18:44:49 +0200

    refactor: Move tooltip logic inside FolderNavItem component

--- src/components/molecules/NoteDetailFolderNavigator.tsx
+++ src/components/molecules/NoteDetailFolderNavigator.tsx
@@ -5,6 +5,7 @@ import Icon from '../atoms/Icon'
 import { useRouter, useRouteParams } from '../../lib/router'
 import { flexCenter } from '../../lib/styled/styleFunctions'
 import { useTranslation } from 'react-i18next'
+import { useDb } from '../../lib/db'
 
 const Container = styled.div`
   display: flex;
@@ -22,7 +23,7 @@ const IconContainer = styled.div`
   color: ${({ theme }) => theme.navButtonColor};
 `
 
-const FolderNavItem = styled.button`
+const FolderNavItemButton = styled.button`
   background-color: transparent;
   border: none;
   white-space: nowrap;
@@ -55,14 +56,17 @@ interface FolderData {
   pathname: string
 }
 
-const NoteDetailFolderNavigator = ({
-  storageId,
-  storageName,
-  noteId,
-  noteFolderPathname,
-}: NoteDetailFolderNavigatorProps) => {
-  const { t } = useTranslation()
+interface FolderNavItemProps {
+  path: string
+  children?: React.ReactNode
+}
 
+const FolderNavItem: React.FC<FolderNavItemProps> = ({
+  children,
+  path,
+}: FolderNavItemProps) => {
+  const { t } = useTranslation()
+  const db = useDb()
   const { push } = useRouter()
   const routeParams = useRouteParams()
 
@@ -73,6 +77,70 @@ const NoteDetailFolderNavigator = ({
     return routeParams.folderPathname
   }, [routeParams])
 
+  const parsePath = (path: string) => {
+    const result = {
+      storageId: '',
+      storageName: '',
+      folderName: '',
+      folderPath: '',
+    }
+
+    const storageMatch = /\/storages\/(.*)\/notes/.exec(path)
+    if (storageMatch) {
+      const storage = db.storageMap[storageMatch[1]]
+      if (storage) {
+        result.storageId = storage.id
+        result.storageName = storage.name
+      }
+    }
+    const folderMatch = /\/notes\/(.*)\//.exec(path)
+    if (folderMatch) {
+      result.folderPath = `/${folderMatch[1]}`
+
+      const folderNameMatch = /[^/]*$/.exec(result.folderPath)
+      if (folderNameMatch) {
+        result.folderName = folderNameMatch[0]
+      }
+    }
+    return result
+  }
+
+  const { storageName, folderName, folderPath } = parsePath(path)
+  const getStorageTooltip = (storageName: string) =>
+    `${t('storage.storage')} ${storageName}: ${t('general.allnote')}`
+
+  const getFolderTooltip = (foldername: string) =>
+    `${t('folder.folder')} ${foldername}: ${t('general.allnote')}`
+
+  const tooltip = folderName
+    ? getFolderTooltip(folderName)
+    : getStorageTooltip(storageName)
+
+  const isActive =
+    currentFolderPathname === folderPath ||
+    (!folderName && currentFolderPathname === '/')
+
+  return (
+    <FolderNavItemButton
+      title={tooltip}
+      href={path}
+      onClick={(event: MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault()
+        push(path)
+      }}
+      className={isActive ? 'active' : ''}
+    >
+      {folderName ? folderName : storageName}
+      {children}
+    </FolderNavItemButton>
+  )
+}
+
+const NoteDetailFolderNavigator = ({
+  storageId,
+  noteId,
+  noteFolderPathname,
+}: NoteDetailFolderNavigatorProps) => {
   const folderDataList = useMemo<FolderData[]>(() => {
     if (noteFolderPathname === '/') {
       return []
@@ -90,43 +158,19 @@ const NoteDetailFolderNavigator = ({
     return folderDataList
   }, [noteFolderPathname])
 
-  const storageTooltip = `${t('storage.storage')} ${storageName}: ${t('general.allnote')}`;
-
-  const getFolderTooltip = (foldername: string) =>
-    `${t('folder.folder')} ${foldername}: ${t('general.allnote')}`;
-
   return (
     <Container>
       <IconContainer>
         <Icon path={mdiBookOpen} />
       </IconContainer>
-      <FolderNavItem
-        title={storageTooltip}
-        href={`/app/storages/${storageId}/notes/${noteId}`}
-        onClick={(event: MouseEvent<HTMLAnchorElement>) => {
-          event.preventDefault()
-          push(`/app/storages/${storageId}/notes/${noteId}`)
-        }}
-        className={currentFolderPathname === '/' ? 'active' : ''}
-      >
-        {storageName}
-      </FolderNavItem>
+      <FolderNavItem path={`/app/storages/${storageId}/notes/${noteId}`} />
+
       {folderDataList.map((folderData) => (
         <React.Fragment key={folderData.pathname}>
           <Icon path={mdiSlashForward} />
           <FolderNavItem
-            title={getFolderTooltip(folderData.name)}
-            onClick={() => {
-              push(
-                `/app/storages/${storageId}/notes${folderData.pathname}/${noteId}`
-              )
-            }}
-            className={
-              currentFolderPathname === folderData.pathname ? 'active' : ''
-            }
-          >
-            {folderData.name}
-          </FolderNavItem>
+            path={`/app/storages/${storageId}/notes${folderData.pathname}/${noteId}`}
+          />
         </React.Fragment>
       ))}
     </Container>
