7ebebd2efcde94ea74164bce0fff7860075569e2
Laurens Deprost <ldeprost@gmail.com>
2020-09-12 14:16:37 +0200

    feat: Handle internal links to headings within notes

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -3,8 +3,24 @@ import { openExternal } from './electronOnly'
 
 export const appIsElectron = isElectron()
 
+const isInternalLink = (link: string) => link[0] === '#'
+
+const openInternalLink = (link: string) => {
+  const extractedId = link.slice(1)
+  const targetId = `user-content-${extractedId}`
+  const targetElement = window.document.getElementById(targetId)
+  if (targetElement != null) {
+    targetElement.scrollIntoView()
+  }
+}
+
 export const openNew = (url: string) => {
   if (url.length === 0) {
+    return
+  }
+
+  if (isInternalLink(url)) {
+    openInternalLink(url)
     return
   }
 
