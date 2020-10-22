4b70ccb1595cbfac65303705451300e695725e6e
Junyoung Choi <fluke8259@gmail.com>
2020-10-23 07:46:18 +0900

    Make PageContainer vertically scrollable

--- src/components/atoms/PageContainer.tsx
+++ src/components/atoms/PageContainer.tsx
@@ -3,7 +3,8 @@ import styled from '../../lib/styled'
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: auto;
   height: 100%;
   padding: 0 16px;
 `
