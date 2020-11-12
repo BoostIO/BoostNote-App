8495b0730c0b5a32ae0e6bdec4005991c254cb55
Junyoung Choi <fluke8259@gmail.com>
2020-11-12 19:38:42 +0900

    Refactor events

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -2,88 +2,79 @@ import { ChangeEventHandler } from 'react'
 
 export type SelectChangeEventHandler = ChangeEventHandler<HTMLSelectElement>
 
+function createCustomEventHelper(
+  name: string
+): {
+  dispatch: () => void
+  listen: (handler: (event: CustomEvent) => void) => void
+  unlisten: (handler: (event: CustomEvent) => void) => void
+}
+function createCustomEventHelper<D = any>(
+  name: string
+): {
+  dispatch: (detail: D) => void
+  listen: (handler: (event: CustomEvent<D>) => void) => void
+  unlisten: (handler: (event: CustomEvent<D>) => void) => void
+}
+function createCustomEventHelper<D = any>(name: string) {
+  return {
+    dispatch(detail: D) {
+      window.dispatchEvent(new CustomEvent(name, { detail }))
+    },
+    listen(handler: (event: CustomEvent<D>) => void) {
+      window.addEventListener(name, handler as (event: Event) => void)
+    },
+    unlisten(handler: (event: CustomEvent<D>) => void) {
+      window.removeEventListener(name, handler as (event: Event) => void)
+    },
+  }
+}
+
 const NoteDetailFocusTitleInputEventName = 'NoteDetail:focusTitleInput'
-
-export function dispatchNoteDetailFocusTitleInputEvent() {
-  window.dispatchEvent(new CustomEvent(NoteDetailFocusTitleInputEventName))
-}
-
-export function listenNoteDetailFocusTitleInputEvent(
-  handler: (event: Event) => void
-) {
-  window.addEventListener(NoteDetailFocusTitleInputEventName, handler)
-}
-
-export function unlistenNoteDetailFocusTitleInputEvent(
-  handler: (event: Event) => void
-) {
-  window.removeEventListener(NoteDetailFocusTitleInputEventName, handler)
-}
+export const {
+  dispatch: dispatchNoteDetailFocusTitleInputEvent,
+  listen: listenNoteDetailFocusTitleInputEvent,
+  unlisten: unlistenNoteDetailFocusTitleInputEvent,
+} = createCustomEventHelper(NoteDetailFocusTitleInputEventName)
 
 const BoostHubLoginEventName = 'BoostHub:login'
-
 interface BoostHubLoginEventDetail {
   code: string
 }
-
 export type BoostHubLoginEvent = CustomEvent<BoostHubLoginEventDetail>
-
-export function dispatchBoostHubLoginEvent({ code }: BoostHubLoginEventDetail) {
-  window.dispatchEvent(
-    new CustomEvent(BoostHubLoginEventName, { detail: { code } })
-  )
-}
-
-export function listenBoostHubLoginEvent(
-  handler: (event: BoostHubLoginEvent) => void
-) {
-  window.addEventListener(
-    BoostHubLoginEventName,
-    handler as (event: Event) => void
-  )
-}
-
-export function unlistenBoostHubLoginEvent(
-  handler: (event: BoostHubLoginEvent) => void
-) {
-  window.removeEventListener(
-    BoostHubLoginEventName,
-    handler as (event: Event) => void
-  )
-}
+export const {
+  dispatch: dispatchBoostHubLoginEvent,
+  listen: listenBoostHubLoginEvent,
+  unlisten: unlistenBoostHubLoginEvent,
+} = createCustomEventHelper<BoostHubLoginEventDetail>(BoostHubLoginEventName)
 
 const BoostHubNavigateRequestEventName = 'BoostHub:navigateRequest'
-
 interface BoostHubNavigateRequestEventDetail {
   url: string
 }
-
 export type BoostHubNavigateRequestEvent = CustomEvent<
   BoostHubNavigateRequestEventDetail
 >
-
-export function dispatchBoostHubNavigateRequestEvent({
-  url,
-}: BoostHubNavigateRequestEventDetail) {
-  window.dispatchEvent(
-    new CustomEvent(BoostHubNavigateRequestEventName, { detail: { url } })
-  )
+export const {
+  dispatch: dispatchBoostHubNavigateRequestEvent,
+  listen: listenBoostHubNavigateRequestEvent,
+  unlisten: unlistenBoostHubNavigateRequestEvent,
+} = createCustomEventHelper<BoostHubNavigateRequestEventDetail>(
+  BoostHubNavigateRequestEventName
+)
+const BoostHubTeamCreateEventName = 'BoostHub:teamCreate'
+interface BoostHubTeamCreateEventDetail {
+  team: {
+    id: string
+    name: string
+    domain: string
+  }
 }
-
-export function listenBoostHubNavigateRequestEvent(
-  handler: (event: BoostHubNavigateRequestEvent) => void
-) {
-  window.addEventListener(
-    BoostHubNavigateRequestEventName,
-    handler as (event: Event) => void
-  )
-}
-
-export function unlistenBoostHubNavigateRequestEvent(
-  handler: (event: BoostHubNavigateRequestEvent) => void
-) {
-  window.removeEventListener(
-    BoostHubNavigateRequestEventName,
-    handler as (event: Event) => void
-  )
-}
+export type BoostHubTeamCreateEvent = CustomEvent<BoostHubTeamCreateEventDetail>
+export const {
+  dispatch: dispatchBoostHubTeamCreateEvent,
+  listen: listenBoostHubTeamCreateEvent,
+  unlisten: unlistenBoostHubTeamCreateEvent,
+} = createCustomEventHelper<BoostHubTeamCreateEventDetail>(
+  BoostHubTeamCreateEventName
+)
