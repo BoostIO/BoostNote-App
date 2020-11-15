00fe30e72bde0f986ef800577237fbd69010535a
Junyoung Choi <fluke8259@gmail.com>
2020-11-16 04:33:34 +0900

    Implement BoostHubLogInPage

--- src/components/organisms/BoostHubSignInForm.tsx
+++ src/components/organisms/BoostHubSignInForm.tsx
@@ -21,6 +21,7 @@ import {
   listenBoostHubLoginEvent,
   unlistenBoostHubLoginEvent,
 } from '../../lib/events'
+import { useRouter } from '../../lib/router'
 
 const BoostHubSignInForm = () => {
   const { setPreferences } = usePreferences()
@@ -29,6 +30,7 @@ const BoostHubSignInForm = () => {
   const [code, setCode] = useState('')
   const [requesting, setRequesting] = useState(false)
   const [manualFormOpened, setManualFormOpened] = useState(false)
+  const { push } = useRouter()
 
   const startLoginRequest = useCallback(async () => {
     setRequesting(true)
@@ -72,11 +74,16 @@ const BoostHubSignInForm = () => {
             }
           }),
         })
+        if (teams.length > 0) {
+          push(`/app/boosthub/teams/${teams[0].domain}`)
+        } else {
+          push(`/app/boosthub/teams`)
+        }
       } catch (error) {
         console.error('Failed to log in', error)
       }
     },
-    [setPreferences, setGeneralStatus]
+    [setPreferences, setGeneralStatus, push]
   )
 
   useEffect(() => {
@@ -118,15 +125,20 @@ const BoostHubSignInForm = () => {
           ) : (
             <FormGroup>
               <FormBlockquote>
+                Click{' '}
                 <a
                   onClick={() => {
                     setCode('')
                     setManualFormOpened(true)
                   }}
+                  style={{
+                    textDecoration: 'underline',
+                    cursor: 'pointer',
+                  }}
                 >
-                  Click here
+                  here
                 </a>{' '}
-                to sign in manually
+                to type sign-in code manually
               </FormBlockquote>
             </FormGroup>
           )}
