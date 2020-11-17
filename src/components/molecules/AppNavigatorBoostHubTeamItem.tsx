271a30cff47c7b6fe64695110351205faf338748
Junyoung Choi <fluke8259@gmail.com>
2020-11-17 13:31:03 +0900

    Remove icon from AppNavBoostHubItem

--- src/components/molecules/AppNavigatorBoostHubTeamItem.tsx
+++ src/components/molecules/AppNavigatorBoostHubTeamItem.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback } from 'react'
 import { border, flexCenter } from '../../lib/styled/styleFunctions'
 import styled from '../../lib/styled'
-import Icon from '../atoms/Icon'
-import { mdiAccountGroup } from '@mdi/js'
 import { useRouter } from '../../lib/router'
 
 const Container = styled.div`
@@ -51,22 +49,6 @@ const MainButton = styled.button`
   }
 `
 
-const IconContainer = styled.div`
-  height: 20px;
-  width: 20px;
-  border-radius: 10px;
-  font-size: 14px;
-  background-color: ${({ theme }) => theme.navBackgroundColor};
-  ${border}
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  bottom: -5px;
-  right: -5px;
-  z-index: 1;
-`
-
 interface AppNavigatorBoostHubTeamItemProps {
   domain: string
   name: string
@@ -99,11 +81,6 @@ const AppNavigatorBoostHubTeamItem = ({
       <MainButton className={active ? 'active' : ''} onClick={navigateToTeam}>
         {name.slice(0, 1)}
       </MainButton>
-      {
-        <IconContainer>
-          <Icon path={mdiAccountGroup} />
-        </IconContainer>
-      }
     </Container>
   )
 }
