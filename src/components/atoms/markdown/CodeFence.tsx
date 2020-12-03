1f5c1e8eb833f9349a1cd31b3ecf706500e872c8
JustTestCode <65500757+JustTestCode@users.noreply.github.com>
2020-12-03 23:30:35 +0800

    Update CodeFence.tsx
    add save to file

--- src/components/atoms/markdown/CodeFence.tsx
+++ src/components/atoms/markdown/CodeFence.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from '../../../lib/styled'
 import copy from 'copy-to-clipboard'
+import { saveAs } from 'file-saver'; 
 import Icon from '../Icon'
 import { mdiContentCopy } from '@mdi/js'
+import {mdiContentSave} from '@mdi/js'
 import { flexCenter } from '../../../lib/styled/styleFunctions'
 
 const CodeFenceContainer = styled.div`
@@ -13,6 +15,33 @@ const CodeFenceCopyButton = styled.button`
   position: absolute;
   top: 0;
   right: 0;
+  height: 30px;
+  width: 30px;
+  box-sizing: border-box;
+  font-size: 18px;
+  outline: none;
+
+  background-color: rgba(0, 0, 0, 0.3);
+  ${flexCenter}
+
+  border: none;
+  cursor: pointer;
+
+  transition: color 200ms ease-in-out;
+  color: ${({ theme }) => theme.navButtonColor};
+  &:hover {
+    color: ${({ theme }) => theme.navButtonHoverColor};
+  }
+
+  &:active,
+  &.active {
+    color: ${({ theme }) => theme.navButtonActiveColor};
+  }
+`
+const CodeFenceSaveButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 30px;
   height: 30px;
   width: 30px;
   box-sizing: border-box;
@@ -53,6 +82,15 @@ const CodeFence = (props: React.HTMLProps<HTMLPreElement> = {}) => {
         >
           <Icon path={mdiContentCopy} />
         </CodeFenceCopyButton>
+        <CodeFenceSaveButton
+          onClick={() => {
+            var blob = new Blob([rawContent], {type: "text/plain;charset=utf-8"});
+            saveAs(blob, "hello world.txt");
+          }}
+          title='Save to File'
+        >
+          <Icon path={mdiContentSave} />
+        </CodeFenceSaveButton>
       </CodeFenceContainer>
     )
   }
