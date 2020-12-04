3e070a0655723e2ad85dad755297509229ea9dc6
JustTestCode <65500757+JustTestCode@users.noreply.github.com>
2020-12-05 04:53:23 +0800

    add save code to file button

--- src/components/atoms/markdown/CodeFence.tsx
+++ src/components/atoms/markdown/CodeFence.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from '../../../lib/styled'
 import copy from 'copy-to-clipboard'
 import Icon from '../Icon'
-import { mdiContentCopy } from '@mdi/js'
+import { mdiContentCopy, mdiContentSave } from '@mdi/js'
 import { flexCenter } from '../../../lib/styled/styleFunctions'
-
+import { saveAs } from 'file-saver';
 const CodeFenceContainer = styled.div`
   position: relative;
 `
@@ -36,7 +36,33 @@ const CodeFenceCopyButton = styled.button`
     color: ${({ theme }) => theme.navButtonActiveColor};
   }
 `
+const CodeFenceSaveButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 30px;
+  height: 30px;
+  width: 30px;
+  box-sizing: border-box;
+  font-size: 18px;
+  outline: none;
 
+  background-color: rgba(0, 0, 0, 0.3);
+  ${flexCenter}
+
+  border: none;
+  cursor: pointer;
+
+  transition: color 200ms ease-in-out;
+  color: ${({ theme }) => theme.navButtonColor};
+  &:hover {
+    color: ${({ theme }) => theme.navButtonHoverColor};
+  }
+
+  &:active,
+  &.active {
+    color: ${({ theme }) => theme.navButtonActiveColor};
+  }
+`
 const CodeFence = (props: React.HTMLProps<HTMLPreElement> = {}) => {
   if (props.className != null && props.className!.includes('CodeMirror')) {
     const otherProps = { ...props }
@@ -53,6 +79,16 @@ const CodeFence = (props: React.HTMLProps<HTMLPreElement> = {}) => {
         >
           <Icon path={mdiContentCopy} />
         </CodeFenceCopyButton>
+        {rawContent && <CodeFenceSaveButton
+          onClick={() => {
+            var blob = new Blob([rawContent], { type: "text/plain;charset=utf-8" });
+            saveAs(blob, "hello world.txt");
+          }}
+
+          title='Save to File'
+        >
+          <Icon path={mdiContentSave} />
+        </CodeFenceSaveButton>}
       </CodeFenceContainer>
     )
   }
