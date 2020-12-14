d92c111aed4b0b80cee694e4ab046d5bcbd3ea49
Igor Escalona <igorer88@users.noreply.github.com>
2020-12-14 17:37:00 -0400

    Hompage Spanish translation  (#462) (#677)
    * Add homepage Spanish translation
    
    * Fix Spanish typo
    
    * Change spanish text
    
    * Fix some typos and add better translations

--- homepage/locales/es.ts
+++ homepage/locales/es.ts
@@ -2,21 +2,21 @@ export default {
   translation: {
     common: {
       openInBrowser: 'Abrir en el navegador',
-      downloadApp: 'Descargar App',
-      // fileSystemBasedStorage: 'File System Based Storage',
-      // comingSoon: 'Coming Soon',
+      downloadApp: 'Descargar Aplicación',
+      fileSystemBasedStorage: 'Almacenamiento basado en Sistema de Archivos',
+      comingSoon: 'Próximamente',
       webApp: 'Aplicación Web',
       desktopApp: 'Aplicación de Escritorio',
-      mobileApp: 'Aplicación movil',
-      boostHub: 'Create an Account for Free',
+      mobileApp: 'Aplicación móvil',
+      boostHub: 'Crea una cuenta gratis',
     },
     header: {
       forTeam: 'Para equipos',
     },
     hero: {
-      // title: 'Boost Happiness, Productivity and Creativity.',
-      // subtitle:
-      //   "Boost Note is an intuitive and stylish markdown editor. It's fully open-source, and used by 1 million developers!",
+      title: 'Impulsa la felicidad, la productividad y la creatividad.',
+      subtitle:
+        'Boost Note es un editor de Markdown intuitivo y estilizado. ¡Es completamente open-source, y es utilizado por 1 millón de desarrolladores!',
     },
     newsletter: {
       // title: 'Also, please join our newsletter to get the latest news and update notes.',
@@ -24,75 +24,76 @@ export default {
       // subscribed: 'Thanks for the subscription!'
     },
     boostHub: {
-      // title: 'Boost Hub, the team version of Boost Note',
-      // description1: 'We’ve developed a collaborative workspace app called “Boost Hub” for developer teams.',
-      // description2:
-      //   'It is more than just a markdown editor or a wiki. You can collaborate with your team members in real-time, from anywhere.',
-      // feature1Name: 'Real-time Coauthoring',
-      // feature1Detail:
-      //   'Write and edit documents as a team and sharing your knowledge and ideas in real-time. No one will be left behind.',
-      // feature2Name: 'Create Diagrams Like a Pro',
-      // feature2Detail:
-      //   'Supports Charts.js, Mermaid, andPlantUML, which means you can put diagrams in your document.',
-      // feature3Name: 'Beautiful Mathematical Script',
-      // feature3Detail:
-      //   'For clean representation of maths equations it is time to use LaTex in the Boost Hub editor.',
+      title: '¿Buscas una versión para equipos?',
+      description1:
+        'Boost Hub es más que solo un editor de Markdown o de Wiki.',
+      description2:
+        'Puedes colaborar con los miembros de tu equipo en tiempo real, desde cualquier lugar.',
+      feature1Name: 'Coautoría en tiempo real',
+      feature1Detail:
+        'Escribe y edita documentos como un equipo compartiendo conocimientos e ideas en tiempo real. Nadie será dejado atrás.',
+      feature2Name: 'Crea diagramas como un profesional',
+      feature2Detail:
+        'Soporta Charts.js, Mermaid, y PlantUML, lo que significa que puedes colocar diagramas a tu documento.',
+      feature3Name: 'Hermoso Script Matemático',
+      feature3Detail:
+        'Para una declaración limpia de ecuaciones matemáticas es hora de usar LaTex en el editor de Boost Hub.',
     },
     features: {
-      // title: 'Features',
-      // cloudStorage: 'Cloud Storage',
-      // cloudStorageDescription:
-      //   'Notes in a cloud storage will be stored safely and accessible from other devices.',
-      // multiplePlatforms: 'Multiple Platforms',
-      // multiplePlatformsDescription:
-      //   'Boost Note app is available in browsers, desktop app and mobile app.',
-      // syntaxHighlight: 'Syntax Highlight',
-      // syntaxHighlightDescription:
-      //   'Boost Note can highlight more than 100 programming languages.',
-      // mathEquations: 'Math Equations',
-      // mathEquationsDescription:
-      //   'Boost Note supports math blocks. In the blocks, you can write math equations with LaTeX syntax.',
-      // customizableTheme: 'Customizable Theme',
-      // customizableThemeDescription:
-      //   'You can customize style of the app UI, its editor and rendered markdown contents.',
-      // fileSystemBasedStorageDescription:
-      //   'You can have full control of your data. Share your notes with your favorite cloud storage service.',
-      // extensibleMarkdown: 'Extensible Markdown',
-      // extensibleMarkdownDescription:
-      //   'You can introduce custom markdown syntax and configure how to render it.',
+      title: 'Características',
+      cloudStorage: 'Almacenamiento en la nube',
+      cloudStorageDescription:
+        'Las notas en almacenamiento en la nube serán guardadas de forma segura y accesibles desde otros dispositivos.',
+      multiplePlatforms: 'Multiples Platformas',
+      multiplePlatformsDescription:
+        'La aplicación Boost Note está disponible en navegadores, aplicación de escritorio y aplicación móvil.',
+      syntaxHighlight: 'Resaltado Sintáxis',
+      syntaxHighlightDescription:
+        'Boost Note puede resaltar más de 100 lenguajes de programación.',
+      mathEquations: 'Ecuaciones Matemáticas',
+      mathEquationsDescription:
+        'Boost Note soporta bloques matemáticos. En los bloques, puedes escribir con sintaxis de LaTeX.',
+      customizableTheme: 'Tema Personalizable',
+      customizableThemeDescription:
+        'Puedes personalizar el estilo de la interfaz tu aplicación, su editor y el contenido del Markdown renderizado.',
+      fileSystemBasedStorageDescription:
+        'Puedes tener control completo de todos tus datos. Comparte tus notas con tu servicio en la nube favorito',
+      extensibleMarkdown: 'Markdown Extensible',
+      extensibleMarkdownDescription:
+        'Puedes introducir sintaxis personalizada de Markdown y configurar cómo renderizarla.',
     },
     pricing: {
-      // title: 'Pricing Plans',
+      title: 'Planes de pago',
       feature: 'Característica',
       basic: 'Básico',
       premium: 'Premium',
-      sync: 'Sincronizar múltiples dispositivos',
+      sync: 'Sincronizando múltiples dispositivos',
       localStorageSize: 'Almacenamiento local',
       unlimited: 'Ilimitado',
       cloudStorageSize: 'Almacenamiento en la nube',
-      // price: 'Price',
-      // free: 'Free',
+      price: 'Precio',
+      free: 'Gratis',
       month: 'mes',
       furtherPlan: 'Después, $5.00 por cada 5GB extra.',
     },
     download: {
-      // legacyApp: 'Legacy App',
-      // legacyAppDescription:
-      //   "We are going to keep maintaining the old app until the current Boost Note.next support most of features of the old app like file system based storage and markdown extensions. So please don't force yourself too much to migrate to the new app.",
-      // legacyDownloadLinks: 'Legacy Download Links',
-      // legacyRepository: 'Legacy Repository',
+      legacyApp: 'Legacy App',
+      legacyAppDescription:
+        'Vamos a seguir manteniendo la aplicación vieja hasta que la actual Boost Note.next soporte la mayoría de las características de la aplicación vieja, cómo el sistema de almacenamiento basado en sistema de archivos y extensiones para Markdown. Así que por favor no se fuerce demasiado usted mismo a migrar a la nueva aplicación.',
+      legacyDownloadLinks: 'Enlaces de descargas Legacy',
+      legacyRepository: 'Repositorio Legacy',
     },
     community: {
       title: 'Se parte de la comunidad',
       description:
-        'Nuestra comunidad de desarrolladores destras de Boost note. Ha ganado un montón de atención en Github y estan contribuyendo mucha gente. Nuestra comunidad siempre le recivira. Únete!',
+        'Nuestra comunidad de desarrolladores detrás de Boost note. Ha ganado un montón de atención en Github y están contribuyendo mucha gente. Nuestra comunidad siempre le recibirá. ¡Únete!',
     },
     footer: {
-      // mission: 'Mission',
-      // pressKit: 'Press Kit',
-      // backers: 'Backers',
-      // userTerms: 'User Terms',
-      // privacyPolicy: 'Privacy Policy',
+      mission: 'Misión',
+      pressKit: 'Kit de prensa',
+      backers: 'Patrocinadores',
+      userTerms: 'Términos para usuario',
+      privacyPolicy: 'Política de privacidad',
     },
   },
 }
