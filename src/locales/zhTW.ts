e0942e8094b1d112590e9e83c5d5ebb670e7049d
xatier <xatierlike@gmail.com>
2021-01-22 00:37:28 -0800

    Update zhTW translation against enUS.ts

--- src/locales/zhTW.ts
+++ src/locales/zhTW.ts
@@ -1,6 +1,7 @@
 export default {
   translation: {
     //General
+    'general.general': '一般',
     'general.error': '錯誤',
     'general.cancel': '取消',
     'general.attachments': '附加檔案',
@@ -12,26 +13,37 @@ export default {
     'general.default': '預設',
     'general.networkError': '網路錯誤',
 
+    // Navigator
+    'navigator.noStorage': '沒有儲存空間',
+    'navigator.createStorage': '點擊以新增儲存空間',
+
+    // Newsletter
+    'newsletter.subscribe': '訂閱',
+    'newsletter.heading': '訂閱更新紀錄',
+    'newsletter.subscribed': '感謝您的訂閱！',
     // Storage
+    'storage.storage': '儲存空間',
     'storage.name': '儲存空間名稱',
     'storage.noStorage': '無儲存空間',
     'storage.create': '新增儲存空間',
-    'storage.edit': '編輯',
-    'storage.rename': '重新命名',
+    'storage.edit': '編輯儲存空間',
+    'storage.rename': '重新命名儲存空間',
     'storage.renameMessage': '請輸入新儲存空間名稱',
     'storage.remove': '移除儲存空間',
-    'storage.removeMessage': '這個筆記將會從這個 APP 解除連結。',
+    'storage.removeMessage': '警告！這將會永久移除所有此儲存空間的筆記。',
     'storage.delete': '刪除儲存空間 {{storage}}',
-    'storage.move': '移動儲存空間',
-    'storage.moveTitle': '移動筆記到其他儲存空間',
-    'storage.moveMessage': '您正在嘗試把筆記移到另一個儲存空間。',
+    'storage.move': '移動筆記',
+    'storage.moveTitle': '移動筆記至其他儲存空間',
+    'storage.moveMessage': '您正在嘗試將筆記移至另一個儲存空間。',
     'storage.copy': '複製筆記',
+    'storage.type': '儲存空間類型',
     'storage.typeLocal': '本機',
     'storage.typeCloud': '雲端',
     'storage.needSignIn': '請登入以新增雲端儲存空間。',
     'storage.syncDate': '上一次同步時間',
 
     //Folder
+    'folder.folder': '資料夾',
     'folder.create': '新資料夾',
     'folder.rename': '重新命名資料夾',
     'folder.renameMessage':
@@ -39,11 +51,18 @@ export default {
     'folder.renameErrorMessage': '無法重新命名該資料夾',
     'folder.remove': '移除資料夾',
     'folder.removeMessage': '所有筆記和子資料夾將會被刪除。',
+    'folder.select': '請選擇資料夾',
+    'folder.noLocationSelected': '沒有選擇位址',
 
     //Tag
     'tag.tags': '標籤',
+    'tag.add': '新增標籤',
     'tag.remove': '移除標籤',
+    'tag.rename': '重新命名標籤',
+    'tag.removeX': '移除 #{{tag}}',
     'tag.removeMessage': '此標籤將從所有筆記中被移除。',
+    'tag.renameMessage':
+      '請輸入新標籤名稱。 帶有 "{{tagName}}" 之筆記將會反應此變更。',
 
     //Note
     'note.duplicate': '複製',
@@ -57,14 +76,19 @@ export default {
     'note.nothingMessage': '查無筆記。',
     'note.noTitle': '無標題',
     'note.date': '之前',
-    'note.createKeyOr': '还是',
+    'note.createKeyOr': '或',
     'note.createKey': 'Enter',
-    'note.createKeyMac': '在Mac系统上',
-    'note.createKeyWinLin': '在Windows系统上',
-    'note.createkeymessage1': '新增筆記, 按',
+    'note.createKeyMac': '在 Mac 系统上',
+    'note.createKeyWinLin': '在 Windows/Linux 系统上',
+    'note.createkeymessage1': '新增筆記, 請按',
     'note.createkeymessage2': '請選擇儲存空間',
     'note.createkeymessage3': '新增筆記',
-    'note.restore': '回復',
+    'note.restore': '回覆',
+    'note.edit': '編輯',
+    'note.splitView': '分割檢視',
+    'note.preview': '預覽',
+    'note.trash': '資源回收桶',
+    'note.export': '匯出',
 
     //Bookmark
     'bookmark.remove': '移除書簽',
@@ -72,7 +96,7 @@ export default {
 
     //About
     'about.about': '關於',
-    'about.boostnoteDescription': '為程式設計師貼身打造的開源筆記。',
+    'about.boostnoteDescription': '為像您一樣的程式設計師貼身打造的開源筆記。',
     'about.website': '官方網站',
     'about.boostWiki': 'Boost Note 團隊版',
     'about.platform': '跨平臺',
@@ -89,7 +113,7 @@ export default {
     'billing.billing': '價位表',
     'billing.message': '請先登入以升級您的方案。',
     'billing.basic': '基本方案',
-    'billing.current': '目前',
+    'billing.current': '目前方案',
     'billing.premium': '升級方案',
     'billing.price': '$3/月 (USD) ',
     'billing.browser': '網頁程式',
@@ -103,12 +127,14 @@ export default {
       '您可以在任何時間追加雲端儲存空間容量，費用是每 5GB 5 美元。',
     'billing.addStorage': '新增額外儲存空間',
 
+    'editor.editor': '編輯器',
+
     // Preferences
     'preferences.general': '偏好設定',
 
     // Preferences GeneralTab
     'preferences.account': '帳號',
-    'preferences.addAccount': '添加帳號',
+    'preferences.addAccount': '新增帳號',
     'preferences.loginWorking': '登入...',
     'preferences.interfaceLanguage': '介面語言',
     'preferences.applicationTheme': '程式佈景主題',
@@ -127,6 +153,12 @@ export default {
     'preferences.analyticsDescription2': '您可以選擇開啟或者關閉這個選項。',
     'preferences.analyticsLabel': '開啟資料收集來幫助我們完善 Boostnote',
     'preferences.displayTutorialsLabel': '教學與 FAQ',
+    'preferences.notesView': '筆記列表檢視',
+    'preferences.notesViewDefault': '預設',
+    'preferences.notesViewCompact': '緊密',
+    'preferences.autoSync': '啟用自動同步',
+    'preferences.subfolders': '子資料夾',
+    'preferences.subfoldersView': '顯示所有子資料夾內容',
 
     // Preferences EditorTab
     'preferences.editorTheme': '編輯器主題',
@@ -144,5 +176,7 @@ export default {
     'preferences.markdownCodeBlockTheme': '程式碼區塊佈景主題',
     'preferences.defaultTheme': '使用預設佈景主題',
     'preferences.markdownPreview': 'Markdown 預覽',
+    'preferences.markdownExport': '匯出',
+    'preferences.markdownExportOption': '包含裝幀',
   },
 }
