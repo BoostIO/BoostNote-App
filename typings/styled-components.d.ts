e77ba431a83f643951508c29930b3f33b0681878
Junyoung Choi <fluke8259@gmail.com>
2021-01-27 15:49:53 +0900

    Fix styled-components types

--- typings/styled-components.d.ts
+++ typings/styled-components.d.ts
@@ -1,22 +1,34 @@
 declare module 'styled-components' {
   export type StyledComponent<T1, T2, T3, T4> = any
-  export function createGlobalStyle<P>(
+  export function createGlobalStyle<T>(
     strings: TemplateStringsArray,
     ...keys: Array<
-      number | undefined | string | ((props: P) => string | number | undefined)
+      | number
+      | undefined
+      | string
+      | ((props: { theme: T }) => string | number | undefined)
     >
   ): any
-  export type ThemedBaseStyledInterface<T> = {
+  export type ThemeKeys<T> = Array<
+    | number
+    | undefined
+    | string
+    | ((props: { theme: T } & P) => string | number | undefined)
+  >
+  export interface ThemedBaseStyledInterface<T>
+    extends ThemeBasedStyledComponentWrapper<T> {
     [key: string]: <P>(
       strings: TemplateStringsArray,
-      ...keys: Array<
-        | number
-        | undefined
-        | string
-        | ((props: { theme: T } & P) => string | number | undefined)
-      >
+      ...keys: ThemeKeys<T>
     ) => any
+  }
+  export type ThemeBasedStyledComponentWrapper<T> = {
+    <P extends { className?: string }>(component: React.ComponentType<P>): (
+      strings: TemplateStringsArray,
+      ...keys: ThemeKeys<T>
+    ) => React.ReactComponentType<P>
   }
   export const ThemeProvider: any
   export const keyframes = any
+  export const ServerStyleSheet: any
 }
