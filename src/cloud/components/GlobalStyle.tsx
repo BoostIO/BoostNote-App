aa8db3664305a34c2e41545e731c416ba272ad24
Junyoung Choi <fluke8259@gmail.com>
2021-01-28 04:25:42 +0900

    Apply bootstrap default stylesheet

--- src/cloud/components/GlobalStyle.tsx
+++ src/cloud/components/GlobalStyle.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 import { BaseTheme } from '../lib/styled/themes/types'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const getGlobalCss = (theme: BaseTheme) => `
 body {
