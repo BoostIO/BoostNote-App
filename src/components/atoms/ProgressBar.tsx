e46fcba88610aa1b14f7ca7b2fa9cf0ff8c6ec6d
Simon Leigh <sleigh.dev@gmail.com>
2021-02-22 15:38:07 +0900

    progress bar atom

---
+++ src/components/atoms/ProgressBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from '../../lib/styled'
+
+interface ProgressBarProps {
+  progress: number
+}
+
+const ProgressBar = ({ progress }: ProgressBarProps) => {
+  return <ProgressBarStyled progress={progress} />
+}
+
+const ProgressBarStyled = styled.div`
+  border: 2px solid ${({ theme }) => theme.borderColor};
+  height: 30px;
+  position: relative;
+  width: 100%;
+
+  &:after {
+    content: '';
+    transition: width 0.5s linear;
+    position: absolute;
+    top: 0;
+    height: 100%;
+    background-color: ${({ theme }) => theme.primaryColor};
+    width: ${({ progress }) => progress}%;
+  }
+`
+
+export default ProgressBar
