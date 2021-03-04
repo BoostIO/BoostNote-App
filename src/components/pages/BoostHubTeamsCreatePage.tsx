ec2d78998cb5b325006f755b36ced8b54589ac8d
Junyoung Choi <fluke8259@gmail.com>
2021-03-04 18:16:56 +0900

    Add connection refused error handler for teams create page

--- src/components/pages/BoostHubTeamsCreatePage.tsx
+++ src/components/pages/BoostHubTeamsCreatePage.tsx
@@ -1,27 +1,65 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useCallback } from 'react'
 import { boostHubTeamsCreatePageUrl } from '../../lib/boosthub'
-import PageDraggableHeader from '../atoms/PageDraggableHeader'
-import { mdiAccountGroup } from '@mdi/js'
 import styled from '../../lib/styled'
 import BoostHubWebview, { WebviewControl } from '../atoms/BoostHubWebview'
+import { FormSecondaryButton } from '../atoms/form'
+import { DidFailLoadEvent } from 'electron/main'
+import { useRouter } from '../../lib/router'
 
 const BoostHubTeamsCreatePage = () => {
   const controlRef = useRef<WebviewControl>()
+  const [refusedConnection, setRefusedConnection] = useState(false)
+  const { push } = useRouter()
+
+  const handleWebviewDidFailLoadEventHandler = useCallback(
+    (event: DidFailLoadEvent) => {
+      if (event.errorDescription !== 'ERR_CONNECTION_REFUSED') {
+        return
+      }
+
+      setRefusedConnection(true)
+    },
+    []
+  )
+
+  const reloadWebview = useCallback(() => {
+    if (controlRef.current == null) {
+      return
+    }
+    controlRef.current.reload()
+    setRefusedConnection(false)
+  }, [])
+
+  const navigateToCreateLocalSpacePage = useCallback(() => {
+    push('/app/storages')
+  }, [push])
 
   return (
     <PageContainer>
-      <PageDraggableHeader
-        onDoubleClick={() => {
-          controlRef.current!.openDevTools()
-        }}
-        iconPath={mdiAccountGroup}
-        label='Create a cloud workspace'
-      />
       <WebViewContainer>
         <BoostHubWebview
           controlRef={controlRef}
           src={boostHubTeamsCreatePageUrl}
+          onDidFailLoad={handleWebviewDidFailLoadEventHandler}
         />
+
+        {refusedConnection && (
+          <ReloadView>
+            <div>
+              <h1 className='heading'>Cannot Reach Server</h1>
+              <p className='description'>
+                Please check your internet connection.
+              </p>
+              <FormSecondaryButton onClick={reloadWebview}>
+                Reload Page
+              </FormSecondaryButton>
+
+              <FormSecondaryButton onClick={navigateToCreateLocalSpacePage}>
+                Create Local Space
+              </FormSecondaryButton>
+            </div>
+          </ReloadView>
+        )}
       </WebViewContainer>
     </PageContainer>
   )
@@ -40,4 +78,24 @@ const WebViewContainer = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+`
+
+const ReloadView = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+  background-color: ${({ theme }) => theme.backgroundColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  .heading {
+    text-align: center;
+  }
+  .description {
+    text-align: center;
+  }
 `
