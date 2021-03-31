6fa751de757fb99fdf433e50c52cea35c78b5706
Komediruzecki <komediruzecki.2015@gmail.com>
2021-03-31 21:59:30 +0200

    Fix CI failing, prettier

--- src/lib/CodeMirror.ts
+++ src/lib/CodeMirror.ts
@@ -21,7 +21,7 @@ import 'codemirror/keymap/vim'
 import 'codemirror-abap'
 
 // Custom addons
-import {initHyperlink} from './addons/hyperlink'
+import { initHyperlink } from './addons/hyperlink'
 
 const dispatchModeLoad = debounce(() => {
   window.dispatchEvent(new CustomEvent('codemirror-mode-load'))
