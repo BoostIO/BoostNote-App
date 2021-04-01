0f6551109d14d0229493cf3918d38beabf0073bb
Junyoung Choi <fluke8259@gmail.com>
2021-04-01 11:32:50 +0900

    Discard LocalStorageCreateForm

--- src/components/pages/StorageCreatePage.tsx
+++ src/components/pages/StorageCreatePage.tsx
@@ -1,39 +1,11 @@
-import React, { useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import {
-  FormGroup,
-  FormCheckInlineItem,
-  FormCheckList,
-  FormLabel,
-} from '../atoms/form'
-import LocalStorageCreateForm from '../organisms/LocalStorageCreateForm'
+import React from 'react'
 import FSStorageCreateForm from '../organisms/FSStorageCreateForm'
 import PageDraggableHeader from '../atoms/PageDraggableHeader'
 import { mdiBookPlusMultiple } from '@mdi/js'
 import PageScrollableContent from '../atoms/PageScrollableContent'
-import { appIsElectron } from '../../lib/platform'
 import styled from '../../lib/styled'
 
-type StorageType = 'local' | 'fs'
-
-interface StorageCreateFormProps {
-  storageType: StorageType
-}
-
-const StorageCreateForm = ({ storageType }: StorageCreateFormProps) => {
-  switch (storageType) {
-    case 'fs':
-      return <FSStorageCreateForm />
-    case 'local':
-    default:
-      return <LocalStorageCreateForm />
-  }
-}
-
 const StorageCreatePage = () => {
-  const { t } = useTranslation()
-  const [storageType, setStorageType] = useState<'local' | 'fs'>('fs')
-
   return (
     <PageContainer>
       <PageDraggableHeader
@@ -41,31 +13,7 @@ const StorageCreatePage = () => {
         label='Create Local Workspace'
       />
       <PageScrollableContent>
-        <FormGroup>
-          <FormLabel>{t('storage.type')}</FormLabel>
-          <FormCheckList>
-            {appIsElectron && (
-              <FormCheckInlineItem
-                id='radio-fsStorageType'
-                type='radio'
-                checked={storageType === 'fs'}
-                onChange={() => setStorageType('fs')}
-              >
-                File System
-              </FormCheckInlineItem>
-            )}
-            <FormCheckInlineItem
-              id='radio-localStorageType'
-              type='radio'
-              checked={storageType === 'local'}
-              onChange={() => setStorageType('local')}
-            >
-              {t('storage.typeLocal')}
-            </FormCheckInlineItem>
-          </FormCheckList>
-        </FormGroup>
-
-        <StorageCreateForm storageType={storageType} />
+        <FSStorageCreateForm />
       </PageScrollableContent>
     </PageContainer>
   )
