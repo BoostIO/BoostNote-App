7007336e11552d1b6cae93d6cdd5d002bd03f2d3
Junyoung Choi <fluke8259@gmail.com>
2021-04-21 15:59:46 +0900

    Adjust x position of space switch popup

--- src/components/v2/organisms/Sidebar/molecules/SidebarSpaces.tsx
+++ src/components/v2/organisms/Sidebar/molecules/SidebarSpaces.tsx
@@ -128,7 +128,7 @@ const SidebarContentRow = ({
 const Container = styled.div`
   position: fixed;
   top: 15px;
-  left: 15px;
+  left: 35px;
   background: ${({ theme }) => theme.colors.background.primary};
   z-index: 101;
   box-shadow: ${({ theme }) => theme.colors.shadow};
