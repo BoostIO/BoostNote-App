f647f1de753a947a49502073ade008487e86e0a7
Simon Leigh <sleigh.dev@gmail.com>
2021-04-27 19:18:16 +0900

    remove unused import

--- src/components/organisms/AppNavigator.tsx
+++ src/components/organisms/AppNavigator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState, useCallback } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import styled from '../../lib/styled'
 import { useDb } from '../../lib/db'
 import { entries } from '../../lib/db/utils'
