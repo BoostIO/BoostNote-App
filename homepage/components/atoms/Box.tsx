12469373e2f9016349edfa8b30008d35cafed279
Junyoung Choi <fluke8259@gmail.com>
2021-04-29 16:21:12 +0900

    Discard deprecated homepage

--- homepage/components/atoms/Box.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '../../lib/styled'
-import { space, SpaceProps, color, ColorProps } from 'styled-system'
-
-const Box = styled.div<SpaceProps & ColorProps>`
-  ${space}
-  ${color}
-`
-
-export default Box
