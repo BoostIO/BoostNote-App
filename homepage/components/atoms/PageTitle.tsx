d5cdb96909782ccc1f251039718b399473dba485
Junyoung Choi <fluke8259@gmail.com>
2021-04-29 16:21:12 +0900

    Discard deprecated homepage

--- homepage/components/atoms/PageTitle.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '../../lib/styled'
-
-const PageTitle = styled.h2`
-  margin: ${({ theme }) => theme.space[0]};
-  font-size: ${({ theme }) => theme.fontSizes[5]}px;
-`
-
-export default PageTitle
