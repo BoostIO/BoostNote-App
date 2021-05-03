6689fb9860c7d7aec21e0e5c7df494d34a800443
Elle Kasai <elle.kasai@gmail.com>
2021-05-03 19:49:22 +0900

    Removed Styled Component

--- src/components/PreferencesModal/TabButton.tsx
+++ src/components/PreferencesModal/TabButton.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react'
-import styled from '../../lib/styled'
 import cc from 'classcat'
+import SettingTabButton from '../../shared/components/organisms/Settings/atoms/SettingTabButton'
 
 interface TabButtonProps {
   label: string
@@ -9,46 +9,14 @@ interface TabButtonProps {
   setTab: (tab: string) => void
 }
 
-const StyledButton = styled.button`
-  width: 100%;
-  border-radius: 4px;
-  height: 30px;
-  background-color: ${({ theme }) => theme.navItemBackgroundColor};
-  border: none;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  margin-bottom: 5px;
-
-  .label {
-    flex: 1;
-    color: ${({ theme }) => theme.navItemColor};
-    text-align: left;
-    padding-left: 15px;
-    font-size: 14px;
-  }
-  &:hover {
-    background-color: ${({ theme }) => theme.navItemHoverBackgroundColor};
-  }
-  &.active {
-    color: ${({ theme }) => theme.textColor};
-    background-color: ${({ theme }) => theme.navItemActiveBackgroundColor};
-
-    .label {
-      color: ${({ theme }) => theme.textColor};
-      color: ${({ theme }) => theme.navItemActiveColor};
-    }
-  }
-`
-
 const TabButton = ({ label, tab, setTab, active }: TabButtonProps) => {
   const selectTab = useCallback(() => {
     setTab(tab)
   }, [tab, setTab])
   return (
-    <StyledButton onClick={selectTab} className={cc([active && 'active'])}>
+    <SettingTabButton onClick={selectTab} className={cc([active && 'active'])}>
       <div className='label'>{label}</div>
-    </StyledButton>
+    </SettingTabButton>
   )
 }
 
