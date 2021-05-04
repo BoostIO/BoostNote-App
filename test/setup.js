e6c6608d9f784cac22e0db85c2c242a87fa44f94
Junyoung Choi <fluke8259@gmail.com>
2021-05-04 16:16:35 +0900

    Fix test

---
+++ test/setup.js
@@ -0,0 +1,36 @@
+function dummy() {
+  // Don't do anything
+}
+
+window.__ELECTRON_ONLY__ = {
+  readFile: dummy,
+  readdir: dummy,
+  writeFile: dummy,
+  unlinkFile: dummy,
+  stat: dummy,
+  mkdir: dummy,
+  readFileType: dummy,
+  readFileTypeFromBuffer: dummy,
+  showOpenDialog: dummy,
+  showSaveDialog: dummy,
+  openExternal: dummy,
+  parseCSON: dummy,
+  stringifyCSON: dummy,
+  openNewWindow: dummy,
+  openContextMenu: dummy,
+  getPathByName: dummy,
+  addIpcListener: dummy,
+  sendIpcMessage: dummy,
+  removeIpcListener: dummy,
+  removeAllIpcListeners: dummy,
+  setAsDefaultProtocolClient: dummy,
+  removeAsDefaultProtocolClient: dummy,
+  isDefaultProtocolClient: dummy,
+  getWebContentsById: dummy,
+  setTrafficLightPosition: dummy,
+  convertHtmlStringToPdfBuffer: dummy,
+  setCookie: dummy,
+  getCookie: dummy,
+  removeCookie: dummy,
+  got: dummy,
+}
