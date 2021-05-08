8b50e6003d542c83715dd60b619da2666e776042
Elle Kasai <elle.kasai@gmail.com>
2021-05-08 15:03:42 +0900

    SettingSidenavItem -> SettingTabButton

--- src/shared/components/organisms/Settings/atoms/SettingTabButton.tsx
+++ src/shared/components/organisms/Settings/atoms/SettingTabButton.tsx
@@ -1,6 +1,36 @@
+import React, { MouseEventHandler } from 'react'
+import cc from 'classcat'
 import styled from '../../../../lib/styled'
+import { SettingsTab } from '../../../../../cloud/lib/stores/settings'
 
-const SettingTabButton = styled.button`
+interface SettingTabButtonProps {
+  label: string
+  active: boolean
+  tab: SettingsTab
+  id?: string
+  onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+const SettingTabButton = ({
+  label,
+  tab,
+  active,
+  id,
+  onClick,
+}: SettingTabButtonProps) => {
+  return (
+    <Container
+      className={cc([active && 'active'])}
+      tab={tab}
+      id={id}
+      onClick={onClick}
+    >
+      {label}
+    </Container>
+  )
+}
+
+const Container = styled.button`
   display: flex;
   align-items: center;
   width: 100%;
