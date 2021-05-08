2538fc74c2704a30e55fa35737ff4e0153fe2432
Elle Kasai <elle.kasai@gmail.com>
2021-05-08 22:23:28 +0900

    Apply SettingTabContent

--- src/cloud/components/organisms/settings/PreferencesTab.tsx
+++ src/cloud/components/organisms/settings/PreferencesTab.tsx
@@ -1,22 +1,16 @@
 import React from 'react'
-import { TabHeader, Column, Container, Scrollable } from './styled'
 import { useTranslation } from 'react-i18next'
 import UserPreferencesForm from './UserPreferencesForm'
+import SettingTabContent from '../../../../shared/components/organisms/Settings/atoms/SettingTabContent'
 
 const PreferencesTab = () => {
   const { t } = useTranslation()
 
   return (
-    <Column>
-      <Scrollable>
-        <Container>
-          <TabHeader className='marginTop'>
-            {t('settings.preferences')}
-          </TabHeader>
-          <UserPreferencesForm />
-        </Container>
-      </Scrollable>
-    </Column>
+    <SettingTabContent
+      header={t('settings.preferences')}
+      body={<UserPreferencesForm />}
+    ></SettingTabContent>
   )
 }
 
