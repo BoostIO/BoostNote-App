205e7b1d571e2f38bcbe316044fce14f723aa62c
Elle Kasai <elle.kasai@gmail.com>
2021-05-09 18:45:35 +0900

    Updated Title

--- src/cloud/lib/i18n/enUS.ts
+++ src/cloud/lib/i18n/enUS.ts
@@ -23,7 +23,7 @@ const enTranslation: TranslationSource = {
   'settings.teamInfo': 'Settings',
   'settings.title': 'Settings',
   'settings.personalInfo': 'Settings',
-  'settings.preferences': 'Theme and Preferences',
+  'settings.preferences': 'Preferences',
   'settings.teamMembers': 'Members',
   'settings.teamUpgrade': 'Upgrade',
   'settings.teamSubscription': 'Billing',
