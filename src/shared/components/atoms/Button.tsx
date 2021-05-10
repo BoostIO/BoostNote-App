f4cba6f9e7134596bbc41775fbe13ec3c1e65e5f
Elle Kasai <elle.kasai@gmail.com>
2021-05-10 15:26:54 +0900

    Fix Alignment

--- src/shared/components/atoms/Button.tsx
+++ src/shared/components/atoms/Button.tsx
@@ -166,6 +166,11 @@ const StyledButton = styled.button`
     margin-left: 5px;
   }
 
+  .button__label {
+    display: flex;
+    align-items: center;
+  }
+
   .button__icon {
     flex: 0 0 auto;
   }
