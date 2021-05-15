dd98f1a590e430175dc93d77b3af1036622b42a6
Elle Kasai <elle.kasai@gmail.com>
2021-05-15 15:03:07 +0900

    Replaced to SettingLink

--- src/shared/components/organisms/Settings/atoms/SettingLink.tsx
+++ src/shared/components/organisms/Settings/atoms/SettingLink.tsx
@@ -1,16 +1,13 @@
 import styled from '../../../../lib/styled'
-import Link from '../../../atoms/Link'
 
-const SettingLink = styled(Link)`
-  a {
+const SettingLink = styled.a`
+  color: ${({ theme }) => theme.colors.text.link};
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
     color: ${({ theme }) => theme.colors.text.link};
-    text-decoration: none;
-
-    &:hover,
-    &:focus {
-      color: ${({ theme }) => theme.colors.text.link};
-      text-decoration: underline;
-    }
+    text-decoration: underline;
   }
 `
 
