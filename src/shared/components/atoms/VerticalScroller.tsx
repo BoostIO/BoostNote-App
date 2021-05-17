9f65c6c588d1bbe404691275b5063d472b0e2a0b
davy-c <d.castel33@gmail.com>
2021-05-17 13:22:06 +0900

    new style

--- src/shared/components/atoms/VerticalScroller.tsx
+++ src/shared/components/atoms/VerticalScroller.tsx
@@ -4,7 +4,14 @@ import cc from 'classcat'
 import styled from '../../lib/styled'
 import { scrollbarOverlay } from '../../lib/styled/styleFunctions'
 
-const VerticalScroller: AppComponent<{}> = ({ children, className }) => {
+interface VerticalScrollerProps {
+  onClick?: React.MouseEventHandler
+}
+const VerticalScroller: AppComponent<VerticalScrollerProps> = ({
+  children,
+  className,
+  onClick,
+}) => {
   const [inScroll, setInScroll] = useState(false)
   const scrollTimer = useRef<any>()
 
@@ -22,6 +29,7 @@ const VerticalScroller: AppComponent<{}> = ({ children, className }) => {
         inScroll && 'vertical__scroller--scrolling',
         className,
       ])}
+      onClick={onClick}
       onScroll={onScrollHandler}
     >
       {children}
--- src/shared/components/organisms/Modal/index.tsx
+++ src/shared/components/organisms/Modal/index.tsx
@@ -1,11 +1,12 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo, useRef } from 'react'
 import { mdiClose } from '@mdi/js'
 import cc from 'classcat'
-import { useModal } from '../../../lib/stores/modal'
+import { ModalElement, useModal } from '../../../lib/stores/modal'
 import { isActiveElementAnInput } from '../../../lib/dom'
 import { useGlobalKeyDownHandler } from '../../../lib/keyboard'
 import styled from '../../../lib/styled'
 import Button from '../../atoms/Button'
+import VerticalScroller from '../../atoms/VerticalScroller'
 
 const Modal = () => {
   const { modals, closeLastModal } = useModal()
@@ -24,78 +25,111 @@ const Modal = () => {
   return (
     <Container className='modal'>
       {modals.map((modal, i) => (
-        <React.Fragment key={`modal-${i}`}>
-          <div
-            className='modal__background'
-            onClick={(event) => {
-              event.preventDefault()
-              closeLastModal()
-            }}
-          />
-          <div
-            className={cc([
-              'modal__window',
-              `modal__window__width--${modal.width}`,
-            ])}
-          >
-            {modal.showCloseIcon && (
-              <Button
-                variant='icon'
-                iconPath={mdiClose}
-                onClick={closeLastModal}
-                className='modal__window__close'
-                iconSize={26}
-              />
-            )}
-            <div className='modal__wrapper'>
-              {modal.title != null && (
-                <h3 className='modal__title'>{modal.title}</h3>
-              )}
-              <div className='modal__content'>{modal.content}</div>
-            </div>
-          </div>
-        </React.Fragment>
+        <ModalItem
+          key={`modal-${i}`}
+          modal={modal}
+          closeModal={closeLastModal}
+        />
       ))}
     </Container>
+  )
+}
+
+const ModalItem = ({
+  closeModal,
+  modal,
+}: {
+  closeModal: () => void
+  modal: ModalElement
+}) => {
+  const contentRef = useRef<HTMLDivElement>(null)
+  const onScrollClickHandler: React.MouseEventHandler = useCallback(
+    (event) => {
+      console.log('clicked')
+      if (
+        contentRef.current != null &&
+        contentRef.current.contains(event.target as Node)
+      ) {
+        return
+      }
+
+      closeModal()
+    },
+    [closeModal]
+  )
+
+  return (
+    <VerticalScroller
+      className='modal__window__scroller'
+      onClick={onScrollClickHandler}
+    >
+      <div
+        ref={contentRef}
+        className={cc([
+          'modal__window',
+          `modal__window__width--${modal.width}`,
+        ])}
+      >
+        {modal.showCloseIcon && (
+          <Button
+            variant='icon'
+            iconPath={mdiClose}
+            onClick={closeModal}
+            className='modal__window__close'
+            iconSize={26}
+          />
+        )}
+        <div className='modal__wrapper'>
+          {modal.title != null && (
+            <h3 className='modal__title'>{modal.title}</h3>
+          )}
+          <div className='modal__content'>{modal.content}</div>
+        </div>
+      </div>
+    </VerticalScroller>
   )
 }
 
 const zIndexModals = 8001
 const Container = styled.div`
   z-index: ${zIndexModals};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: hidden;
 
-  .modal__background {
+  &::before {
+    content: '';
     z-index: ${zIndexModals + 1};
     position: fixed;
     height: 100vh;
     width: 100vw;
     top: 0;
     left: 0;
-    right: 0;
-    bottom: 0;
     background-color: #000;
     opacity: 0.7;
   }
 
+  .modal__window__scroller {
+    z-index: ${zIndexModals + 2};
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    outline: 0;
+  }
+
   .modal__window {
     z-index: ${zIndexModals + 2};
-    display: flex;
     position: relative;
     width: 900px;
     max-width: 96%;
     background-color: ${({ theme }) => theme.colors.background.primary};
     box-shadow: ${({ theme }) => theme.colors.shadow};
     border-radius: 4px;
-    overflow: auto;
+    height: fit-content;
+    margin: 1.75rem auto;
+    display: block;
+    float: center;
 
     &.modal__window__width--large {
       width: 1100px;
