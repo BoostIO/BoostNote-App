1cef53109aaede1afb9db8f78085f9b867146935
davy-c <d.castel33@gmail.com>
2021-05-18 12:25:04 +0900

    teamsublimit

--- src/cloud/components/organisms/settings/TeamSubLimit.tsx
+++ src/cloud/components/organisms/settings/TeamSubLimit.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import cc from 'classcat'
 import { usePage } from '../../../lib/stores/pageStore'
 import { useSettings } from '../../../lib/stores/settings'
-import SettingTeamSubLimit from '../../../../shared/components/organisms/Settings/atoms/SettingTeamSubLimit'
+import styled from '../../../../shared/lib/styled'
 
 const TeamSubLimit = () => {
   const { subscription, team, currentSubInfo } = usePage()
@@ -18,7 +18,7 @@ const TeamSubLimit = () => {
 
   if (currentSubInfo.trialing) {
     return (
-      <SettingTeamSubLimit>
+      <Container>
         <a
           className='upgrade-link'
           href='#'
@@ -36,12 +36,12 @@ const TeamSubLimit = () => {
             You can upgrade at anytime during your trial.
           </p>
         </a>
-      </SettingTeamSubLimit>
+      </Container>
     )
   }
 
   return (
-    <SettingTeamSubLimit>
+    <Container>
       <a
         className='upgrade-link'
         href='#'
@@ -69,8 +69,74 @@ const TeamSubLimit = () => {
           </p>
         )}
       </a>
-    </SettingTeamSubLimit>
+    </Container>
   )
 }
+
+const Container = styled.nav`
+  width: 100%;
+  margin-top: ${({ theme }) => theme.sizes.spaces.l}px;
+
+  h6 {
+    margin: 0;
+    color: ${({ theme }) => theme.colors.variants.primary.base};
+    font-size: ${({ theme }) => theme.sizes.fonts.df}px;
+  }
+
+  p {
+    margin: ${({ theme }) => theme.sizes.spaces.sm}px 0;
+    color: ${({ theme }) => theme.colors.text.subtle};
+    font-size: ${({ theme }) => theme.sizes.fonts.sm}px;
+  }
+
+  .upgrade-link {
+    display: block;
+    margin-top: ${({ theme }) => theme.sizes.spaces.sm}px;
+    margin-bottom: ${({ theme }) => theme.sizes.spaces.xsm}px;
+    padding: ${({ theme }) => theme.sizes.spaces.df}px;
+    cursor: pointer;
+    text-decoration: none;
+
+    &:hover,
+    &:focus {
+      background-color: ${({ theme }) => theme.colors.background.tertiary};
+    }
+  }
+
+  .note-limit {
+    font-size: ${({ theme }) => theme.sizes.fonts.sm}px;
+  }
+
+  .progress-sm {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 3px;
+    background-color: ${({ theme }) => theme.colors.background.quaternary};
+    border-radius: 0.25rem;
+    font-size: 0.75rem;
+    overflow: hidden;
+    text-align: center;
+  }
+
+  .progress-bar {
+    flex-direction: column;
+    justify-content: center;
+    height: 3px;
+    max-width: 100%;
+    background-color: ${({ theme }) => theme.colors.background.primary};
+    text-align: center;
+    white-space: nowrap;
+    transition: width 0.6s ease;
+
+    &.over-limit {
+      background-color: ${({ theme }) => theme.colors.variants.danger.base};
+    }
+  }
+
+  .text-danger {
+    color: ${({ theme }) => theme.colors.variants.danger.base};
+  }
+`
 
 export default TeamSubLimit
