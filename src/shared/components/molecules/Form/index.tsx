70257aa0481af831e773f4b623f8bbcb51ca6747
Junyoung Choi <fluke8259@gmail.com>
2021-05-20 11:19:29 +0900

    Make rows and onSubmit of Form/index.tsx optional

--- src/shared/components/molecules/Form/index.tsx
+++ src/shared/components/molecules/Form/index.tsx
@@ -9,8 +9,8 @@ import { FormSelect } from '../../../../components/atoms/form'
 import FormEmoji, { FormEmojiProps } from './atoms/FormEmoji'
 
 interface FormProps {
-  rows: FormRowProps[]
-  onSubmit: React.FormEventHandler
+  rows?: FormRowProps[]
+  onSubmit?: React.FormEventHandler
 
   submitButton?: ButtonProps & { label: React.ReactNode; spinning?: boolean }
   onCancel?: () => void
@@ -42,41 +42,45 @@ const Form: AppComponent<FormProps> = ({
     <Container
       onSubmit={async (event: React.FormEvent) => {
         event.preventDefault()
+        if (onSubmit == null) {
+          return
+        }
         setSubmitState(true)
         new Promise(() => onSubmit(event)).finally(() => setSubmitState(false))
       }}
       className={cc(['form', className])}
     >
-      {rows.map((row, i) => {
-        return (
-          <div className='form__row' key={`row--${i}`}>
-            {row.title != null && (
-              <div className='form__row__title'>{row.title}</div>
-            )}
-            {row.items != null && (
-              <div className='form__row__items'>
-                {row.items.map((item, k) => (
-                  <div
-                    className={`form__row__item form__row__item--${item.type}`}
-                    key={`form__row__item--${i}-${k}`}
-                  >
-                    {item.type === 'input' ? (
-                      <FormInput {...item.props} />
-                    ) : item.type === 'select' ? (
-                      <FormSelect {...item.props} />
-                    ) : item.type === 'emoji' ? (
-                      <FormEmoji {...item.props} />
-                    ) : null}
-                  </div>
-                ))}
-              </div>
-            )}
-            {row.description != null && (
-              <div className='form__row__description'>{row.description}</div>
-            )}
-          </div>
-        )
-      })}
+      {rows != null &&
+        rows.map((row, i) => {
+          return (
+            <div className='form__row' key={`row--${i}`}>
+              {row.title != null && (
+                <div className='form__row__title'>{row.title}</div>
+              )}
+              {row.items != null && (
+                <div className='form__row__items'>
+                  {row.items.map((item, k) => (
+                    <div
+                      className={`form__row__item form__row__item--${item.type}`}
+                      key={`form__row__item--${i}-${k}`}
+                    >
+                      {item.type === 'input' ? (
+                        <FormInput {...item.props} />
+                      ) : item.type === 'select' ? (
+                        <FormSelect {...item.props} />
+                      ) : item.type === 'emoji' ? (
+                        <FormEmoji {...item.props} />
+                      ) : null}
+                    </div>
+                  ))}
+                </div>
+              )}
+              {row.description != null && (
+                <div className='form__row__description'>{row.description}</div>
+              )}
+            </div>
+          )
+        })}
       {children}
       {submitButton != null && (
         <div className='form__row'>
@@ -98,6 +102,10 @@ export default Form
 
 const Container = styled.form`
   font-size: ${({ theme }) => theme.sizes.fonts.df}px;
+
+  .form__row__title {
+    margin-bottom: ${({ theme }) => theme.sizes.spaces.sm}px;
+  }
 
   .form__row__description {
     color: ${({ theme }) => theme.colors.text.subtle};
