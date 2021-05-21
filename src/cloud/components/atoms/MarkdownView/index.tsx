2d14dcdd32d19cb5961bf265f683c7d0f907c09d
Simon Leigh <sleigh.dev@gmail.com>
2021-05-21 14:23:30 +0900

    remove console log

--- src/cloud/components/atoms/MarkdownView/index.tsx
+++ src/cloud/components/atoms/MarkdownView/index.tsx
@@ -383,7 +383,6 @@ function getSelectionContext(
 
   const rangeStart = offset1 + selection.anchorOffset
   const rangeEnd = offset2 + selection.focusOffset
-  console.log(rangeStart, rangeEnd)
   return {
     start: Math.min(rangeStart, rangeEnd),
     end: Math.max(rangeStart, rangeEnd),
