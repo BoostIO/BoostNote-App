5cac6b71cda0489e6f70ae12eed22dc0900fb864
Elle Kasai <elle.kasai@gmail.com>
2021-06-07 17:41:44 +0900

    Added cursor: pointer;

--- src/cloud/components/GlobalStyle.tsx
+++ src/cloud/components/GlobalStyle.tsx
@@ -20,6 +20,9 @@ body {
 input,
 select,
 button {
+  &:hover {
+    cursor: pointer;
+  }
   &:focus {
     outline: none;
   }
@@ -80,6 +83,9 @@ export default createGlobalStyle<BaseTheme>`
   input,
   select,
   button {
+    &:hover {
+      cursor: pointer;
+    }
     &:focus {
       outline: none;
     }
