de7398089690f1c1fd1358f2c3428ac0ee85c9e1
davy-c <d.castel33@gmail.com>
2021-07-05 14:16:34 +0900

    search

---
+++ src/cloud/components/organisms/CloudGlobalSearch.tsx
@@ -0,0 +1,204 @@
+import React, { useCallback, useMemo, useState } from 'react'
+import { mdiFileDocumentOutline } from '@mdi/js'
+import { useDebounce } from 'react-use'
+import {
+  SidebarSearchHistory,
+  SidebarSearchResult,
+} from '../../../shared/components/organisms/SearchLayout'
+import useApi from '../../../shared/lib/hooks/useApi'
+import {
+  GetSearchResultsRequestQuery,
+  getSearchResultsV2,
+  HistoryItem,
+  SearchResult,
+} from '../../api/search'
+import { SerializedDoc } from '../../interfaces/db/doc'
+import { SerializedFolder } from '../../interfaces/db/folder'
+import { SerializedTeam } from '../../interfaces/db/team'
+import { useRouter } from '../../lib/router'
+import { useSearch } from '../../lib/stores/search'
+import { getDocTitle } from '../../lib/utils/patterns'
+import { getDocLinkHref } from '../atoms/Link/DocLink'
+import { getFolderHref } from '../atoms/Link/FolderLink'
+import SearchLayout from '../../../shared/components/organisms/SearchLayout'
+import { useNav } from '../../lib/stores/nav'
+import { useI18n } from '../../lib/hooks/useI18n'
+import { lngKeys } from '../../lib/i18n/types'
+
+interface CloudGlobalSearchProps {
+  team?: SerializedTeam
+}
+
+const CloudGlobalSearch = ({ team }: CloudGlobalSearchProps) => {
+  const { addToSearchHistory, searchHistory, history } = useSearch()
+  const { push } = useRouter()
+  const { translate } = useI18n()
+  const [searchQuery, setStateSearchQuery] = useState('')
+  const [searchResults, setSearchResults] = useState<SidebarSearchResult[]>([])
+  const setSearchQuery = useCallback((val: string) => {
+    setStateSearchQuery(val)
+  }, [])
+  const { docsMap, foldersMap } = useNav()
+
+  const { submit: submitSearch, sending: fetchingSearchResults } = useApi({
+    api: ({ teamId, query }: { teamId: string; query: any }) =>
+      getSearchResultsV2({ teamId, query }),
+    cb: ({ results }) =>
+      setSearchResults(mapSearchResults(results, push, team)),
+  })
+
+  const [isNotDebouncing, cancel] = useDebounce(
+    async () => {
+      if (team == null || searchQuery.trim() === '') {
+        return
+      }
+
+      if (fetchingSearchResults) {
+        cancel()
+      }
+
+      const searchParams = searchQuery
+        .split(' ')
+        .reduce<GetSearchResultsRequestQuery>(
+          (params, str) => {
+            if (str === '--body') {
+              params.body = true
+              return params
+            }
+            if (str === '--title') {
+              params.title = true
+              return params
+            }
+            params.query = params.query == '' ? str : `${params.query} ${str}`
+            return params
+          },
+          { query: '' }
+        )
+
+      addToSearchHistory(searchParams.query)
+      await submitSearch({ teamId: team.id, query: searchParams })
+    },
+    600,
+    [searchQuery]
+  )
+
+  const sidebarSearchState = useMemo(() => {
+    return {
+      fetching: fetchingSearchResults,
+      isNotDebouncing: isNotDebouncing() === true,
+    }
+  }, [isNotDebouncing, fetchingSearchResults])
+
+  const historyItems = useMemo(() => {
+    return mapHistory(history || [], push, docsMap, foldersMap, team)
+  }, [team, history, push, docsMap, foldersMap])
+
+  return (
+    <SearchLayout
+      searchPlaceholder={`${translate(lngKeys.GeneralSearchVerb)}...`}
+      recentlySearched={searchHistory}
+      recentlyVisited={historyItems}
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+      searchResults={searchResults}
+      searchState={sidebarSearchState}
+    />
+  )
+}
+
+export default CloudGlobalSearch
+
+function mapSearchResults(
+  results: SearchResult[],
+  push: (url: string) => void,
+  team?: SerializedTeam
+) {
+  if (team == null) {
+    return []
+  }
+
+  return results.reduce((acc, item) => {
+    if (item.type === 'folder') {
+      const href = `${process.env.BOOST_HUB_BASE_URL}${getFolderHref(
+        item.result,
+        team,
+        'index'
+      )}`
+      acc.push({
+        label: item.result.name,
+        href,
+        emoji: item.result.emoji,
+        onClick: () => push(href),
+      })
+      return acc
+    }
+
+    const href = `${process.env.BOOST_HUB_BASE_URL}${getDocLinkHref(
+      item.result,
+      team,
+      'index'
+    )}`
+    acc.push({
+      label: getDocTitle(item.result, 'Untitled'),
+      href,
+      defaultIcon: mdiFileDocumentOutline,
+      emoji: item.result.emoji,
+      contexts: item.type === 'docContent' ? [item.context] : undefined,
+      onClick: () => push(href),
+    })
+    return acc
+  }, [] as SidebarSearchResult[])
+}
+
+function mapHistory(
+  history: HistoryItem[],
+  push: (href: string) => void,
+  docsMap: Map<string, SerializedDoc>,
+  foldersMap: Map<string, SerializedFolder>,
+  team?: SerializedTeam
+) {
+  if (team == null) {
+    return []
+  }
+
+  const items = [] as SidebarSearchHistory[]
+
+  history.forEach((historyItem) => {
+    if (historyItem.type === 'folder') {
+      const item = foldersMap.get(historyItem.item)
+      if (item != null) {
+        const href = `${process.env.BOOST_HUB_BASE_URL}${getFolderHref(
+          item,
+          team,
+          'index'
+        )}`
+        items.push({
+          emoji: item.emoji,
+          label: item.name,
+          path: item.pathname,
+          href,
+          onClick: () => push(href),
+        })
+      }
+    } else {
+      const item = docsMap.get(historyItem.item)
+      if (item != null) {
+        const href = `${process.env.BOOST_HUB_BASE_URL}${getDocLinkHref(
+          item,
+          team,
+          'index'
+        )}`
+        items.push({
+          emoji: item.emoji,
+          defaultIcon: mdiFileDocumentOutline,
+          label: getDocTitle(item, 'Untitled'),
+          href,
+          path: item.folderPathname,
+          onClick: () => push(href),
+        })
+      }
+    }
+  })
+
+  return items
+}
---
+++ src/shared/components/organisms/SearchLayout/index.tsx
@@ -0,0 +1,421 @@
+import React, { useMemo, useRef } from 'react'
+import { useEffectOnce, useSet } from 'react-use'
+import cc from 'classcat'
+import UpDownList from '../../atoms/UpDownList'
+import SearchCategory from './molecules/SearchCategory'
+import SearchItem from './atoms/SearchItem'
+import CloseButtonWrapper from '../../molecules/CloseButtonWrapper'
+import styled from '../../../lib/styled'
+import plur from 'plur'
+import Spinner from '../../atoms/Spinner'
+import { overflowEllipsis } from '../../../lib/styled/styleFunctions'
+import FormInput from '../../molecules/Form/atoms/FormInput'
+import { mdiMagnify } from '@mdi/js'
+import Icon from '../../atoms/Icon'
+
+interface SearchLayoutProps {
+  searchPlaceholder: string
+  searchQuery: string
+  setSearchQuery: (val: string) => void
+  className?: string
+  recentlyVisited?: SidebarSearchHistory[]
+  recentlySearched?: string[]
+  searchResults?: SidebarSearchResult[]
+  searchState: SidebarSearchState
+}
+
+export type SidebarSearchState = { isNotDebouncing: boolean; fetching: boolean }
+
+export type SidebarSearchResult = {
+  label: string
+  href: string
+  contexts?: string[]
+  onClick: () => void
+  emoji?: string
+  defaultIcon?: string
+}
+
+export type SidebarSearchHistory = {
+  emoji?: string
+  defaultIcon?: string
+  path?: string
+  label: string
+  href: string
+  onClick: () => void
+}
+
+const SearchLayout = ({
+  searchPlaceholder,
+  searchResults = [],
+  searchQuery,
+  setSearchQuery,
+  className,
+  recentlySearched = [],
+  recentlyVisited = [],
+  searchState: { isNotDebouncing, fetching },
+}: SearchLayoutProps) => {
+  const [, { has, add, remove, toggle }] = useSet<string>(new Set())
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffectOnce(() => {
+    inputRef.current?.focus()
+  })
+
+  const results = useMemo(() => {
+    return searchResults.reduce(
+      (acc, val) => {
+        const labelMatch = val.label
+          .toLocaleLowerCase()
+          .includes(searchQuery.toLocaleLowerCase())
+        if (labelMatch && val.contexts == null) {
+          acc.items.push(val)
+        } else if (val.contexts != null) {
+          acc.contextItems.push(val)
+        } else {
+          acc.similar.push(val)
+        }
+        return acc
+      },
+      {
+        items: [],
+        contextItems: [],
+        similar: [],
+      } as {
+        items: SidebarSearchResult[]
+        contextItems: SidebarSearchResult[]
+        similar: SidebarSearchResult[]
+      }
+    )
+  }, [searchResults, searchQuery])
+
+  return (
+    <Container className={cc(['sidebar__search', className])}>
+      <UpDownList className='sidebar__search__wrapper'>
+        <div className='sidebar__search__header'>
+          <CloseButtonWrapper
+            onClick={() => setSearchQuery('')}
+            show={searchQuery !== ''}
+            className='sidebar__search__input__wrapper'
+          >
+            <Icon
+              path={mdiMagnify}
+              size={16}
+              className='sidebar__search__icon'
+            />
+            <FormInput
+              className='sidebar__search__input'
+              placeholder={searchPlaceholder}
+              id='sidebar__search__input'
+              value={searchQuery}
+              onChange={(ev) => setSearchQuery(ev.target.value)}
+              ref={inputRef}
+            />
+          </CloseButtonWrapper>
+        </div>
+        <div className='sidebar__search__results'>
+          <div className='sidebar__search__results__wrapper'>
+            {searchQuery.trim() === '' && (
+              <>
+                {recentlySearched.length > 0 && (
+                  <>
+                    <SearchCategory
+                      id='sidebar__search__keywords'
+                      folded={has('keywords')}
+                      unfold={() => remove('keywords')}
+                      fold={() => add('keywords')}
+                      toggle={() => toggle('keywords')}
+                      className='sidebar__search__category'
+                    >
+                      Recently Searched
+                    </SearchCategory>
+                    {!has('keywords') &&
+                      recentlySearched.map((keyword) => (
+                        <SearchItem
+                          label={keyword}
+                          defaultIcon={mdiMagnify}
+                          labelClick={() => setSearchQuery(keyword)}
+                          key={`recently-searched-${keyword}`}
+                          id={`recently-searched-${keyword}`}
+                        />
+                      ))}
+                  </>
+                )}
+                {recentlyVisited.length > 0 && (
+                  <>
+                    <SearchCategory
+                      id='sidebar__search__history'
+                      folded={has('history')}
+                      unfold={() => remove('history')}
+                      fold={() => add('history')}
+                      toggle={() => toggle('history')}
+                      className='sidebar__search__category'
+                    >
+                      Recently Visited
+                    </SearchCategory>
+                    {!has('history') &&
+                      recentlyVisited.map((item, i) => (
+                        <SearchItem
+                          label={item.label}
+                          labelHref={item.href}
+                          path={item.path}
+                          labelClick={item.onClick}
+                          defaultIcon={item.defaultIcon}
+                          emoji={item.emoji}
+                          key={`recently-visited-${i}`}
+                          id={`recently-visited-${i}`}
+                        />
+                      ))}
+                  </>
+                )}
+              </>
+            )}
+
+            {searchQuery.trim() !== '' && (fetching || !isNotDebouncing) && (
+              <>
+                {fetching ? (
+                  <Spinner
+                    style={{
+                      position: 'absolute',
+                      right: 0,
+                      left: 0,
+                      margin: 'auto',
+                      bottom: 0,
+                      top: 0,
+                    }}
+                  />
+                ) : (
+                  <div className='sidebar__search__empty'>..</div>
+                )}
+              </>
+            )}
+
+            {searchQuery.trim() !== '' &&
+              searchResults.length === 0 &&
+              !fetching &&
+              isNotDebouncing && (
+                <>
+                  <strong className='sidebar__search__empty'>No results</strong>
+                  <div className='sidebar__search__empty'>
+                    Try a different query.
+                  </div>
+                </>
+              )}
+
+            {searchQuery.trim() !== '' &&
+              searchResults.length > 0 &&
+              !fetching &&
+              isNotDebouncing && (
+                <>
+                  {results.items.length + results.contextItems.length === 0 ? (
+                    <>
+                      <strong className='sidebar__search__empty'>
+                        No matches
+                      </strong>
+                      <div className='sidebar__search__empty'>
+                        Try a different query.
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      {results.items.length === 0 ? null : (
+                        <>
+                          <SearchCategory
+                            id='sidebar__search__matches'
+                            folded={has('matches')}
+                            unfold={() => remove('matches')}
+                            fold={() => add('matches')}
+                            toggle={() => toggle('matches')}
+                            className='sidebar__search__category'
+                          >
+                            {results.items.length}{' '}
+                            {plur('item', results.items.length)}
+                          </SearchCategory>
+                          {!has('matches') &&
+                            results.items.map((result, i) => (
+                              <SearchItem
+                                id={`sidebar__result--matches__${i}`}
+                                key={`sidebar__result--matches__${i}`}
+                                label={result.label}
+                                labelHref={result.href}
+                                labelClick={result.onClick}
+                                emoji={result.emoji}
+                                defaultIcon={result.defaultIcon}
+                                highlighted={searchQuery}
+                              />
+                            ))}
+                        </>
+                      )}
+
+                      {results.contextItems.length === 0 ? null : (
+                        <>
+                          <SearchCategory
+                            id='sidebar__search__context__matches'
+                            folded={has('context__matches')}
+                            unfold={() => remove('context__matches')}
+                            fold={() => add('context__matches')}
+                            toggle={() => toggle('context__matches')}
+                            className='sidebar__search__category'
+                          >
+                            Found in {results.contextItems.length}{' '}
+                            {plur('item', results.contextItems.length)}
+                          </SearchCategory>
+                          {!has('context__matches') &&
+                            results.contextItems.map((result, i) => (
+                              <SearchItem
+                                id={`sidebar__result__${i}`}
+                                key={`sidebar__result__${i}`}
+                                label={result.label}
+                                labelHref={result.href}
+                                labelClick={result.onClick}
+                                contexts={result.contexts}
+                                emoji={result.emoji}
+                                defaultIcon={result.defaultIcon}
+                                highlighted={searchQuery}
+                                folded={
+                                  result.contexts != null
+                                    ? has(`${result.label}-${i}`)
+                                    : undefined
+                                }
+                                folding={
+                                  result.contexts != null
+                                    ? {
+                                        unfold: () =>
+                                          remove(`${result.label}-${i}`),
+                                        fold: () => add(`${result.label}-${i}`),
+                                        toggle: () =>
+                                          toggle(`${result.label}-${i}`),
+                                      }
+                                    : undefined
+                                }
+                              />
+                            ))}
+                        </>
+                      )}
+                    </>
+                  )}
+
+                  {results.similar.length === 0 ? null : (
+                    <>
+                      <SearchCategory
+                        id='sidebar__search__similarities'
+                        folded={has('similarities')}
+                        unfold={() => remove('similarities')}
+                        fold={() => add('similarities')}
+                        toggle={() => toggle('similarities')}
+                        className='sidebar__search__category'
+                      >
+                        {results.similar.length} Similar{' '}
+                        {plur('result', results.similar.length)}
+                      </SearchCategory>
+                      {!has('similarities') &&
+                        results.similar.map((result, i) => (
+                          <SearchItem
+                            id={`sidebar__result--similar__${i}`}
+                            key={`sidebar__result--similar__${i}`}
+                            label={result.label}
+                            labelHref={result.href}
+                            labelClick={result.onClick}
+                            emoji={result.emoji}
+                            defaultIcon={result.defaultIcon}
+                            highlighted={searchQuery}
+                          />
+                        ))}
+                    </>
+                  )}
+                </>
+              )}
+          </div>
+        </div>
+      </UpDownList>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  flex: 1 1 0;
+  min-width: 0;
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+
+  .search__category + .search__category,
+  .search__item + .search__category {
+    margin-top: ${({ theme }) => theme.sizes.spaces.md}px;
+  }
+
+  .sidebar__search__header {
+    flex: 0 0 auto;
+  }
+
+  .sidebar__search__input__wrapper {
+    width: 100%;
+    position: relative;
+    flex: 0 0 auto;
+    input {
+      border-top: 0 !important;
+      border-left: 0 !important;
+      border-right: 0 !important;
+      border-radus: 0 !important;
+      padding-left: ${({ theme }) => theme.sizes.spaces.l}px;
+    }
+
+    .sidebar__search__icon {
+      position: absolute;
+      left: 10px;
+    }
+  }
+
+  .sidebar__search__results {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    overflow: auto;
+    flex: 1 1 auto;
+    padding: ${({ theme }) => theme.sizes.spaces.df}px 0;
+  }
+
+  .sidebar__search__results__wrapper {
+    flex: 1 1 auto;
+    width: 100%;
+    max-width: 1200px;
+    margin: auto;
+  }
+
+  .sidebar__search__empty {
+    margin: 0px ${({ theme }) => theme.sizes.spaces.df}px;
+    font-size: ${({ theme }) => theme.sizes.fonts.df}px;
+    color: ${({ theme }) => theme.colors.text.subtle};
+  }
+
+  .sidebar__search__wrapper {
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .sidebar__search__category {
+    display: flex;
+    justify-content: flex-start;
+    margin: 0px ${({ theme }) => theme.sizes.spaces.df}px;
+    font-size: ${({ theme }) => theme.sizes.fonts.md}px;
+    white-space: nowrap;
+    text-align: left;
+
+    .button__label {
+      ${overflowEllipsis}
+    }
+  }
+
+  .sidebar__search__empty + .sidebar__search__category,
+  .sidebar__search__item + .sidebar__search__category {
+    margin-top: ${({ theme }) => theme.sizes.spaces.df}px;
+  }
+
+  .sidebar__search__item {
+    margin: 0 !important;
+  }
+`
+
+export default SearchLayout
