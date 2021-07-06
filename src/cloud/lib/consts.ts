70dff47d3586a523c925faf7b68e1040f8ecef20
Junyoung Choi <fluke8259@gmail.com>
2021-07-06 09:37:55 +0900

    Fix invalid backup realtime server url

--- src/cloud/lib/consts.ts
+++ src/cloud/lib/consts.ts
@@ -3,7 +3,7 @@ export const nodeEnv = process.env.NODE_ENV || 'development'
 export const intercomAppId = process.env.INTERCOM_APP_ID || 'elidid'
 export const boostHubBaseUrl =
   process.env.BOOST_HUB_BASE_URL || 'http://localhost:3001'
-export const realtimeUrl = process.env.REALTIME_URL || 'http://localhost:3002'
+export const realtimeUrl = process.env.REALTIME_URL || 'http://localhost:3003'
 export const sseUrl = process.env.SSE_URL || 'http://localhost:3002'
 export const stripePublishableKey =
   process.env.STRIPE_PUBLISHABLE_KEY || 'elidid'
