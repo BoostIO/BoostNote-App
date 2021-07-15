1322822b2a91ee516167f173b591a4aa71716d87
Junyoung Choi <fluke8259@gmail.com>
2021-07-15 12:42:23 +0900

    Detect mobile os name

--- src/shared/lib/platform.ts
+++ src/shared/lib/platform.ts
@@ -1,10 +1,19 @@
-export type OsNameOptions = 'windows' | 'macos' | 'unix' | 'linux' | 'unknown'
+export type OsNameOptions =
+  | 'windows'
+  | 'macos'
+  | 'unix'
+  | 'linux'
+  | 'unknown'
+  | 'ios'
+  | 'android'
 
 function getOsName(): OsNameOptions {
-  if (navigator.appVersion.indexOf('Win') != -1) return 'windows'
-  if (navigator.appVersion.indexOf('Mac') != -1) return 'macos'
-  if (navigator.appVersion.indexOf('X11') != -1) return 'unix'
-  if (navigator.appVersion.indexOf('Linux') != -1) return 'linux'
+  if (navigator.userAgent.indexOf('Android') != -1) return 'android'
+  if (navigator.userAgent.indexOf('iPhone') != -1) return 'ios'
+  if (navigator.userAgent.indexOf('Win') != -1) return 'windows'
+  if (navigator.userAgent.indexOf('Mac') != -1) return 'macos'
+  if (navigator.userAgent.indexOf('X11') != -1) return 'unix'
+  if (navigator.userAgent.indexOf('Linux') != -1) return 'linux'
 
   return 'unknown'
 }
