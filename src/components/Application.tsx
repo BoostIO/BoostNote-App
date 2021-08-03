f540ae20d6f43532364f7fba4d98ea12653c1122
Junyoung Choi <fluke8259@gmail.com>
2021-08-03 19:15:24 +0900

    Discard LocalGlobalSearch

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,16 +1,14 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useMemo } from 'react'
 import ContentLayout, {
   ContentLayoutProps,
 } from '../shared/components/templates/ContentLayout'
 import { useRouter } from '../lib/router'
 import { LocalSpaceRouteParams, useRouteParams } from '../lib/routeParams'
-import { mapTopBarTree } from '../lib/v2/mappers/local/topbarTree'
 import { useDb } from '../lib/db'
 import { useGeneralStatus } from '../lib/generalStatus'
 import { addIpcListener, removeIpcListener } from '../lib/electronOnly'
 import SidebarContainer from './organisms/SidebarContainer'
 import ApplicationLayout from '../shared/components/molecules/ApplicationLayout'
-import LocalGlobalSearch from './organisms/LocalGlobalSearch'
 import { NoteDoc } from '../lib/db/types'
 
 interface ApplicationProps {
@@ -28,35 +26,16 @@ const Application = ({
   const { workspaceId } = routeParams
   const storage = storageMap[workspaceId]
 
-  const { push, goBack, goForward, pathname } = useRouter()
+  const { goBack, goForward } = useRouter()
   const { generalStatus, setGeneralStatus } = useGeneralStatus()
   const { noteViewMode, preferredEditingViewMode } = generalStatus
   const { bookmarkNote, unbookmarkNote } = useDb()
-  const [showSearchScreen, setShowSearchScreen] = useState<boolean>()
-
-  const previousPathnameRef = useRef(pathname)
-  useEffect(() => {
-    if (previousPathnameRef.current === pathname) {
-      return
-    }
-    previousPathnameRef.current = pathname
-    return () => {
-      setShowSearchScreen(false)
-    }
-  }, [pathname])
 
   const note = useMemo<NoteDoc | undefined>(() => {
     return undefined
   }, [])
 
   const noteId = note?._id
-
-  const topbarTree = useMemo(() => {
-    if (storage == null) {
-      return undefined
-    }
-    return mapTopBarTree(storage.noteMap, storage.folderMap, storage, push)
-  }, [push, storage])
 
   const bookmark = useCallback(async () => {
     if (noteId == null || storage == null) {
@@ -154,26 +133,18 @@ const Application = ({
     <ApplicationLayout
       sidebar={<SidebarContainer workspace={storage} />}
       pageBody={
-        showSearchScreen ? (
-          <LocalGlobalSearch
-            workspace={storage}
-            closeSearch={() => setShowSearchScreen(false)}
-          />
-        ) : (
-          <ContentLayout
-            {...content}
-            topbar={{
-              ...topbar,
-              tree: topbarTree,
-              navigation: {
-                goBack,
-                goForward,
-              },
-            }}
-          >
-            {children}
-          </ContentLayout>
-        )
+        <ContentLayout
+          {...content}
+          topbar={{
+            ...topbar,
+            navigation: {
+              goBack,
+              goForward,
+            },
+          }}
+        >
+          {children}
+        </ContentLayout>
       }
     />
   )
