fd8f26603a2a14fd353be3721bd594980cc82c2b
Simon Leigh <sleigh.dev@gmail.com>
2021-08-03 12:08:03 +0900

    blocks store

--- src/cloud/lib/hooks/useDocBlocks.ts
+++ src/cloud/lib/hooks/useDocBlocks.ts
@@ -1,20 +1,36 @@
 import { useBlocks } from '../stores/blocks'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { Block } from '../../api/blocks'
 
-type BlockState =
-  | { type: 'loading' }
-  | { type: 'loaded'; blocks: Block<any, any>[] }
+type BlockState = { type: 'loading' } | { type: 'loaded'; block: Block }
+
+export interface BlockActions {
+  create: (block: Omit<Block, 'id'>, parent: Block) => Promise<Block>
+  update: (block: Block) => Promise<Block>
+  remove: (block: Block) => Promise<void>
+}
 
 export function useDocBlocks(id: string) {
-  const { observeDocBlocks, ...actions } = useBlocks()
+  const { observeDocBlocks, create, update, remove } = useBlocks()
   const [state, setState] = useState<BlockState>({ type: 'loading' })
 
   useEffect(() => {
-    return observeDocBlocks(id, (blocks) => {
-      setState({ type: 'loaded', blocks })
+    const unsub = observeDocBlocks(id, (block) => {
+      setState({ type: 'loaded', block })
     })
+    return () => {
+      setState({ type: 'loading' })
+      unsub()
+    }
   }, [id, observeDocBlocks])
+
+  const actions: BlockActions = useMemo(() => {
+    return {
+      create: (block, parent) => create(block, parent, id),
+      update: (block) => update(block, id),
+      remove: (block) => remove(block, id),
+    }
+  }, [id, create, update, remove])
 
   return {
     state,
