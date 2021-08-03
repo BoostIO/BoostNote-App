ce0de747c73ee7c1048146c2e84ff5bf5dc28347
Simon Leigh <sleigh.dev@gmail.com>
2021-08-03 12:05:48 +0900

    update doc interface

--- src/cloud/interfaces/db/doc.ts
+++ src/cloud/interfaces/db/doc.ts
@@ -5,6 +5,7 @@ import type { SerializedTag } from './tag'
 import { SerializedWorkspace } from './workspace'
 import { SerializedShareLink } from './shareLink'
 import { SerializedUser } from './user'
+import { ContainerBlock } from '../../api/blocks'
 
 export type DocStatus = 'in_progress' | 'completed' | 'archived' | 'paused'
 
@@ -30,6 +31,7 @@ export interface SerializableDocProps {
   dueDate: string
   assignees?: SerializedDocAssignee[]
   userId?: string
+  rootBlock?: ContainerBlock
 }
 
 export interface SerializedUnserializableDocProps {
