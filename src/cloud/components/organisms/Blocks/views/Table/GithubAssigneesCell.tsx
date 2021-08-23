2d7a8627a7cc840bc9d4f586fb38801dacf0a20b
Simon Leigh <sleigh.dev@gmail.com>
2021-08-23 14:52:36 +0900

    Github cells

---
+++ src/cloud/components/organisms/Blocks/views/Table/GithubAssigneesCell.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyledUserIcon } from '../../../../atoms/UserIcon'
+
+interface Assignee {
+  id: number
+  avatar_url: string
+  login: string
+}
+
+interface GitHubAssigneesCellProps {
+  assignees: Assignee[]
+}
+
+const GitHubAssigneesCell = ({ assignees }: GitHubAssigneesCellProps) => {
+  return (
+    <div>
+      {assignees.map((user) => (
+        <StyledUserIcon className='subtle'>
+          <img src={user.avatar_url} alt={user.login[0]} />
+        </StyledUserIcon>
+      ))}
+    </div>
+  )
+}
+
+export default GitHubAssigneesCell
---
+++ src/cloud/components/organisms/Blocks/views/Table/GithubLabelsCell.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from '../../../../../../shared/lib/styled'
+
+interface Label {
+  name: string
+  color: string
+}
+
+interface GithubLabelsCellProps {
+  labels: Label[]
+}
+
+const GithubLabelsCell = ({ labels }: GithubLabelsCellProps) => {
+  return (
+    <StyledGithubLabels>
+      {labels.map((label) => (
+        <span style={{ backgroundColor: `#${label.color}` }}>{label.name}</span>
+      ))}
+    </StyledGithubLabels>
+  )
+}
+
+const StyledGithubLabels = styled.div`
+  display: flex;
+  span {
+    margin-right: ${({ theme }) => theme.sizes.spaces.xsm}px;
+    padding: ${({ theme }) => theme.sizes.spaces.xsm}px;
+    border-radius: 5px;
+    font-size: ${({ theme }) => theme.sizes.fonts.sm}px;
+  }
+`
+
+export default GithubLabelsCell
