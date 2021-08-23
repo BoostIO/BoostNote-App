cc71e7fb956e41c9a5b2c069a0d3324ee118970e
Simon Leigh <sleigh.dev@gmail.com>
2021-08-23 14:52:36 +0900

    Github cells

---
+++ src/cloud/components/organisms/Blocks/views/Table/GithubStatusCell.tsx
@@ -0,0 +1,41 @@
+import { mdiAlertCircleCheckOutline, mdiAlertCircleOutline } from '@mdi/js'
+import React from 'react'
+import {
+  SuccessIcon,
+  WarningIcon,
+} from '../../../../../../shared/components/atoms/Icon'
+import styled from '../../../../../../shared/lib/styled'
+import { capitalize } from '../../../../../lib/utils/string'
+
+interface GithubStatusCellProps {
+  state: 'open' | 'closed'
+}
+
+const GithubStatusCell = ({ state }: GithubStatusCellProps) => {
+  return (
+    <StyledStatus>
+      {state === 'open' ? (
+        <SuccessIcon path={mdiAlertCircleOutline} />
+      ) : (
+        <WarningIcon path={mdiAlertCircleCheckOutline} />
+      )}
+      <span>{capitalize(state)}</span>
+    </StyledStatus>
+  )
+}
+
+const StyledStatus = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > svg:first-child {
+    margin-right: ${({ theme }) => theme.sizes.spaces.xsm}px;
+  }
+
+  & > span {
+    flex-grow: 1;
+    text-align: left;
+  }
+`
+
+export default GithubStatusCell
