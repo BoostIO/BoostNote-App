b9d4401445834e50c3b682e4b00c631a9c17f49f
davy-c <d.castel33@gmail.com>
2021-08-25 11:09:57 +0900

    prevent popping red errors when not initialized

--- src/cloud/components/ApplicationContent.tsx
+++ src/cloud/components/ApplicationContent.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Spinner from '../../design/components/atoms/Spinner'
 import {
   ContentWrapper,
   ContentWrapperProps,
 } from '../../design/components/templates/ContentLayout'
+import { useNav } from '../lib/stores/nav'
 
-const ApplicationContent: React.FC<ContentWrapperProps> = ({ children }) => (
-  <ContentWrapper>{children}</ContentWrapper>
-)
+const ApplicationContent: React.FC<ContentWrapperProps> = ({ children }) => {
+  const { initialLoadDone } = useNav()
+
+  if (!initialLoadDone) {
+    return (
+      <ContentWrapper>
+        <Spinner />
+      </ContentWrapper>
+    )
+  }
+
+  return <ContentWrapper>{children}</ContentWrapper>
+}
 
 export default ApplicationContent
