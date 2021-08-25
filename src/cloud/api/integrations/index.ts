de5f6ad8aaf7392c8960478553717592db0bc088
Simon Leigh <sleigh.dev@gmail.com>
2021-08-25 16:17:29 +0900

    new integration actions

--- src/cloud/api/integrations/index.ts
+++ src/cloud/api/integrations/index.ts
@@ -30,15 +30,41 @@ export interface IntegrationActionTypes {
     }
   }[]
   ['repo:issues']: any[]
+  ['repo:labels']: {
+    id: number
+    name: string
+    color: string
+    description: string
+  }[]
+  ['repo:collaborators']: { id: number; avatar_url: string; login: string }[]
 }
 
 export async function getAction<A extends keyof IntegrationActionTypes>(
-  integration: SerializedTeamIntegration,
+  integration: Pick<SerializedTeamIntegration, 'id'>,
   action: A,
   args?: Record<string, any>
 ): Promise<IntegrationActionTypes[A]> {
   const { data } = await callApi(`/api/integrations/${integration.id}/action`, {
     search: { action, ...args },
+  })
+  return data
+}
+
+export interface IntegrationPostActionTypes {
+  ['issue:assign']: any
+  ['issue:update']: any
+}
+
+export async function postAction<A extends keyof IntegrationPostActionTypes>(
+  integration: Pick<SerializedTeamIntegration, 'id'>,
+  action: A,
+  args?: Record<string, any>,
+  body?: any
+): Promise<IntegrationPostActionTypes[A]> {
+  const { data } = await callApi(`/api/integrations/${integration.id}/action`, {
+    search: { action, ...args },
+    json: body,
+    method: 'post',
   })
   return data
 }
