6bbeda618c7c20e9b1f18845a8eb83b3cb10ac2c
Simon Leigh <sleigh.dev@gmail.com>
2021-09-03 17:41:15 +0900

    block prop module

---
+++ src/cloud/lib/blocks/props.ts
@@ -0,0 +1,36 @@
+const PROP_TYPES = [
+  'text',
+  'number',
+  'date',
+  'url',
+  'checkbox',
+  'user',
+] as const
+
+export type PropType = typeof PROP_TYPES[number]
+export type PropKey = `${string}:${PropType}`
+
+export function isPropKey(k: any): k is PropKey {
+  if (typeof k !== 'string') {
+    return false
+  }
+
+  const [_key, type, ...rest] = k.split(':')
+  if (rest.length !== 0) {
+    return false
+  }
+
+  return PROP_TYPES.includes(type as any)
+}
+
+export function makePropKey(name: string, type: PropType): PropKey {
+  return `${name}:${type}`
+}
+
+export function getPropName(type: PropKey): string {
+  return type.split(':')[0]
+}
+
+export function getPropType(type: PropKey): PropType {
+  return type.split(':')[1] as PropType
+}
