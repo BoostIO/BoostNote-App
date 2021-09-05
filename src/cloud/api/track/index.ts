4bc7302f89f6d185f6347a7e5a19d4231250addb
Junyoung Choi <fluke8259@gmail.com>
2021-09-06 02:45:06 +0900

    Disable tracking when mock backend is enabled

--- src/cloud/api/track/index.ts
+++ src/cloud/api/track/index.ts
@@ -1,10 +1,14 @@
 import { MixpanelFrontEvent } from '../../interfaces/analytics/mixpanel'
 import { callApi } from '../../lib/client'
+import { mockBackend } from '../../lib/consts'
 
 export async function trackEvent(
   eventName: MixpanelFrontEvent,
   data?: Record<string, any>
 ) {
+  if (mockBackend) {
+    return
+  }
   try {
     return callApi('api/track', {
       json: { ...data, eventName },
