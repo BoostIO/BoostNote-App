b1de8165b7cf81b77f502841abc56e683db73e8d
Junyoung Choi <fluke8259@gmail.com>
2021-09-06 03:06:02 +0900

    Add SetMap

--- src/cloud/api/mock/db/utils.ts
+++ src/cloud/api/mock/db/utils.ts
@@ -7,3 +7,29 @@ export function generateMockId() {
 export function getCurrentTime() {
   return new Date().toISOString()
 }
+
+export class SetMap<T> {
+  map: Map<string, Set<T>>
+
+  constructor() {
+    this.map = new Map()
+  }
+
+  getSet(key: string) {
+    return this.map.get(key) || new Set<T>()
+  }
+
+  removeSet(key: string) {
+    this.map.delete(key)
+  }
+
+  addValue(key: string, value: T) {
+    const set = this.getSet(key)
+    set.add(value)
+  }
+
+  removeValue(key: string, value: T) {
+    const set = this.getSet(key)
+    set.delete(value)
+  }
+}
