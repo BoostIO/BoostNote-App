e37da1b8a05eb90ff6171c2374ea45e295c783ca
davy-c <d.castel33@gmail.com>
2021-09-07 07:56:34 +0900

    fix tables and UI;

--- src/cloud/lib/utils/blocks.ts
+++ src/cloud/lib/utils/blocks.ts
@@ -1,3 +1,4 @@
+import { capitalize } from 'lodash'
 import { compareDateString } from '../../../design/lib/date'
 import { Block } from '../../api/blocks'
 
@@ -13,4 +14,17 @@ export function orderBlockChildren({ children, ...rest }: Block): Block {
             .map((block) => orderBlockChildren(block))
         : [],
   } as Block
+}
+
+export function blockTitle(block: Block) {
+  switch (block.type) {
+    case 'github.issue':
+      return block.data?.title || 'Github Issue'
+    case 'embed':
+    case 'table':
+    case 'container':
+      return block.name.trim() === '' ? capitalize(block.type) : block.name
+    default:
+      return capitalize(block.type)
+  }
 }
