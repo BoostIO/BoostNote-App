Iceymann18777/BoostNote.next

Update new doc button

src/cloud/components/molecules/NewDocButton.tsx
@@ -1,7 +1,15 @@
-import { mdiPencilBoxMultipleOutline, mdiTextBoxPlus } from '@mdi/js'
-import React from 'react'
+import {
+  mdiPencilBoxMultipleOutline,
+  mdiPackageVariantClosed,
+  mdiTextBoxPlusOutline,
+  mdiPencilBoxOutline,
+} from '@mdi/js'
+import React, { useCallback } from 'react'
 import SidebarButton from '../../../shared/components/organisms/Sidebar/atoms/SidebarButton'
-import { MenuTypes } from '../../../shared/lib/stores/contextMenu'
+import {
+  MenuTypes,
+  useContextMenu,
+} from '../../../shared/lib/stores/contextMenu'
 import { useModal } from '../../../shared/lib/stores/modal'
 import { SerializedTeam } from '../../interfaces/db/team'
 import { useCloudResourceModals } from '../../lib/hooks/useCloudResourceModals'
@@ -20,31 +28,64 @@ const NewDocButton = ({ team }: { team: SerializedTeam }) => {
   const { openNewDocForm } = useCloudResourceModals()
   const { openModal } = useModal()
   const { translate } = useI18n()
+  const { popup } = useContextMenu()
 
+  const openNewDocModal = useCallback(
+    (isCanvas = false) => {
+      openNewDocForm(
+        {
+          team,
+          parentFolderId: currentParentFolderId,
+          workspaceId: currentWorkspaceId,
+          blocks: isCanvas,
+        },
+        {
+          precedingRows: [
+            {
+              description: `${
+                workspacesMap.get(currentWorkspaceId || '')?.name
+              }${currentPath}`,
+            },
+          ],
+        }
+      )
+    },
+    [
+      openNewDocForm,
+      workspacesMap,
+      currentWorkspaceId,
+      team,
+      currentParentFolderId,
+      currentPath,
+    ]
+  )
+
+  const openDocTypeSelect: React.MouseEventHandler = useCallback(
+    (ev) => {
+      popup(ev, [
+        {
+          icon: mdiTextBoxPlusOutline,
+          type: MenuTypes.Normal,
+          label: translate(lngKeys.CreateNewDoc),
+          onClick: () => openNewDocModal(),
+        },
+        {
+          icon: mdiPackageVariantClosed,
+          type: MenuTypes.Normal,
+          label: translate(lngKeys.CreateNewCanvas),
+          onClick: () => openNewDocModal(true),
+        },
+      ])
+    },
+    [openNewDocModal, popup, translate]
+  )
   return (
     <SidebarButton
       variant='primary'
-      icon={mdiTextBoxPlus}
+      icon={mdiPencilBoxOutline}
       id='sidebar-newdoc-btn'
       label={translate(lngKeys.CreateNewDoc)}
-      labelClick={() =>
-        openNewDocForm(
-          {
-            team,
-            parentFolderId: currentParentFolderId,
-            workspaceId: currentWorkspaceId,
-          },
-          {
-            precedingRows: [
-              {
-                description: `${
-                  workspacesMap.get(currentWorkspaceId || '')?.name
-                }${currentPath}`,
-              },
-            ],
-          }
-        )
-      }
+      labelClick={openDocTypeSelect}
       contextControls={[
         {
           icon: mdiPencilBoxMultipleOutline,

BoostIO/BoostNote-App

src/cloud/components/buttons/NewDocButton.tsx
@@ -1,8 +1,16 @@
-import { mdiPencilBoxMultipleOutline, mdiTextBoxPlus } from '@mdi/js'
-import React from 'react'
 import SidebarButton from '../../../design/components/organisms/Sidebar/atoms/SidebarButton'
-import { MenuTypes } from '../../../design/lib/stores/contextMenu'
+import {
+  MenuTypes,
+  useContextMenu,
+} from '../../../design/lib/stores/contextMenu'
 import { useModal } from '../../../design/lib/stores/modal'
+import {
+  mdiPencilBoxMultipleOutline,
+  mdiPackageVariantClosed,
+  mdiTextBoxPlusOutline,
+  mdiPencilBoxOutline,
+} from '@mdi/js'
+import React, { useCallback } from 'react'
 import { SerializedTeam } from '../../interfaces/db/team'
 import { useCloudResourceModals } from '../../lib/hooks/useCloudResourceModals'
 import { useI18n } from '../../lib/hooks/useI18n'
@@ -20,31 +28,64 @@ const NewDocButton = ({ team }: { team: SerializedTeam }) => {
   const { openNewDocForm } = useCloudResourceModals()
   const { openModal } = useModal()
   const { translate } = useI18n()
+  const { popup } = useContextMenu()
 
+  const openNewDocModal = useCallback(
+    (isCanvas = false) => {
+      openNewDocForm(
+        {
+          team,
+          parentFolderId: currentParentFolderId,
+          workspaceId: currentWorkspaceId,
+          blocks: isCanvas,
+        },
+        {
+          precedingRows: [
+            {
+              description: `${
+                workspacesMap.get(currentWorkspaceId || '')?.name
+              }${currentPath}`,
+            },
+          ],
+        }
+      )
+    },
+    [
+      openNewDocForm,
+      workspacesMap,
+      currentWorkspaceId,
+      team,
+      currentParentFolderId,
+      currentPath,
+    ]
+  )
+
+  const openDocTypeSelect: React.MouseEventHandler = useCallback(
+    (ev) => {
+      popup(ev, [
+        {
+          icon: mdiTextBoxPlusOutline,
+          type: MenuTypes.Normal,
+          label: translate(lngKeys.CreateNewDoc),
+          onClick: () => openNewDocModal(),
+        },
+        {
+          icon: mdiPackageVariantClosed,
+          type: MenuTypes.Normal,
+          label: translate(lngKeys.CreateNewCanvas),
+          onClick: () => openNewDocModal(true),
+        },
+      ])
+    },
+    [openNewDocModal, popup, translate]
+  )
   return (
     <SidebarButton
       variant='primary'
-      icon={mdiTextBoxPlus}
+      icon={mdiPencilBoxOutline}
       id='sidebar-newdoc-btn'
       label={translate(lngKeys.CreateNewDoc)}
-      labelClick={() =>
-        openNewDocForm(
-          {
-            team,
-            parentFolderId: currentParentFolderId,
-            workspaceId: currentWorkspaceId,
-          },
-          {
-            precedingRows: [
-              {
-                description: `${
-                  workspacesMap.get(currentWorkspaceId || '')?.name
-                }${currentPath}`,
-              },
-            ],
-          }
-        )
-      }
+      labelClick={openDocTypeSelect}
       contextControls={[
         {
           icon: mdiPencilBoxMultipleOutline,