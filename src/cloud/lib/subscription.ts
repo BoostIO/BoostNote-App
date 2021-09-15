31596a8d2ebb00aa65274ee6ef29471c7cc46f61
davy-c <d.castel33@gmail.com>
2021-09-15 15:51:35 +0900

    revision days

--- src/cloud/lib/subscription.ts
+++ src/cloud/lib/subscription.ts
@@ -6,7 +6,7 @@ export const freePlanStorageMb = 100
 export const standardPlanStorageMb = 1000
 export const proPlanStorageMb = 10000
 
-export const revisionHistoryStandardDays = 7
+export const revisionHistoryStandardDays = 3
 export const newTeamDiscountDays = 7
 
 export const freePlanMembersLimit = 1
