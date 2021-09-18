f06c3206091acb3170ce4b9e81a88ff35248d246
davy-c <d.castel33@gmail.com>
2021-09-18 10:47:39 +0900

    sidenav collapse

---
+++ src/cloud/components/SidebarToggleButton.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { usePreferences } from '../lib/stores/preferences'
+import Button from '../../design/components/atoms/Button'
+import { mdiArrowLeft, mdiArrowRight } from '@mdi/js'
+import { AppComponent } from '../../design/lib/types'
+import cc from 'classcat'
+import styled from '../../design/lib/styled'
+import { rightSideTopBarHeight } from '../../design/components/organisms/Topbar'
+
+const SidebarToggleButton: AppComponent<{ id?: string }> = ({
+  className,
+  id,
+}) => {
+  const { preferences, setPreferences } = usePreferences()
+
+  return (
+    <Container>
+      <Button
+        variant='secondary'
+        className={cc(['sidebar--toggle', className])}
+        iconPath={preferences.sidebarIsHidden ? mdiArrowRight : mdiArrowLeft}
+        iconSize={20}
+        id={id}
+        size='sm'
+        onClick={() =>
+          setPreferences((prev) => {
+            return { sidebarIsHidden: !prev.sidebarIsHidden }
+          })
+        }
+      />
+    </Container>
+  )
+}
+
+export default SidebarToggleButton
+
+const Container = styled.div`
+  position: fixed;
+  top: calc(${rightSideTopBarHeight}px + 30px);
+  left: -14px;
+  opacity: 0.5;
+  z-index: 1;
+
+  &:hover {
+    transition: all ease-in-out 0.3s;
+    left: 10px;
+    opacity: 1;
+  }
+`
