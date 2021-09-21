32207a19ef780f0a2fcc2f7625e05ecc69c5ec4a
Erick Sanhueza <ericko.sanhueza@gmail.com>
2021-09-21 17:51:58 -0300

    replace icon and change color of the due date selector to outdated docs

--- src/cloud/components/DocProperties/DocDueDateSelect.tsx
+++ src/cloud/components/DocProperties/DocDueDateSelect.tsx
@@ -4,7 +4,7 @@ import DocPropertyValueButton from './DocPropertyValueButton'
 import { format as formatDate } from 'date-fns'
 import styled from '../../../design/lib/styled'
 import Button from '../../../design/components/atoms/Button'
-import { mdiCalendarMonthOutline, mdiClose } from '@mdi/js'
+import { mdiCalendarMonthOutline, mdiCalendarRemoveOutline, mdiClose } from '@mdi/js'
 import { useI18n } from '../../lib/hooks/useI18n'
 import { lngKeys } from '../../lib/i18n/types'
 
@@ -46,10 +46,15 @@ const DocDueDateSelect = ({
         popperPlacement='top-end'
         customInput={
           <DocPropertyValueButton
+            className={ !(dueDate == null || dueDate >= new Date()) 
+              ? 'due__date__expired' 
+              : ''}
             sending={sending}
             empty={dueDate == null}
             isReadOnly={isReadOnly}
-            iconPath={mdiCalendarMonthOutline}
+            iconPath={dueDate == null || dueDate >= new Date() 
+              ? mdiCalendarMonthOutline 
+              : mdiCalendarRemoveOutline}
           >
             {dueDate != null
               ? formatDate(dueDate, 'MMM dd, yyyy')
@@ -77,6 +82,25 @@ const Container = styled.div`
   width: 100%;
   color: ${({ theme }) => theme.colors.text.primary};
   position: relative;
+
+  .due__date__expired {
+    .doc__property__button__icon,
+    .doc__property__button__label {
+      color: #bd2929;
+    }
+  }
+
+  .due__date__expired {
+    &:hover,
+    &:active,
+    &:focus,
+    &.button__state--active {
+      .doc__property__button__icon,
+      .doc__property__button__label {
+        color: #de1e1e;
+      }
+    }
+  }
 
   .due__date__clear {
     display: none;
