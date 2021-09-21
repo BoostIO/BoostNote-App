384c11b5a3eda58aac3b3b8d2cb12f8257fbf738
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2021-09-21 21:21:46 +0200

    Create it_IT.ts

---
+++ src/locales/it_IT.ts
@@ -0,0 +1,191 @@
+export default {
+  translation: {
+    //General
+    'general.general': 'Generale',
+    'general.error': 'Errore',
+    'general.cancel': 'Cancella',
+    'general.attachments': 'Allegati',
+    'general.archive': 'Archivio',
+    'general.allNotes': 'Tutte le note',
+    'general.signin': 'Accedi',
+    'general.signOut': 'Disconnetti',
+    'general.save': 'Salva',
+    'general.default': 'Predefenito',
+    'general.networkError': 'Errore di rete',
+
+    // Navigator
+    'navigator.noStorage': 'Non ci sono spazi',
+    'navigator.createStorage': 'Clicca qui per crearne uno.',
+
+    // Newsletter
+    'newsletter.subscribe': 'Iscriviti',
+    'newsletter.heading': 'Iscriviti per le Note di aggiornamento',
+    'newsletter.subscribed': 'Grazie per l'iscrizione!',
+    // Storage
+    'storage.storage': 'Spazio',
+    'storage.name': 'Nome spazio',
+    'storage.noStorage': 'Nessun spazio',
+    'storage.create': 'Crea Spazio',
+    'storage.edit': 'Modifica Spazio',
+    'storage.rename': 'Rinomina Spazio',
+    'storage.renameMessage': 'Inserisci il nuovo nome dello spazio',
+    'storage.remove': 'Remuovi Spazio',
+    'storage.removeMessage':
+      'Attenzione! Questo rimuoverà definitivamente tutte le note archiviate localmente in questa posizione.',
+    'storage.delete': 'Cancella {{storage}} Spazio',
+    'storage.move': 'Sposta Nota',
+    'storage.moveTitle': 'Sposta la nota in un altro spazio',
+    'storage.moveMessage': 'Stai cercando di spostare una nota in uno spazio diverso.',
+    'storage.copy': 'Copia Nota',
+    'storage.type': 'Tipo di Spazio',
+    'storage.typeLocal': 'Locale',
+    'storage.typeCloud': 'Cloud',
+    'storage.needSignIn': 'Devi accedere per creare uno spazio cloud.',
+    'storage.syncDate': 'Ultima sincronizzazione alle',
+
+    //Folder
+    'folder.folder': 'Cartella',
+    'folder.create': 'Nuova Cartella',
+    'folder.rename': 'Rinomina Cartella',
+    'folder.renameMessage':
+      'Inserisci il nuovo nome della cartella, verranno aggiornati anche i percorsi di ogni nota e sottocartella.',
+    'folder.renameErrorMessage': 'Impossibile rinominare la cartella',
+    'folder.remove': 'Rimuovi Cartella',
+    'folder.removeMessage': 'Tutte le note e le sottocartelle verranno eliminate.',
+    'folder.select': 'Seleziona Cartella',
+    'folder.noLocationSelected': 'Nessuna posizione selezionata',
+
+    //Tag
+    'tag.tags': 'Etichette',
+    'tag.add': 'Aggiungi Etichetta',
+    'tag.remove': 'Rimuovi Etichetta',
+    'tag.rename': 'Rimuovi Etichetta',
+    'tag.removeX': 'Rimuovi #{{tag}}',
+    'tag.removeMessage': 'L'etichetta verrà rimossa da tutte le note.',
+    'tag.renameMessage':
+      'Inserisci il nuovo nome dell'etichetta. Note con l'etichetta "{{tagName}}" sarà aggiornato per riflettere il cambiamento.',
+
+    //Note
+    'note.duplicate': 'Duplicato',
+    'note.delete': 'Cancella',
+    'note.delete2': 'Cancella una Nota',
+    'note.deleteMessage': 'La nota verrà eliminata definitivamente',
+    'note.empty': 'Nota vuota',
+    'note.unselect': 'Nessuna nota selezionata',
+    'note.search': 'Cerca Note',
+    'note.nothing': 'Nessuna Nota',
+    'note.nothingMessage': 'Non è stato possibile trovare note.',
+    'note.noTitle': 'Nessun titolo',
+    'note.date': 'fa',
+    'note.createKeyOr': 'o',
+    'note.createKey': 'N',
+    'note.createKeyMac': 'su Mac',
+    'note.createKeyWinLin': 'su Windows/Linux',
+    'note.createkeymessage1': 'Per creare un nuovo documento, premere',
+    'note.createkeymessage2': 'Seleziona uno spazio',
+    'note.createkeymessage3': 'per creare un nuovo documento',
+    'note.restore': 'Ripristina',
+    'note.edit': 'Modifica',
+    'note.splitView': 'Vista divisa',
+    'note.preview': 'Anteprima',
+    'note.archive': 'Archivio',
+    'note.export': 'Esporta',
+
+    //Bookmark
+    'bookmark.remove': 'Rimuovi segnalibri',
+    'bookmark.add': 'Segnalibri',
+
+    //About
+    'about.about': 'Informazioni',
+    'about.boostnoteDescription':
+      'App per prendere appunti creata per i programmatori proprio come te.',
+    'about.website': 'Sito ufficiale',
+    'about.boostWiki': 'Boost Note',
+    'about.platform': 'Multipiattaforma',
+    'about.community': 'Community',
+    'about.github': 'GitHub Repository',
+    'about.bounty': 'Bounty su IssueHunt',
+    'about.blog': 'Blog',
+    'about.slack': 'Gruppo Slack',
+    'about.twitter': 'Account Twitter',
+    'about.facebook': 'Gruppo Facebook',
+    'about.reddit': 'Reddit',
+
+    //Billing
+    'billing.billing': 'Fatturazione',
+    'billing.message': 'Accedi per aggiornare il tuo piano.',
+    'billing.basic': 'Basic',
+    'billing.current': 'Attuale',
+    'billing.premium': 'Premium',
+    'billing.price': '$3/Mese (USD) *',
+    'billing.browser': 'Browser App',
+    'billing.desktop': 'Desktop App (Mac/Windows/Linux)',
+    'billing.mobile': 'Mobile App (iOS/Android)',
+    'billing.sync': 'Sincronizzazione di più dispositivi',
+    'billing.local': 'Archiviazione Locale',
+    'billing.cloud': 'Cloud Locale',
+    'billing.storageSize': 'Dimensione dell'Archiviazione Cloud',
+    'billing.addStorageDescription':
+      '* Se hai bisogno di più spazio di archiviazione cloud, puoi aggiungerlo in qualsiasi momento pagando $ 5 (USD) per ogni 5 GB. Fai clic sul pulsante "Aggiungi spazio di archiviazione aggiuntivo" di seguito.',
+    'billing.addStorage': 'Aggiungi spazio di archiviazione aggiuntivo',
+
+    'editor.editor': 'Editor',
+
+    // Preferences
+    'preferences.general': 'Preferenze',
+    'preferences.generalTab': 'Generale',
+
+    // Preferences GeneralTab
+    'preferences.account': 'Account',
+    'preferences.addAccount': 'Accedi',
+    'preferences.loginWorking': 'Loggin in...',
+    'preferences.interfaceLanguage': 'Lingua dell'interfaccia',
+    'preferences.applicationTheme': 'Tema dell'applicazione',
+    'preferences.auto': 'Auto',
+    'preferences.light': 'Chiaro',
+    'preferences.dark': 'Scuro',
+    'preferences.sepia': 'Seppia',
+    'preferences.solarizedDark': 'Scuro Solarizzato',
+    'preferences.noteSorting': 'Ordinamento delle note',
+    'preferences.dateUpdated': 'Data aggiornamento',
+    'preferences.dateCreated': 'Data di creazione',
+    'preferences.title': 'Titolo',
+    'preferences.analytics': 'Analisi',
+    'preferences.analyticsDescription1':
+      'Boost Note raccoglie dati anonimi al solo scopo di migliorare l'applicazione e rigorosamente non raccoglie alcuna informazione personale come il contenuto delle tue note. Puoi vedere come funziona su GitHub.',
+    'preferences.analyticsDescription2':
+      'Puoi scegliere di abilitare o disabilitare questa opzione.',
+    'preferences.analyticsLabel': 'Abilita l'analisi per aiutare a migliorare Boostnote',
+    'preferences.displayTutorialsLabel': 'Tutorial e FAQ',
+    'preferences.autoSync': 'Abilita sincronizzazione automatica',
+
+    // Preferences KeymapTab
+    'preferences.keymap': 'Mappa dei tasti',
+
+    // Preferences EditorTab
+    'preferences.editorTheme': 'Tema dell'Editor',
+    'preferences.editorFontSize': 'Dimensione Carattere dell'Editor',
+    'preferences.editorFontFamily': 'Famiglia di Carattere dell'Editor',
+    'preferences.editorIndentType': 'Tipo di Rientro dell'Editor',
+    'preferences.tab': 'Tab',
+    'preferences.spaces': 'Spazi',
+    'preferences.editorIndentSize': 'Dimensione Rientro Editor',
+    'preferences.editorKeymap': 'Mappa dei Tasti dell'Editor',
+    'preferences.editorPreview': 'Anteprima dell'Editor',
+
+    // Preferences MarkdownTab
+    'preferences.markdownTab': 'Impostazioni Markdown',
+    'preferences.previewStyle': 'Anteprima Stile',
+    'preferences.markdownCodeBlockTheme': 'Tema Blocco Codice',
+    'preferences.defaultTheme': 'Usa lo stile predefinito',
+    'preferences.markdownPreview': 'Anteprima Markdown',
+
+    // Preferences ExportTab
+    'preferences.exportTab': 'Impostazioni di esportazione',
+    'preferences.printBackground': 'Sfondo di stampa',
+    'preferences.landscape': 'Paesaggio',
+    'preferences.marginType': 'Margini',
+    'preferences.pageSize': 'Dimensioni della pagina',
+    'preferences.includeFrontMatter': 'Includi argomento iniziale',
+  },
+}
