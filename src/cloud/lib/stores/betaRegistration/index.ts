55a1090ae87273a7b613cb388efb1539b14c54bf
davy-c <d.castel33@gmail.com>
2021-10-20 09:21:01 +0900

    remove block editor

--- src/cloud/lib/stores/betaRegistration/index.ts
+++
@@ -1,9 +0,0 @@
-import { createStoreContext } from '../../utils/context'
-import { useBetaRegistrationStore } from './store'
-
-export const {
-  StoreProvider: BetaRegistrationProvider,
-  useStore: useBetaRegistration,
-} = createStoreContext(useBetaRegistrationStore, 'Beta Registration')
-
-export { default as withBetaRegistration } from './withBetaRegistration'
