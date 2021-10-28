9480d798453f39b7460cd59ee6714daf26ef448a
Simon Leigh <sleigh.dev@gmail.com>
2021-10-28 09:43:04 +0900

    update dashboard types

--- src/cloud/components/Modal/contents/Dashboard/interfaces.ts
+++ src/cloud/components/Modal/contents/Dashboard/interfaces.ts
@@ -1,73 +1,13 @@
-import {
-  StatusCondition,
-  LabelsCondition,
-  AssigneesCondition,
-  TodayDateConditionValue,
-  InWeekDateConditionValue,
-  InMonthDateConditionValue,
-} from '../../../../interfaces/db/dashboard'
+import { Condition, ConditionType } from '../../../../interfaces/db/dashboard'
 
-export type EditibleBetweenDateConditionValue = {
-  type: 'between'
-  from: Date | null
-  to: Date | null
-}
+export type EditableQuery = EditableCondition[]
 
-export type EditibleAfterDateConditionValue = {
-  type: 'after'
-  date: Date | null
-}
+export type EditableCondition =
+  | ConditionType<'query', EditableQuery>
+  | Exclude<Condition, { type: 'query' }>
+  | { type: 'null'; rule: 'and' | 'or' }
 
-export type EditibleBeforeDateConditionValue = {
-  type: 'before'
-  date: Date | null
-}
-
-export type EditibleSpecificDateConditionValue = {
-  type: 'specific'
-  date: Date | null
-}
-
-export type EditibleDateConditionValue =
-  | TodayDateConditionValue
-  | InWeekDateConditionValue
-  | InMonthDateConditionValue
-  | EditibleSpecificDateConditionValue
-  | EditibleBeforeDateConditionValue
-  | EditibleAfterDateConditionValue
-  | EditibleBetweenDateConditionValue
-
-export type EditibleDueDateCondition = {
-  type: 'due_date'
-  value: EditibleDateConditionValue | null
-}
-export type EditibleCreationDateCondition = {
-  type: 'creation_date'
-  value: EditibleDateConditionValue | null
-}
-export type EditibleUpdateDateCondition = {
-  type: 'update_date'
-  value: EditibleDateConditionValue | null
-}
-
-export type NullSecondaryCondition = {
-  type: 'null'
-}
-
-export type EditibleSecondaryCondition =
-  | NullSecondaryCondition
-  | StatusCondition
-  | LabelsCondition
-  | EditibleDueDateCondition
-  | EditibleCreationDateCondition
-  | EditibleUpdateDateCondition
-  | AssigneesCondition
-
-export type EditibleSecondaryConditionType =
-  | 'null'
-  | 'status'
-  | 'labels'
-  | 'due_date'
-  | 'assignees'
-  | 'creation_date'
-  | 'update_date'
+export type Kind<T extends { type: string }, K extends T['type']> = Extract<
+  T,
+  { type: K }
+>
