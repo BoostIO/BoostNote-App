51315316ecd9445a0d606b477a6277e7d49c708e
davy-c <d.castel33@gmail.com>
2021-10-29 09:53:07 +0900

    table view component

---
+++ src/cloud/components/Views/index.tsx
@@ -0,0 +1,71 @@
+import React, { useMemo, useState } from 'react'
+import { useEffectOnce } from 'react-use'
+import Flexbox from '../../../design/components/atoms/Flexbox'
+import styled from '../../../design/lib/styled'
+import { SerializedView } from '../../interfaces/db/view'
+import { useCloudApi } from '../../lib/hooks/useCloudApi'
+import TableView from './Table/TableView'
+import ViewsSelector from './ViewsSelector'
+
+interface ViewsListProps {
+  views: SerializedView[]
+  parent: { folder: string } | { dashboard: string }
+}
+
+const ViewsList = ({ views, parent }: ViewsListProps) => {
+  const [selectedViewId, setSelectedViewId] = useState<number>()
+  const { createViewApi } = useCloudApi()
+
+  const currentView = useMemo(() => {
+    if (selectedViewId == null) {
+      return undefined
+    }
+
+    return views.find((view) => view.id === selectedViewId)
+  }, [selectedViewId, views])
+
+  useEffectOnce(() => {
+    if (views.length > 0) {
+      setSelectedViewId(views[0].id)
+    }
+  })
+
+  return (
+    <Container className='views__list'>
+      {currentView == null ? (
+        <Flexbox justifyContent='space-between' className='views__header'>
+          <ViewsSelector
+            selectedViewId={selectedViewId}
+            setSelectedViewId={setSelectedViewId}
+            createViewApi={createViewApi}
+            parent={parent}
+            views={views}
+          />
+        </Flexbox>
+      ) : currentView.type === 'table' ? (
+        <TableView
+          selectedViewId={selectedViewId}
+          setSelectedViewId={setSelectedViewId}
+          createViewApi={createViewApi}
+          parent={parent}
+          views={views}
+          view={currentView}
+        />
+      ) : null}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  width: 100%;
+
+  .views__header {
+    width: 100%;
+  }
+`
+
+export default ViewsList
