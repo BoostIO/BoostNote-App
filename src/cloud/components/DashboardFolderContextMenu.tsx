8ad470b902cc75d0de57d07cc51346e2ee0e36bb
davy-c <d.castel33@gmail.com>
2021-11-08 07:27:51 +0900

    remove filter rows from dashboard context

--- src/cloud/components/DashboardFolderContextMenu.tsx
+++ src/cloud/components/DashboardFolderContextMenu.tsx
@@ -2,13 +2,11 @@
 import {
   mdiAccountCircleOutline,
   mdiAccountMultiple,
-  mdiCalendarMonthOutline,
   mdiClockOutline,
   mdiContentCopy,
   mdiContentSaveOutline,
   mdiOpenInNew,
   mdiPencil,
-  mdiTag,
   mdiTrashCanOutline,
 } from '@mdi/js'
 import React, { useCallback, useMemo, useState } from 'react'
@@ -32,15 +30,7 @@ import UpdateDashboardModal from './Modal/contents/Dashboard/UpdateDashboardModa
 import { useModal } from '../../design/lib/stores/modal'
 import { useCloudApi } from '../lib/hooks/useCloudApi'
 import { useDialog } from '../../design/lib/stores/dialog'
-import { format, formatDuration } from 'date-fns'
-import { capitalize } from 'lodash'
-import DocAssigneeSelect from './Props/Pickers/AssigneeSelect'
-import styled from '../../design/lib/styled'
-import DocTagsListItem from './DocTagsListItem'
-import { useNav } from '../lib/stores/nav'
 import { getTeamLinkHref } from './Link/TeamLink'
-import { SerializedTag } from '../interfaces/db/tag'
-import { isUUIDArray } from '../lib/utils/array'
 
 interface DashboardContextMenuProps {
   dashboard: SerializedDashboard
@@ -57,7 +47,6 @@ const DashboardContextMenu = ({
   const { openModal } = useModal()
   const { sendingMap, deleteDashboardApi } = useCloudApi()
   const { messageBox } = useDialog()
-  const { tagsMap } = useNav()
 
   const docUrl = useMemo(() => {
     return (
@@ -87,133 +76,7 @@ const DashboardContextMenu = ({
     dashboard.userId != null ? usersMap.get(dashboard.userId) : undefined
 
   return (
-    <MetadataContainer rows={[{ type: 'header', content: 'Filter options' }]}>
-      {Array.isArray(dashboard.condition) &&
-        dashboard.condition.map((condition, i) => {
-          switch (condition.type) {
-            case 'creation_date':
-            case 'update_date':
-            case 'due_date':
-              let label = ''
-              switch (condition.value.type) {
-                case 'relative': {
-                  if (condition.value.period === 0) {
-                    label = 'Today'
-                  } else {
-                    label = `Last ${formatDuration({
-                      seconds: condition.value.period,
-                    })}`
-                  }
-                  break
-                }
-                case 'specific':
-                  label = format(
-                    new Date(condition.value.date),
-                    'MMMM dd, yyyy, HH:mm'
-                  )
-                  break
-                case 'after':
-                case 'before':
-                  label = `${capitalize(condition.value.type)} ${format(
-                    new Date(condition.value.date),
-                    'MMMM dd, yyyy, HH:mm'
-                  )}`
-                  break
-                case 'between':
-                  label = `Between ${format(
-                    new Date(condition.value.from),
-                    'MMMM dd'
-                  )} and ${format(new Date(condition.value.to), 'MMMM dd')}`
-                  break
-                default:
-                  break
-              }
-
-              return (
-                <MetadataContainerRow
-                  key={`condition-${i}`}
-                  row={{
-                    label:
-                      condition.type === 'due_date'
-                        ? translate(lngKeys.DueDate)
-                        : condition.type === 'creation_date'
-                        ? translate(lngKeys.CreationDate)
-                        : translate(lngKeys.UpdateDate),
-                    type: 'content',
-                    icon:
-                      condition.type === 'due_date'
-                        ? mdiCalendarMonthOutline
-                        : mdiClockOutline,
-                    content: label,
-                  }}
-                />
-              )
-            case 'prop':
-              return isUUIDArray(condition.value.value) ? (
-                <MetadataContainerRow
-                  key={`condition-${i}`}
-                  row={{
-                    label: capitalize(condition.value.name) || 'Untitled',
-                    type: 'content',
-                    icon: mdiAccountCircleOutline,
-                    content: (
-                      <DocAssigneeSelect
-                        isLoading={false}
-                        readOnly={true}
-                        disabled={true}
-                        defaultValue={condition.value.value}
-                        update={() => {}}
-                      />
-                    ),
-                  }}
-                />
-              ) : (
-                <MetadataContainerRow
-                  key={`condition-${i}`}
-                  row={{
-                    label: `Prop: ${condition.value.name}`,
-                    type: 'content',
-                    icon: mdiAccountCircleOutline,
-                    content: condition.value.value,
-                  }}
-                />
-              )
-            case 'label':
-              const tags = condition.value.reduce((acc, val) => {
-                const tag = tagsMap.get(val)
-                if (tag != null) {
-                  acc.push(tag)
-                }
-                return acc
-              }, [] as SerializedTag[])
-              return (
-                tags.length > 0 && (
-                  <MetadataContainerRow
-                    key={`condition-${i}`}
-                    row={{
-                      label: translate(lngKeys.GeneralLabels),
-                      type: 'content',
-                      icon: mdiTag,
-                      content: (
-                        <TagList>
-                          {tags.map((tag) => (
-                            <DocTagsListItem
-                              team={team}
-                              tag={tag}
-                              key={tag.id}
-                            />
-                          ))}
-                        </TagList>
-                      ),
-                    }}
-                  />
-                )
-              )
-            default:
-              return null
-          }
-        })}
-      <MetadataContainerBreak />
+    <MetadataContainer>
       <MetadataContainerRow
         row={{
           content: 'Info',
@@ -357,16 +220,5 @@ const DashboardContextMenu = ({
     </MetadataContainer>
   )
 }
-
-const TagList = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  flex-wrap: wrap;
-
-  .doc__tags__list__item {
-    margin-bottom: ${({ theme }) => theme.sizes.spaces.xsm}px;
-  }
-`
 
 export default React.memo(DashboardContextMenu)
