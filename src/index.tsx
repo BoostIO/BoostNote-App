639d9ac66f0112cc34f63ee5239a6d8781e32bca
Junyoung Choi <fluke8259@gmail.com>
2021-11-10 20:04:19 +0900

    Render webview only for electron frontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import { V2ToastProvider } from './design/lib/stores/toast'
-import { V2WindowProvider } from './design/lib/stores/window'
-import { V2DialogProvider } from './design/lib/stores/dialog'
-import { V2ModalProvider } from './design/lib/stores/modal'
-import { V2ContextMenuProvider } from './design/lib/stores/contextMenu'
-import { DbProvider } from './lib/db'
-import { RouterProvider } from './lib/router'
-import { combineProviders } from './lib/context'
-import { PreferencesProvider } from './lib/preferences'
-import { GeneralStatusProvider } from './lib/generalStatus'
-import { StorageRouterProvider } from './lib/storageRouter'
-import { BoostHubStoreProvider } from './lib/boosthub'
-import { CloudIntroModalProvider } from './lib/cloudIntroModal'
-import { MigrationProvider } from './lib/migrate/store'
-import { DialogProvider } from './lib/dialog'
-
-const V2CombinedProvider = combineProviders(
-  V2ToastProvider,
-  V2WindowProvider,
-  V2ContextMenuProvider,
-  V2ModalProvider,
-  V2DialogProvider
-)
-
-const CombinedProvider = combineProviders(
-  V2CombinedProvider,
-  BoostHubStoreProvider,
-  MigrationProvider,
-  GeneralStatusProvider,
-  DialogProvider,
-  DbProvider,
-  PreferencesProvider,
-  StorageRouterProvider,
-  RouterProvider,
-  CloudIntroModalProvider
-)
 
 function render(Component: typeof App) {
   let rootDiv = document.getElementById('root')
@@ -45,12 +9,7 @@ function render(Component: typeof App) {
     rootDiv.setAttribute('id', 'root')
     document.body.appendChild(rootDiv)
   }
-  ReactDOM.render(
-    <CombinedProvider>
-      <Component />
-    </CombinedProvider>,
-    document.getElementById('root')
-  )
+  ReactDOM.render(<Component />, document.getElementById('root'))
 }
 
 if (module.hot != null) {
