2fbba884ef1a8174ce0d9b4f9137e1e6e89e9d63
davy-c <d.castel33@gmail.com>
2021-11-19 07:29:13 +0900

    move label auto complete selector in context modal

--- src/cloud/components/DocPage/DocTagsList/TagsAutoCompleteInput.tsx
+++ src/cloud/components/DocPage/DocTagsList/TagsAutoCompleteInput.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useCallback, useEffect, useMemo, useRef } from 'react'
+import React, { useState, useCallback, useMemo, useRef } from 'react'
 import { mdiLabelOutline, mdiPlus } from '@mdi/js'
 import { useNav } from '../../../lib/stores/nav'
 import { createTag } from '../../../api/teams/tags'
 import { SerializedTeam } from '../../../interfaces/db/team'
 import { SerializedDocWithSupplemental } from '../../../interfaces/db/doc'
 import { useUpDownNavigationListener } from '../../../lib/keyboard'
-import { isChildNode } from '../../../lib/dom'
 import { useToast } from '../../../../design/lib/stores/toast'
 import styled from '../../../../design/lib/styled'
 import FormInput from '../../../../design/components/molecules/Form/atoms/FormInput'
@@ -13,73 +12,84 @@ import cc from 'classcat'
 import { lngKeys } from '../../../lib/i18n/types'
 import { useI18n } from '../../../lib/hooks/useI18n'
 import Spinner from '../../../../design/components/atoms/Spinner'
-import Flexbox from '../../../../design/components/atoms/Flexbox'
 import DocPropertyValueButton from '../../Props/Pickers/PropertyValueButton'
 import Icon from '../../../../design/components/atoms/Icon'
+import { useModal } from '../../../../design/lib/stores/modal'
+import { useEffectOnce } from 'react-use'
+import Flexbox from '../../../../design/components/atoms/Flexbox'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface TagsAutoCompleteInputProps {
   doc: SerializedDocWithSupplemental
   team: SerializedTeam
 }
 
 const TagsAutoCompleteInput = ({ team, doc }: TagsAutoCompleteInputProps) => {
-  const [showInput, setShowInput] = useState<boolean>(false)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const autocompleteRef = useRef<HTMLDivElement>(null)
-  const containerRef = useRef<HTMLDivElement>(null)
-  const { tagsMap, updateDocsMap, updateTagsMap } = useNav()
-  const [sending, setSending] = useState<boolean>(false)
-  const { pushApiErrorMessage } = useToast()
-  const [tagText, setTagText] = useState<string>('')
   const { translate } = useI18n()
-
-  useEffect(() => {
-    if (showInput && inputRef.current != null) {
-      inputRef.current.focus()
-    }
-  }, [showInput])
-
-  const createTagHandler = useCallback(
-    async (tagText: string) => {
-      if (sending || tagText.trim() === '') {
-        return
-      }
-
-      setSending(true)
-      try {
-        const { doc: newDoc, tag } = await createTag(team, {
-          docId: doc.id,
-          text: tagText,
-        })
-        updateTagsMap([tag.id, tag])
-        updateDocsMap([newDoc.id, newDoc])
-        setShowInput(false)
-        setTagText('')
-      } catch (error) {
-        pushApiErrorMessage(error)
-      }
-
-      setSending(false)
+  const { openContextModal } = useModal()
+  const activateAndFocus = useCallback(
+    (ev) => {
+      ev.stopPropagation()
+      ev.preventDefault()
+      openContextModal(ev, <TagsSelectorModal team={team} doc={doc} />, {
+        removePadding: true,
+        width: 200,
+        keepAll: true,
+      })
     },
-    [pushApiErrorMessage, sending, doc.id, team, updateDocsMap, updateTagsMap]
+    [team, doc, openContextModal]
   )
 
-  const activateAndFocus = useCallback(() => {
-    setShowInput(true)
-  }, [])
+  return (
+    <Container
+      className={cc([
+        'doc__tags__create',
+        (doc.tags || []).length === 0 && 'doc__tags__create--empty',
+      ])}
+    >
+      {(doc.tags || []).length === 0 ? (
+        <DocPropertyValueButton
+          iconPath={mdiLabelOutline}
+          id='tag__add__btn'
+          empty={true}
+          onClick={activateAndFocus}
+          isReadOnly={false}
+        >
+          {translate(lngKeys.AddALabel)}
+        </DocPropertyValueButton>
+      ) : (
+        <button
+          className='tag__add'
+          id='tag__add__btn'
+          onClick={activateAndFocus}
+        >
+          <Icon path={mdiPlus} size={16} />
+        </button>
+      )}
+    </Container>
+  )
+}
 
-  const onBlurHandler = (event: any) => {
-    if (
-      containerRef.current !== event.relatedTarget &&
-      !isChildNode(
-        containerRef.current,
-        event.relatedTarget as HTMLElement | null
-      )
-    ) {
-      setShowInput(false)
+interface TagsSelectorModalProps {
+  doc: SerializedDocWithSupplemental
+  team: SerializedTeam
+}
+
+const TagsSelectorModal = ({ team, doc }: TagsSelectorModalProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const autocompleteRef = useRef<HTMLDivElement>(null)
+  const { tagsMap, updateDocsMap, updateTagsMap } = useNav()
+  const { pushApiErrorMessage } = useToast()
+  const [sending, setSending] = useState<boolean>(false)
+  const [tagText, setTagText] = useState<string>('')
+  const { translate } = useI18n()
+  const { closeLastModal } = useModal()
+
+  useEffectOnce(() => {
+    if (inputRef.current != null) {
+      inputRef.current.focus()
     }
-  }
+  })
 
   const tagsIdsAlreadyInDoc = useMemo(() => {
     if (doc.tags == null || doc.tags.length === 0) {
@@ -135,6 +145,39 @@ const TagsAutoCompleteInput = ({ team, doc }: TagsAutoCompleteInputProps) => {
     return options
   }, [tagsMap, tagsIdsAlreadyInDoc, tagText, translate])
 
+  const createTagHandler = useCallback(
+    async (tagText: string) => {
+      if (sending || tagText.trim() === '') {
+        return
+      }
+
+      setSending(true)
+      try {
+        const { doc: newDoc, tag } = await createTag(team, {
+          docId: doc.id,
+          text: tagText,
+        })
+        updateTagsMap([tag.id, tag])
+        updateDocsMap([newDoc.id, newDoc])
+        setTagText('')
+        closeLastModal()
+      } catch (error) {
+        pushApiErrorMessage(error)
+      }
+
+      setSending(false)
+    },
+    [
+      pushApiErrorMessage,
+      sending,
+      doc.id,
+      team,
+      updateDocsMap,
+      updateTagsMap,
+      closeLastModal,
+    ]
+  )
+
   const selectOptionHandler = useCallback(
     async (event: any, option: string) => {
       event.preventDefault()
@@ -157,112 +200,48 @@ const TagsAutoCompleteInput = ({ team, doc }: TagsAutoCompleteInputProps) => {
   })
 
   return (
-    <Container
-      className={cc([
-        'doc__tags__create',
-        (doc.tags || []).length === 0 && 'doc__tags__create--empty',
-      ])}
-    >
-      {!showInput ? (
-        (doc.tags || []).length === 0 ? (
-          <DocPropertyValueButton
-            iconPath={mdiLabelOutline}
-            id='tag__add__btn'
-            empty={true}
-            onClick={activateAndFocus}
-            isReadOnly={false}
-          >
-            {translate(lngKeys.AddALabel)}
-          </DocPropertyValueButton>
-        ) : (
-          <button
-            className='tag__add'
-            id='tag__add__btn'
-            onClick={activateAndFocus}
-          >
-            <Icon path={mdiPlus} size={16} />
-          </button>
-        )
-      ) : (
-        <Flexbox alignItems='center'>
-          <div
-            className='tag__add__input__container'
-            onBlur={onBlurHandler}
-            ref={containerRef}
-          >
-            <FormInput
-              id='autocomplete-tags'
-              ref={inputRef}
-              className='autocomplete__input'
-              placeholder='Add New Label...'
-              value={tagText}
-              onChange={inputOnChangeEvent}
-              disabled={sending}
-              autoComplete='off'
-            />
-            {autoCompleteOptions.length > 0 && (
-              <div className='autocomplete__container' ref={autocompleteRef}>
-                {autoCompleteOptions.map((option, i) => (
-                  <a
-                    className='autocomplete__option'
-                    key={`option-autocomplete=${i}`}
-                    id={`option--autocomplete=${i}`}
-                    href='#'
-                    onClick={(e: any) => selectOptionHandler(e, option.value)}
-                  >
-                    {option.label}
-                  </a>
-                ))}
-              </div>
-            )}
-          </div>
-          {sending && <Spinner className='tag__add__input__spinner' />}
-        </Flexbox>
+    <ModalContainer ref={containerRef} className='autocomplete__container'>
+      <Flexbox>
+        <FormInput
+          id='autocomplete-tags'
+          ref={inputRef}
+          className='autocomplete__input'
+          placeholder='Add New Label...'
+          value={tagText}
+          onChange={inputOnChangeEvent}
+          disabled={sending}
+          autoComplete='off'
+        />
+        {sending && <Spinner className='tag__add__input__spinner' />}
+      </Flexbox>
+      {!sending && autoCompleteOptions.length > 0 && (
+        <div className='autocomplete__container' ref={autocompleteRef}>
+          {autoCompleteOptions.map((option, i) => (
+            <a
+              className='autocomplete__option'
+              key={`option-autocomplete=${i}`}
+              id={`option--autocomplete=${i}`}
+              href='#'
+              onClick={(e: any) => selectOptionHandler(e, option.value)}
+            >
+              {option.label}
+            </a>
+          ))}
+        </div>
       )}
-    </Container>
+    </ModalContainer>
   )
 }
 
-const Container = styled.div`
-  &.doc__tags__create--empty {
-    width: 100%;
-    margin-bottom: 0 !important;
-  }
-
-  .tag__add {
-    font-size: ${({ theme }) => theme.sizes.fonts.df}px;
-    border-radius: 100%;
-    width: 25px;
-    height: 25px;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    background: none;
-    border: 1px solid ${({ theme }) => theme.colors.border.main};
-    color: ${({ theme }) => theme.colors.text.subtle};
-    margin: 0 4px;
-    padding: 0;
-
-    &:hover,
-    &:focus {
-      color: ${({ theme }) => theme.colors.text.primary} !important;
-    }
-  }
-
-  .tag__add__input__container {
-    position: relative;
-    width: 100%;
-  }
-
+const ModalContainer = styled.div`
   .autocomplete__input {
+    flex: 1 1 auto;
     line-height: inherit !important;
     height: 28px !important;
     width: 100%;
   }
 
   .autocomplete__container {
-    z-index: 9000;
-    position: absolute;
     padding: ${({ theme }) => theme.sizes.spaces.xsm}px 0;
     width: 100%;
     height: auto;
@@ -301,10 +280,39 @@ const Container = styled.div`
   }
 
   .tag__add__input__spinner {
+    flex: 0 0 auto;
     border-color: ${({ theme }) => theme.colors.variants.primary.text};
     border-right-color: transparent;
     display: inline-flex;
     margin-left: ${({ theme }) => theme.sizes.spaces.xsm}px;
+    margin-right: ${({ theme }) => theme.sizes.spaces.xsm}px;
+  }
+`
+
+const Container = styled.div`
+  &.doc__tags__create--empty {
+    width: 100%;
+    margin-bottom: 0 !important;
+  }
+
+  .tag__add {
+    font-size: ${({ theme }) => theme.sizes.fonts.df}px;
+    border-radius: 100%;
+    width: 25px;
+    height: 25px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: none;
+    border: 1px solid ${({ theme }) => theme.colors.border.main};
+    color: ${({ theme }) => theme.colors.text.subtle};
+    margin: 0 4px;
+    padding: 0;
+
+    &:hover,
+    &:focus {
+      color: ${({ theme }) => theme.colors.text.primary} !important;
+    }
   }
 `
 
