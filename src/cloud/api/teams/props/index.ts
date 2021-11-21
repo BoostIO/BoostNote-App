09d6f900c490d85dc4f8ffa62f8d783622a27137
davy-c <d.castel33@gmail.com>
2021-11-22 08:56:53 +0900

    new property add context

--- src/cloud/api/teams/props/index.ts
+++ src/cloud/api/teams/props/index.ts
@@ -1,13 +1,24 @@
+import {
+  PropSubType,
+  PropType,
+  StaticPropType,
+} from '../../../interfaces/db/props'
 import { callApi } from '../../../lib/client'
 
 export type ListPropertySuggestionsRequestBody = {
   team: string
+  workspace?: string
+  folder?: string
   propertyType?: string
   jsonPropertyType?: string
 }
 
 export interface ListPropertySuggestionsResponseBody {
-  data: { name: string; type: string }[]
+  data: {
+    name: string
+    type: StaticPropType | PropType
+    subType?: PropSubType
+  }[]
 }
 
 export async function listPropertySuggestions(
