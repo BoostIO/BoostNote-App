9f4aed6b9c0e6973ab0afdc1bebefce9b363d000
davy-c <d.castel33@gmail.com>
2021-11-30 10:25:17 +0900

    fix padding for view selector

--- src/cloud/lib/views/index.ts
+++ src/cloud/lib/views/index.ts
@@ -1,3 +1,10 @@
+import {
+  mdiCalendarMonthOutline,
+  mdiTable,
+  mdiViewParallelOutline,
+} from '@mdi/js'
+import { SupportedViewTypes } from '../../interfaces/db/view'
+
 export type ViewMoveType =
   | 'before'
   | 'after'
@@ -5,3 +12,15 @@ export type ViewMoveType =
       target: number
       type: 'before' | 'after'
     }
+
+export function getIconPathOfViewType(type: SupportedViewTypes) {
+  switch (type) {
+    case 'calendar':
+      return mdiCalendarMonthOutline
+    case 'kanban':
+      return mdiViewParallelOutline
+    default:
+    case 'table':
+      return mdiTable
+  }
+}
