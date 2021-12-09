f6d88000cb4e2f64dc43f0d95e0bf21fb50e90fb
davy-c <d.castel33@gmail.com>
2021-12-09 12:02:55 +0900

    unify calendar and kanban props select (tmp)

--- src/cloud/components/Views/Kanban/KanbanWatchedPropSetter.tsx
+++ src/cloud/components/Views/Kanban/KanbanWatchedPropSetter.tsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
+import { mdiCheck } from '@mdi/js'
+import React, { useMemo, useRef, useState } from 'react'
 import { useEffectOnce } from 'react-use'
-import SuggestionSelect, {
-  Suggestion,
-} from '../../../../design/components/molecules/SuggestionSelect'
+import Flexbox from '../../../../design/components/atoms/Flexbox'
+import Icon from '../../../../design/components/atoms/Icon'
+import Spinner from '../../../../design/components/atoms/Spinner'
+import MetadataContainer from '../../../../design/components/organisms/MetadataContainer'
+import MetadataContainerRow from '../../../../design/components/organisms/MetadataContainer/molecules/MetadataContainerRow'
+import styled from '../../../../design/lib/styled'
+import { sortByAttributeAsc } from '../../../../design/lib/utils/array'
 import {
   ListPropertySuggestionsRequestBody,
   ListPropertySuggestionsResponseBody,
 } from '../../../api/teams/props'
+import { PropSubType, PropType } from '../../../interfaces/db/props'
 import { SerializedView } from '../../../interfaces/db/view'
 import { useCloudApi } from '../../../lib/hooks/useCloudApi'
-import { isPropType } from '../../../lib/props'
+import {
+  getDefaultColumnSuggestionsPerType,
+  getIconPathOfPropType,
+  isPropType,
+} from '../../../lib/props'
 import { filterIter } from '../../../lib/utils/iterator'
 
 interface KanbanWatchedPropSetterProps {
@@ -19,18 +29,22 @@ interface KanbanWatchedPropSetterProps {
   setProp: (prop: string) => void
 }
 
+interface PropertySuggestions {
+  name: string
+  type: PropType
+  subType?: PropSubType
+}
+
 const KanbanWatchedPropSetter = ({
   prop,
   setProp,
   view,
   teamId,
 }: KanbanWatchedPropSetterProps) => {
-  const { fetchPropertySuggestionsApi } = useCloudApi()
   const [fetching, setFetching] = useState(true)
-  const [value, setValue] = useState(prop)
-  const [suggestions, setSuggestions] = useState<
-    Record<string, Suggestion<string>[]>
-  >({})
+  const { fetchPropertySuggestionsApi } = useCloudApi()
+  const menuRef = useRef<HTMLDivElement>(null)
+  const [suggestions, setSuggestions] = useState<PropertySuggestions[]>([])
 
   useEffectOnce(() => {
     const fetchSuggestions = async () => {
@@ -52,30 +66,121 @@ const KanbanWatchedPropSetter = ({
 
       const res = await fetchPropertySuggestionsApi(body)
       if (!res.err) {
-        setSuggestions({
-          Suggestions: filterIter(
+        setSuggestions(
+          filterIter(
             (val) => isPropType(val.type),
             (res.data as ListPropertySuggestionsResponseBody).data
-          ).map((data) => ({ name: data.name, value: data.name })),
-        })
+          ) as any
+        )
       } else {
-        setSuggestions({})
+        setSuggestions([])
       }
       setFetching(false)
     }
     fetchSuggestions()
   })
 
+  const orderedSuggestions = useMemo(() => {
+    const fromChildren = sortByAttributeAsc('name', suggestions)
+
+    const childrenSuggestionNames = fromChildren.map(
+      (suggestion) => suggestion.name
+    )
+    const fromApp = filterIter(
+      (suggestion) =>
+        suggestion.type === 'status' &&
+        !childrenSuggestionNames.includes(suggestion.name),
+      getDefaultColumnSuggestionsPerType()
+    )
+    return {
+      fromChildren,
+      fromApp,
+    }
+  }, [suggestions])
+
   return (
-    <SuggestionSelect
-      sending={fetching ? 'fetching' : undefined}
-      value={value}
-      onChange={setValue}
-      onCreate={setProp}
-      onSelect={setProp}
-      suggestions={suggestions}
-    />
+    <Container ref={menuRef}>
+      {fetching && <Spinner />}
+      {orderedSuggestions.fromChildren.length > 0 && (
+        <>
+          <MetadataContainerRow
+            row={{
+              type: 'header',
+              content: `From Child Docs`,
+            }}
+          />
+          {orderedSuggestions.fromChildren.map((propSuggestion) => {
+            const id = `fc-${propSuggestion.name}`
+            const isSelected = prop === propSuggestion.name
+            return (
+              <MetadataContainerRow
+                key={id}
+                row={{
+                  type: 'button',
+                  props: {
+                    label: (
+                      <Flexbox justifyContent='space-between'>
+                        <span>{propSuggestion.name}</span>
+                        {isSelected && <Icon path={mdiCheck} />}
+                      </Flexbox>
+                    ),
+                    iconPath: getIconPathOfPropType(propSuggestion.type),
+                    id: id,
+                    onClick: () => setProp(propSuggestion.name),
+                    disabled: isSelected,
+                  },
+                }}
+              />
+            )
+          })}
+        </>
+      )}
+      {orderedSuggestions.fromApp.length > 0 && (
+        <>
+          <MetadataContainerRow
+            row={{
+              type: 'header',
+              content: `Suggested`,
+            }}
+          />
+          {orderedSuggestions.fromApp.map((propSuggestion) => {
+            const id = `suggested-${propSuggestion.name}`
+            const isSelected = prop === propSuggestion.name
+            return (
+              <MetadataContainerRow
+                key={id}
+                row={{
+                  type: 'button',
+                  props: {
+                    label: (
+                      <Flexbox justifyContent='space-between'>
+                        <span>{propSuggestion.name}</span>
+                        {isSelected && <Icon path={mdiCheck} />}
+                      </Flexbox>
+                    ),
+                    iconPath: getIconPathOfPropType(propSuggestion.type),
+                    disabled: isSelected,
+                    id: id,
+                    onClick: () => setProp(propSuggestion.name),
+                  },
+                }}
+              />
+            )
+          })}
+        </>
+      )}
+    </Container>
   )
 }
+
+const Container = styled(MetadataContainer)`
+  .button__label {
+    width: 100%;
+
+    .flexbox {
+      flex: 1 1 auto;
+    }
+  }
+`
 
 export default KanbanWatchedPropSetter
