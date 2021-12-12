05f7ee47f47d561864c919c142e994e8168c973a
davy-c <d.castel33@gmail.com>
2021-12-13 07:26:17 +0900

    calendar event item changes

--- src/cloud/components/Views/Calendar/CalendarEventItem.tsx
+++ src/cloud/components/Views/Calendar/CalendarEventItem.tsx
@@ -1,17 +1,27 @@
 import { EventContentArg } from '@fullcalendar/common'
 import { EventApi } from '@fullcalendar/react'
 import { mdiDotsHorizontal, mdiFileDocumentOutline } from '@mdi/js'
+import { Emoji } from 'emoji-mart'
 import React from 'react'
+import Flexbox from '../../../../design/components/atoms/Flexbox'
+import Icon from '../../../../design/components/atoms/Icon'
 import NavigationItem from '../../../../design/components/molecules/Navigation/NavigationItem'
 import styled from '../../../../design/lib/styled'
+import { overflowEllipsis } from '../../../../design/lib/styled/styleFunctions'
 import { SerializedDocWithSupplemental } from '../../../interfaces/db/doc'
 import { getDocTitle } from '../../../lib/utils/patterns'
+import {
+  CalendarViewProp,
+  isCalendarStaticProp,
+} from '../../../lib/views/calendar'
+import PropPicker from '../../Props/PropPicker'
 
 type CalendarEventItemProps = {} & EventContentArg
 
 type DocEventExtendedProps = EventApi & {
   extendedProps: {
     doc: SerializedDocWithSupplemental
+    displayedProps: CalendarViewProp[]
     onContextClick: (event: React.MouseEvent) => void
     onClick?: () => void
   }
@@ -23,21 +33,56 @@ const CalendarEventItem = ({ event }: CalendarEventItemProps) => {
     return null
   }
 
+  const { doc, displayedProps, onContextClick, onClick } = event.extendedProps
+
   return (
     <Container className='event__item'>
       <NavigationItem
         className='event__item__nav'
-        label={getDocTitle(event.extendedProps.doc, 'Untitled')}
-        labelClick={event.extendedProps.onClick}
-        icon={
-          event.extendedProps.doc.emoji != null
-            ? { type: 'emoji', path: event.extendedProps.doc.emoji }
-            : { type: 'icon', path: mdiFileDocumentOutline }
+        label={
+          <Flexbox direction='column'>
+            <Flexbox className='event__label__wrapper'>
+              <div className='event__label__icon'>
+                {doc.emoji != null ? (
+                  <Emoji emoji={doc.emoji} set='apple' size={16} />
+                ) : (
+                  <Icon path={mdiFileDocumentOutline} size={16} />
+                )}
+              </div>
+              <span className='event__label'>
+                {getDocTitle(doc, 'Untitled')}
+              </span>
+            </Flexbox>
+            {displayedProps.map((prop, i) => {
+              const docProp = doc.props[prop.name]
+
+              if (isCalendarStaticProp(prop)) {
+                return
+              }
+
+              if (docProp == null || docProp.data == null) {
+                return null
+              }
+
+              return (
+                <div key={`event-${doc.id}-prop-${i}`} className='event__prop'>
+                  <PropPicker
+                    parent={{ type: 'doc', target: doc }}
+                    propName={prop.name}
+                    propData={docProp}
+                    readOnly={true}
+                    showIconPath={true}
+                  />
+                </div>
+              )
+            })}
+          </Flexbox>
         }
+        labelClick={onClick}
         controls={[
           {
             icon: mdiDotsHorizontal,
-            onClick: event.extendedProps.onContextClick,
+            onClick: onContextClick,
           },
         ]}
       />
@@ -65,8 +110,43 @@ const Container = styled.div`
   flex-direction: column;
   background-color: ${({ theme }) => theme.colors.background.tertiary};
 
-  .event__item__nav {
+  .event__item__nav,
+  .navigation__item__wrapper {
     width: 100%;
+    height: min-content;
+  }
+
+  .navigation__item__label {
+    min-height: 24px;
+  }
+
+  .navigation__item__label__ellipsis {
+    flex: 1 1 auto;
+    height: min-content;
+    display: flex;
+    overflow: hidden;
+  }
+
+  .event__label__wrapper,
+  .event__prop {
+    flex: 0 0 auto;
+    width: 100%;
+    overflow: hidden;
+  }
+
+  .event__label__icon {
+    flex: 0 0 auto;
+    margin-right: ${({ theme }) => theme.sizes.spaces.sm}px;
+  }
+
+  .event__label {
+    display: block;
+    ${overflowEllipsis()}
+  }
+
+  .item__property__button {
+    pointer-events: none;
+    padding-left: 0;
   }
 `
 
