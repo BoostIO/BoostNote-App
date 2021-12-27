028da8997e336d610c50604baecd67bae3e7b606
davy-c <d.castel33@gmail.com>
2021-12-28 08:02:45 +0900

    dashboard api

---
+++ src/cloud/api/teams/dashboards/index.ts
@@ -0,0 +1,58 @@
+import { callApi } from '../../../lib/client'
+import { SerializedDashboard } from '../../../interfaces/db/dashboard'
+
+export interface CreateDashboardRequestBody {
+  name: string
+  teamId: string
+}
+
+export interface CreateDashboardResponseBody {
+  data: SerializedDashboard
+}
+
+export async function createDashboard(body: CreateDashboardRequestBody) {
+  const data = await callApi<CreateDashboardResponseBody>(`/api/dashboards`, {
+    json: body,
+    method: 'post',
+  })
+
+  return data
+}
+
+export interface UpdateDashboardRequestBody {
+  name: string
+}
+
+export interface UpdateDashboardResponseBody {
+  data: SerializedDashboard
+}
+
+export async function updateDashboard(
+  dashboard: SerializedDashboard,
+  body: UpdateDashboardRequestBody
+) {
+  const data = await callApi<CreateDashboardResponseBody>(
+    `/api/dashboards/${dashboard.id}`,
+    {
+      json: body,
+      method: 'put',
+    }
+  )
+
+  return data
+}
+
+export interface DeleteDashboardResponseBody {
+  data: SerializedDashboard
+}
+
+export async function deleteDashboard(dashboard: { id: string }) {
+  const data = await callApi<DeleteDashboardResponseBody>(
+    `/api/dashboards/${dashboard.id}`,
+    {
+      method: 'delete',
+    }
+  )
+
+  return data
+}
