414df1aeda61a90c19076f189d934ddfdd4613a8
Simon Leigh <sleigh.dev@gmail.com>
2022-01-11 13:57:26 +0900

    get necessary data for pages

--- src/cloud/pages/workflows/create.tsx
+++ src/cloud/pages/workflows/create.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { getTeamIndexPageData } from '../../api/pages/teams'
 
 const WorkflowCreatePage = () => {
   return <div>Create</div>
 }
+
+WorkflowCreatePage.getInitialProps = getTeamIndexPageData
 
 export default WorkflowCreatePage
