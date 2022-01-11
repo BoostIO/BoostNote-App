960c31d37d9f77b4db1e7589330c5745ba12627c
Simon Leigh <sleigh.dev@gmail.com>
2022-01-11 13:57:26 +0900

    get necessary data for pages

--- src/cloud/pages/automations/create.tsx
+++ src/cloud/pages/automations/create.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { getTeamIndexPageData } from '../../api/pages/teams'
 
 const AutomationCreatePage = () => {
   return <div>Create</div>
 }
+
+AutomationCreatePage.getInitialProps = getTeamIndexPageData
 
 export default AutomationCreatePage
