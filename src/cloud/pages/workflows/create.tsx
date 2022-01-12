3d12fed6ac9bc8e43322b1c3dc379b32eb5e25f2
Simon Leigh <sleigh.dev@gmail.com>
2022-01-13 08:22:57 +0900

    draft page impl

--- src/cloud/pages/workflows/create.tsx
+++ src/cloud/pages/workflows/create.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getTeamIndexPageData } from '../../api/pages/teams'
+import ApplicationContent from '../../components/ApplicationContent'
+import ApplicationPage from '../../components/ApplicationPage'
+import PipeBuilder from '../../components/Automations/PipeBuilder'
+import { SerializedPipe } from '../../interfaces/db/automations'
 
 const WorkflowCreatePage = () => {
-  return <div>Create</div>
+  const [pipe, setPipe] = useState<SerializedPipe>(() => ({
+    name: '',
+    event: 'github.issues.opened',
+    action: 'boost.document.create',
+    configuration: {},
+    filter: {},
+  }))
+
+  return (
+    <ApplicationPage>
+      <ApplicationContent>
+        <PipeBuilder pipe={pipe} onChange={setPipe} />
+      </ApplicationContent>
+    </ApplicationPage>
+  )
 }
 
 WorkflowCreatePage.getInitialProps = getTeamIndexPageData
