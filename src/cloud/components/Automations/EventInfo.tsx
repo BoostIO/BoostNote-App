0db6470bad7d3464c86d4567cdb0547f640564ec
Simon Leigh <sleigh.dev@gmail.com>
2022-01-13 08:15:36 +0900

    event info display

---
+++ src/cloud/components/Automations/EventInfo.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { JsonTypeDef } from '../../lib/automations/events'
+
+interface EventSelectProps {
+  name: string
+  typeDef: JsonTypeDef
+}
+
+const EventInfo = ({ typeDef }: EventSelectProps) => {
+  return (
+    <div>
+      <pre>{JSON.stringify(typeDef, null, 2)}</pre>
+    </div>
+  )
+}
+
+export default EventInfo
