fa562cd40693500484ac74dee1a7bd15af8ad951
Simon Leigh <sleigh.dev@gmail.com>
2022-01-13 16:47:29 +0900

    add Workflow Edit page

--- src/cloud/pages/workflows/[workflowId].tsx
+++ src/cloud/pages/workflows/[workflowId].tsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { getWorkflow } from '../../api/automation/workflow'
-import { getTeamIndexPageData } from '../../api/pages/teams'
-import { SerializedWorkflow } from '../../interfaces/db/automations'
+import React, { useCallback, useState } from 'react'
+import { getWorkflow, updateWorkflow } from '../../api/automation/workflow'
 import { GetInitialPropsParameters } from '../../interfaces/pages'
+import { getTeamIndexPageData } from '../../api/pages/teams'
+import ApplicationContent from '../../components/ApplicationContent'
+import ApplicationPage from '../../components/ApplicationPage'
+import { SerializedWorkflow } from '../../interfaces/db/automations'
+import Topbar from '../../../design/components/organisms/Topbar'
+import { useToast } from '../../../design/lib/stores/toast'
+import { GeneralAppProps } from '../../interfaces/api'
+import WorkflowBuilder, {
+  NewWorkflow,
+} from '../../components/Automations/WorkflowBuilder'
 
-const WorkflowPage = ({ workflow }: { workflow: SerializedWorkflow }) => {
+type WorkflowPageProps = GeneralAppProps & { workflow: SerializedWorkflow }
+
+const WorkflowPage = ({ team, workflow: initial }: WorkflowPageProps) => {
+  const { pushApiErrorMessage } = useToast()
+  const [working, setWorking] = useState(false)
+
+  const saveWorkflow = useCallback(
+    async (workflow: NewWorkflow | SerializedWorkflow) => {
+      try {
+        setWorking(true)
+        await updateWorkflow(initial.id, {
+          name: workflow.name,
+          description: workflow.description,
+          pipes: workflow.pipes,
+          team: team.id,
+        })
+      } catch (err) {
+        pushApiErrorMessage(err)
+      } finally {
+        setWorking(false)
+      }
+    },
+    [pushApiErrorMessage, team, initial.id]
+  )
+
   return (
-    <div>
-      <div>{workflow.name}</div>
-    </div>
+    <ApplicationPage>
+      <Topbar>
+        <h2>Workflows</h2>
+      </Topbar>
+      <ApplicationContent>
+        <WorkflowBuilder
+          initialWorkflow={initial}
+          onSubmit={saveWorkflow}
+          working={working}
+        />
+      </ApplicationContent>
+    </ApplicationPage>
   )
 }
 
