8423e5bea6e591a5fc9e22034c5bf78cae4a70b4
Simon Leigh <sleigh.dev@gmail.com>
2022-01-17 11:37:43 +0900

    automation create page

--- src/cloud/pages/automations/create.tsx
+++ src/cloud/pages/automations/create.tsx
@@ -1,63 +1,61 @@
-import React, { useMemo, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import ApplicationContent from '../../components/ApplicationContent'
 import ApplicationPage from '../../components/ApplicationPage'
 import Topbar from '../../../design/components/organisms/Topbar'
 import { getTeamIndexPageData } from '../../api/pages/teams'
 import { GeneralAppProps } from '../../interfaces/api'
-import {
-  SerializedAutomation,
-  SerializedWorkflow,
-} from '../../interfaces/db/automations'
+import { SerializedWorkflow } from '../../interfaces/db/automations'
 import { GetInitialPropsParameters } from '../../interfaces/pages'
 import { getWorkflows } from '../../api/automation/workflow'
-import Form from '../../../design/components/molecules/Form'
-import FormRow from '../../../design/components/molecules/Form/templates/FormRow'
-import FormRowItem from '../../../design/components/molecules/Form/templates/FormRowItem'
-import FormInput from '../../../design/components/molecules/Form/atoms/FormInput'
-import FormSelect from '../../../design/components/molecules/Form/atoms/FormSelect'
-import { flattenObj } from '../../lib/utils/object'
+import { createAutomation } from '../../api/automation/automation'
+import { useToast } from '../../../design/lib/stores/toast'
+import { useRouter } from '../../lib/router'
+import { getTeamURL } from '../../lib/utils/patterns'
+import AutomationBuilder, {
+  BaseAutomation,
+} from '../../components/Automations/AutomationBuilder'
 
 type AutomationCreatePageProps = GeneralAppProps & {
   workflows: SerializedWorkflow[]
 }
 
-// pipe info
-// env input if necessary
-const AutomationCreatePage = ({ workflows }: AutomationCreatePageProps) => {
-  const [automation, setAutomation] = useState({
-    name: 'New Automation',
-    description: '',
-    workflowId: null as null | number,
-  })
+const AutomationCreatePage = ({
+  workflows,
+  team,
+}: AutomationCreatePageProps) => {
+  const { push } = useRouter()
+  const { pushApiErrorMessage, pushMessage } = useToast()
+  const [working, setWorking] = useState(false)
 
-  const workflowOptions = useMemo(() => {
-    return workflows.map((workflow) => ({
-      label: workflow.name,
-      value: workflow.id.toString(),
-    }))
-  }, [workflows])
+  const saveAutomation = useCallback(
+    async (automation: BaseAutomation) => {
+      if (automation.workflowId == null) {
+        return
+      }
 
-  const currentWorkflow = useMemo(() => {
-    return workflows.find((workflow) => workflow.id === automation.workflowId)
-  }, [workflows, automation.workflowId])
-
-  const currentSelected = useMemo(() => {
-    return currentWorkflow != null
-      ? { label: currentWorkflow.name, value: currentWorkflow.id.toString() }
-      : undefined
-  }, [currentWorkflow])
-
-  const necessaryVars = useMemo(() => {
-    if (currentWorkflow == null) {
-      return []
-    }
-
-    return currentWorkflow.pipes.flatMap((pipe) => {
-      return Object.values(flattenObj(pipe.configuration))
-        .filter((val) => typeof val === 'string' && val.startsWith('$env.'))
-        .map((val) => val.substr('$env.'.length))
-    })
-  }, [currentWorkflow])
+      try {
+        setWorking(true)
+        const newAutomation = await createAutomation({
+          name: automation.name,
+          description: automation.description,
+          workflow: automation.workflowId,
+          team: team.id,
+          env: automation.env,
+        })
+        pushMessage({
+          type: 'success',
+          title: 'Automation Created',
+          description: `${newAutomation.name} has been successfully enabled!`,
+        })
+        push(`${getTeamURL(team)}/automations/${newAutomation.id}`)
+      } catch (err) {
+        pushApiErrorMessage(err)
+      } finally {
+        setWorking(false)
+      }
+    },
+    [team, push, pushApiErrorMessage, pushMessage]
+  )
 
   return (
     <ApplicationPage>
@@ -65,40 +63,12 @@ const AutomationCreatePage = ({ workflows }: AutomationCreatePageProps) => {
         <h2>Automations</h2>
       </Topbar>
       <ApplicationContent>
-        <Form>
-          <FormRow>
-            <FormRowItem>
-              <FormInput
-                value={automation.name}
-                onChange={(ev) =>
-                  setAutomation({ ...automation, name: ev.target.value })
-                }
-              />
-            </FormRowItem>
-          </FormRow>
-          <FormRow>
-            <FormRowItem>
-              <FormInput
-                value={automation.description}
-                onChange={(ev) =>
-                  setAutomation({ ...automation, description: ev.target.value })
-                }
-              />
-            </FormRowItem>
-          </FormRow>
-          <FormRow>
-            <FormRowItem>
-              <FormSelect
-                placeholder='Select Workflow..'
-                options={workflowOptions}
-                value={currentSelected}
-                onChange={({ value }) =>
-                  setAutomation({ ...automation, workflowId: Number(value) })
-                }
-              />
-            </FormRowItem>
-          </FormRow>
-        </Form>
+        <AutomationBuilder
+          initialAutomation={defaultAutomation}
+          onSubmit={saveAutomation}
+          workflows={workflows}
+          working={working}
+        />
       </ApplicationContent>
     </ApplicationPage>
   )
@@ -119,3 +89,10 @@ AutomationCreatePage.getInitialProps = async (
 }
 
 export default AutomationCreatePage
+
+const defaultAutomation = {
+  name: 'New Automation',
+  description: '',
+  workflowId: null as null | number,
+  env: {},
+}
