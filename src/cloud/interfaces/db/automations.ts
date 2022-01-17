31bfe85983c74572ad163f60d50c159ed896f54e
Simon Leigh <sleigh.dev@gmail.com>
2022-01-17 11:36:43 +0900

    api typing fixes

--- src/cloud/interfaces/db/automations.ts
+++ src/cloud/interfaces/db/automations.ts
@@ -23,7 +23,7 @@ export interface SerializedAutomation {
   description: string
   workflowId: number
   teamId: string
-  args: any
+  env: any
   createdById: string
   createdAt: string
   updatedAt: string
