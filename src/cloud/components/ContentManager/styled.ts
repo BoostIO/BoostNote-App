f33f29dbc66e2fd8d7d1a59a803cf5ffe00be657
kazup01 <kazz@boostio.co>
2022-01-27 18:53:20 +0900

    Fix broken UI in the table view

--- src/cloud/components/ContentManager/styled.ts
+++ src/cloud/components/ContentManager/styled.ts
@@ -1,4 +1,3 @@
-import { rightSideTopBarHeight } from '../../../design/components/organisms/Topbar'
 import styled from '../../../design/lib/styled'
 
 export const StyledContentManager = styled.div`
@@ -10,5 +9,4 @@ export const StyledContentManagerList = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  max-height: calc(100vh - ${rightSideTopBarHeight}px);
 `
