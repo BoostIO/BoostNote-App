7b7d8a5e498d1e1b1ab83e9a99db319c66118e92
davy-c <d.castel33@gmail.com>
2022-02-10 11:52:13 +0900

    fix table view display bug

--- src/cloud/components/ContentManager/styled.ts
+++ src/cloud/components/ContentManager/styled.ts
@@ -1,5 +1,4 @@
 import styled from '../../../design/lib/styled'
-import { rightSideTopBarHeight } from '../../../design/components/organisms/Topbar'
 
 export const StyledContentManager = styled.div`
   display: block;
@@ -10,5 +9,4 @@ export const StyledContentManagerList = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  max-height: calc(100vh - ${rightSideTopBarHeight}px);
 `
