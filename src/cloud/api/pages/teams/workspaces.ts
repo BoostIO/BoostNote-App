1e60378ebd1551ce0d7c18948ce5abbfdfe90dd9
Simon Leigh <sleigh.dev@gmail.com>
2022-04-25 13:10:49 +0900

    get views on workspace page

--- src/cloud/api/pages/teams/workspaces.ts
+++ src/cloud/api/pages/teams/workspaces.ts
@@ -5,6 +5,7 @@ import { GetInitialPropsParameters } from '../../../interfaces/pages'
 import { SerializedDoc } from '../../../interfaces/db/doc'
 import { SerializedFolder } from '../../../interfaces/db/folder'
 import { getResourceFromSlug } from '../../mock/db/utils'
+import { ListViewsResponseBody } from '../../teams/views'
 
 export type WorkspacesShowPageResponseBody = GeneralAppProps & {
   pageWorkspace: SerializedWorkspace
@@ -18,25 +19,30 @@ export async function getWorkspaceShowPageData({
   const [, _, ...otherPathnames] = pathname.split('/')
   const [, workspaceId] = getResourceFromSlug(otherPathnames.join('/'))
 
-  const [{ workspace }, { docs }, { folders }] = await Promise.all([
-    callApi<{ workspace: SerializedWorkspace }>(
-      `api/workspaces/${workspaceId}`,
-      { signal, search }
-    ),
-    callApi<{ docs: SerializedDoc[] }>(`api/docs`, {
-      search: { workspaceId, parentFolder: 'root' },
-      signal,
-    }),
-    callApi<{ folders: SerializedFolder[] }>(`api/folders`, {
-      search: { workspaceId, parentFolder: 'root' },
-      signal,
-    }),
-  ])
+  const [{ workspace }, { docs }, { folders }, { data: views }] =
+    await Promise.all([
+      callApi<{ workspace: SerializedWorkspace }>(
+        `api/workspaces/${workspaceId}`,
+        { signal, search }
+      ),
+      callApi<{ docs: SerializedDoc[] }>(`api/docs`, {
+        search: { workspaceId, parentFolder: 'root' },
+        signal,
+      }),
+      callApi<{ folders: SerializedFolder[] }>(`api/folders`, {
+        search: { workspaceId, parentFolder: 'root' },
+        signal,
+      }),
+      callApi<ListViewsResponseBody>(`/api/views`, {
+        search: { workspace: workspaceId },
+      }),
+    ])
 
   return {
     pageWorkspace: workspace,
     workspaces: [workspace],
     docs,
     folders,
+    views,
   }
 }
